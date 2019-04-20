e909f8f67703559b764fefd56a5ef2b31ef05c7e
Hiyabye <hiyabye0465@gmail.com>
2019-04-20 11:40:50 +0900

    Add 02438.c

---
+++ 02xxx/02438.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+
+	scanf("%d", &n);
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<i+1; j++) {
+		  printf("*");
+    }
+	  printf("\n");
+  }
+	return 0;
+}
