d0d61a89de4e4b2722131534ab7246735d7075ab
Hiyabye <hiyabye0465@gmail.com>
2019-08-23 23:15:12 +0900

    Add 01330.c

---
+++ 01xxx/01330.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+	if (a > b)    printf(">");
+	else if (a<b) printf("<");
+	else          printf("==");
+	return 0;
+}
