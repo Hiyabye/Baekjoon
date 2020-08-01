b86ee757a0871bcba98a22370dabc3b08246c2e3
Hiyabye <hiyabye0465@gmail.com>
2020-08-01 10:45:15 +0900

    Add 02444.c

---
+++ 02xxx/02444.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+
+  scanf("%d", &n);
+  for (int i=1; i<n; i++) {
+    for (int j=0; j<n-i; j++)   printf(" ");
+    for (int j=0; j<i*2-1; j++) printf("*");
+    printf("\n");
+  }
+  for (int i=n; i>0; i--) {
+    for (int j=0; j<n-i; j++)   printf(" ");
+    for (int j=0; j<i*2-1; j++) printf("*");
+    printf("\n");
+  }
+  return 0;
+}
