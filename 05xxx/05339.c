c38527afe1dfede27606520cff6b9bc9d210074d
Hiyabye <hiyabye0465@gmail.com>
2020-11-08 22:20:52 +0900

    Add 05339.c

---
+++ 05xxx/05339.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("     /~\\\n");
+  printf("    ( oo|\n");
+  printf("    _\\=/_\n");
+  printf("   /  _  \\\n");
+  printf("  //|/.\\|\\\\\n");
+  printf(" ||  \\ /  ||\n");
+  printf("============\n");
+  printf("|          |\n");
+  printf("|          |\n");
+  printf("|          |");
+  return 0;
+}
