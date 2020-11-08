eef1d3a09de07ada8c84f9e08f098fcbfeeb0b7f
Hiyabye <hiyabye0465@gmail.com>
2020-11-08 19:30:07 +0900

    Add 05338.c

---
+++ 05xxx/05338.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("       _.-;;-._\n");
+  printf("'-..-'|   ||   |\n");
+  printf("'-..-'|_.-;;-._|\n");
+  printf("'-..-'|   ||   |\n");
+  printf("'-..-'|_.-''-._|");
+  return 0;
+}
