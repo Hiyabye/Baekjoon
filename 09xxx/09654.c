1f8650d38278d4da1800ef162c9608c504142af7
Hiyabye <hiyabye0465@gmail.com>
2020-11-08 22:17:01 +0900

    Add 09654.c

---
+++ 09xxx/09654.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n");
+  printf("N2 Bomber      Heavy Fighter  Limited    21        \n");
+  printf("J-Type 327     Light Combat   Unlimited  1         \n");
+  printf("NX Cruiser     Medium Fighter Limited    18        \n");
+  printf("N1 Starfighter Medium Fighter Unlimited  25        \n");
+  printf("Royal Cruiser  Light Combat   Limited    4         ");
+  return 0;
+}
