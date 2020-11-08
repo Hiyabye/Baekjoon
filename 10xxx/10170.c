972bed9a5c8e12e8e1b474b5e3c1441a578e3911
Hiyabye <hiyabye0465@gmail.com>
2020-11-08 22:30:26 +0900

    Add 10170.c

---
+++ 10xxx/10170.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("NFC West       W   L  T\n");
+  printf("-----------------------\n");
+  printf("Seattle        13  3  0\n");
+  printf("San Francisco  12  4  0\n");
+  printf("Arizona        10  6  0\n");
+  printf("St. Louis      7   9  0\n\n");
+  printf("NFC North      W   L  T\n");
+  printf("-----------------------\n");
+  printf("Green Bay      8   7  1\n");
+  printf("Chicago        8   8  0\n");
+  printf("Detroit        7   9  0\n");
+  printf("Minnesota      5  10  1\n");
+  return 0;
+}
