9ad888055208a6f6c56b8ae7c30315564c004d32
Hiyabye <hiyabye0465@gmail.com>
2022-12-10 17:49:23 +0900

    Add 25083.c

---
+++ 25xxx/25083.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("         ,r'\"7\n");
+  printf("r`-_   ,'  ,/\n");
+  printf(" \\. \". L_r'\n");
+  printf("   `~\\/\n");
+  printf("      |\n");
+  printf("      |");
+  return 0;
+}
