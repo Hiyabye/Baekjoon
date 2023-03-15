01a44ec2e43f8a05e44de357f1142a732a6bb76e
Hiyabye <hiyabye0465@gmail.com>
2023-03-16 00:13:49 +0900

    Add 02556.c

---
+++ 02xxx/02556.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+
+  scanf("%d", &n);
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) printf("*");
+    printf("\n");
+  }
+  return 0;
+}
