9a0ce640eb4070deff922d9e98bc706293b01704
Hiyabye <hiyabye0465@gmail.com>
2023-06-27 10:34:11 +0900

    Add 14581.c

---
+++ 14xxx/14581.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  char s[21];
+
+  scanf("%s", s);
+  printf(":fan::fan::fan:\n");
+  printf(":fan::%s::fan:\n", s);
+  printf(":fan::fan::fan:");
+  return 0;
+}
