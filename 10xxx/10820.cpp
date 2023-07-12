b4cc9376ad8282ffaec8f82b824c01a8c9ab345a
Hiyabye <hiyabye0465@gmail.com>
2023-07-12 23:02:15 +0900

    Add 10820.cpp

---
+++ 10xxx/10820.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+  string s;
+
+  getline(cin, s);
+  while (!cin.eof()) {
+    int a = 0, b = 0, c = 0, d = 0;
+    for (int i = 0; i < s.length(); ++i) {
+      if (s[i] >= 'a' && s[i] <= 'z') a++;
+      else if (s[i] >= 'A' && s[i] <= 'Z') b++;
+      else if (s[i] >= '0' && s[i] <= '9') c++;
+      else if (s[i] == ' ') d++;
+    }
+    cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
+    getline(cin, s);
+  }
+  return 0;
+}
