5567dc500527731a6897aa4041694d65f0dd424a
Hiyabye <hiyabye0465@gmail.com>
2023-07-14 17:59:51 +0900

    Add 11279.cpp

---
+++ 11xxx/11279.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  priority_queue<int, vector<int>, less<int> > pq;
+
+  int x;
+  for (int i=0; i<n; i++) {
+    cin >> x;
+    if (x == 0) {
+      if (pq.empty()) cout << "0\n";
+      else {
+        cout << pq.top() << "\n";
+        pq.pop();
+      }
+    } else {
+      pq.push(x);
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
