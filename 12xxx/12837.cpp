4d620f318862ae4175d5c25f8649755b665449b9
Hiyabye <hiyabye0465@gmail.com>
2023-07-14 21:58:40 +0900

    Add 12837.cpp

---
+++ 12xxx/12837.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+template <typename T>
+
+class SegmentTree {
+private:
+  vector<T> tree;
+  vector<T> arr;
+  int n;
+  T (*func)(T, T);
+
+  void init(int node, int start, int end) {
+    if (start == end) {
+      tree[node] = arr[start];
+    } else {
+      init(node*2, start, (start+end)/2);
+      init(node*2+1, (start+end)/2+1, end);
+      tree[node] = func(tree[node*2], tree[node*2+1]);
+    }
+  }
+
+  T query(int node, int start, int end, int left, int right) {
+    if (left > end || right < start) return 0;
+    if (left <= start && end <= right) return tree[node];
+    T lval = query(node*2, start, (start+end)/2, left, right);
+    T rval = query(node*2+1, (start+end)/2+1, end, left, right);
+    return func(lval, rval);
+  }
+
+  void update(int node, int start, int end, int index, T val) {
+    if (index < start || index > end) return;
+    if (start == end) {
+      arr[index] = val;
+      tree[node] = val;
+      return;
+    }
+    update(node*2, start, (start+end)/2, index, val);
+    update(node*2+1, (start+end)/2+1, end, index, val);
+    tree[node] = func(tree[node*2], tree[node*2+1]);
+  }
+
+public:
+  SegmentTree(vector<T> &arr, T (*f)(T, T)) : arr(arr), func(f) {
+    n = arr.size();
+    int h = (int)ceil(log2(n));
+    int tree_size = (1 << (h+1));
+    tree.resize(tree_size);
+    init(1, 0, n-1);
+  }
+
+  T query(int left, int right) {
+    return query(1, 0, n-1, left, right);
+  }
+
+  void update(int index, T val) {
+    update(1, 0, n-1, index, val);
+  }
+};
+
+long long add(long long a, long long b) {
+  return a + b;
+}
+
+void solve(void) {
+  int n, q;
+  cin >> n >> q;
+  vector<long long> arr(n, 0);
+  SegmentTree<long long> st(arr, add);
+
+  long long x, y, z;
+  for (int i=0; i<q; i++) {
+    cin >> x >> y >> z;
+    if (x == 1) st.update(y-1, st.query(y-1, y-1) + z);
+    else cout << st.query(0, z-1) - st.query(0, y-2) << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
