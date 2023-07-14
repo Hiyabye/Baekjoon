b3ab93141977f616b1f3e6926659796de0c27361
Hiyabye <hiyabye0465@gmail.com>
2023-07-14 14:39:02 +0900

    Add 02133.cpp

---
+++ 02xxx/02133.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> dp(31, 0);
+
+void solve(void) {
+  dp[0] = 1;
+  for (int i=2; i<=30; i+=2) {
+    dp[i] = dp[i-2] * 3;
+    for (int j=i-4; j>=0; j-=2) {
+      dp[i] += dp[j] * 2;
+    }
+  }
+
+  cin >> n;
+  cout << dp[n];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
