bbdcc5c08af653bc0a850ed93080b0cf6add5a1c
Hiyabye <hiyabye0465@gmail.com>
2023-07-14 20:03:12 +0900

    Add 05639.cpp

---
+++ 05xxx/05639.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<int> a;
+
+void post(int l, int r) {
+  if (l >= r) return;
+  if (l+1 == r) {
+    cout << a[l] << "\n";
+    return;
+  }
+
+  int m = l+1;
+  while (m < r && a[l] > a[m]) m++;
+  post(l+1, m);
+  post(m, r);
+  cout << a[l] << "\n";
+}
+
+void solve(void) {
+  while (cin >> n) {
+    a.push_back(n);
+  }
+  post(0, a.size());
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
