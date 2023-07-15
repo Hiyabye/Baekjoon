0781f949b424111cb67ecb9d8d5ff292fbbf0f46
Hiyabye <hiyabye0465@gmail.com>
2023-07-15 15:30:18 +0900

    Add 01309.cpp

---
+++ 01xxx/01309.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define MOD 9901
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  vector<int> dp(n+1);
+  dp[0] = 1, dp[1] = 3;
+  for (int i=2; i<=n; i++) {
+    dp[i] = (dp[i-1]*2 + dp[i-2]) % MOD;
+  }
+  cout << dp[n];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
