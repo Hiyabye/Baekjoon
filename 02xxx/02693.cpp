47113aa24c92ad4a5ca175381d50b919e9c6131a
Hiyabye <hiyabye0465@gmail.com>
2023-07-15 16:49:19 +0900

    Add 02693.cpp

---
+++ 02xxx/02693.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  vector<int> a(10);
+  for (int i=0; i<10; i++) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  cout << a[7] << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
