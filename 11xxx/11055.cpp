4cf9f08ec5b857bbf4a38cac58a8101e6dbbc243
Hiyabye <hiyabye0465@gmail.com>
2023-07-15 15:02:01 +0900

    Add 11055.cpp

---
+++ 11xxx/11055.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) {
+    cin >> a[i];
+  }
+
+  vector<int> dp(n, 0);
+  for (int i=0; i<n; i++) {
+    dp[i] = a[i];
+    for (int j=0; j<i; j++) {
+      if (a[j] < a[i]) {
+        dp[i] = max(dp[i], dp[j] + a[i]);
+      }
+    }
+  }
+
+  cout << *max_element(dp.begin(), dp.end()) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
