079025fe27702fd3a0d8cca2e25ebfcc0c6779d4
Hiyabye <hiyabye0465@gmail.com>
2023-07-16 20:31:23 +0900

    Add 01260.cpp

---
+++ 01xxx/01260.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void dfs(vector<vector<bool> > &p, vector<bool> &visited, int cur) {
+  visited[cur] = true;
+  cout << cur << " ";
+  
+  for (int i=1; i<p.size(); i++) {
+    if (p[cur][i] && !visited[i]) {
+      dfs(p, visited, i);
+    }
+  }
+}
+
+void bfs(vector<vector<bool> > &p, vector<bool> &visited, int cur) {
+  queue<int> q;
+  q.push(cur);
+  visited[cur] = true;
+
+  while (!q.empty()) {
+    cur = q.front();
+    q.pop();
+    cout << cur << " ";
+
+    for (int i=1; i<p.size(); i++) {
+      if (p[cur][i] && !visited[i]) {
+        q.push(i);
+        visited[i] = true;
+      }
+    }
+  }
+}
+
+void solve(void) {
+  int n, m, v, x, y;
+  cin >> n >> m >> v;
+  vector<vector<bool> > p(n+1, vector<bool>(n+1, false));
+  for (int i=0; i<m; i++) {
+    cin >> x >> y;
+    p[x][y] = p[y][x] = true;
+  }
+
+  vector<bool> visited(n+1, false);
+  dfs(p, visited, v);
+  cout << "\n";
+
+  fill(visited.begin(), visited.end(), false);
+  bfs(p, visited, v);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
