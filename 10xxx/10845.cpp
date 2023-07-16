6841fa143523a77099f6d0373f7129172c907dfc
Hiyabye <hiyabye0465@gmail.com>
2023-07-16 19:46:34 +0900

    Add 10845.cpp

---
+++ 10xxx/10845.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n, x;
+  cin >> n;
+  string s;
+  queue<int> q;
+
+  for (int i=0; i<n; i++) {
+    cin >> s;
+    if (s == "push") {
+      cin >> x;
+      q.push(x);
+    } else if (s == "pop") {
+      if (q.empty()) {
+        cout << "-1\n";
+        continue;
+      }
+      x = q.front();
+      cout << x << "\n";
+      q.pop();
+    } else if (s == "size") {
+      cout << q.size() << "\n";
+    } else if (s == "empty") {
+      cout << q.empty() << "\n";
+    } else if (s == "front") {
+      if (q.empty()) cout << "-1\n";
+      else cout << q.front() << "\n";
+    } else if (s == "back") {
+      if (q.empty()) cout << "-1\n";
+      else cout << q.back() << "\n";
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
