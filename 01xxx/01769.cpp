9760932b1dbd418f8985e4dd04c405753d24e1ac
Hiyabye <hiyabye0465@gmail.com>
2023-07-18 16:33:04 +0900

    Add 01769.cpp

---
+++ 01xxx/01769.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string change(string x) {
+  int ret = 0;
+  for (int i=0; i<x.size(); i++) {
+    ret += x[i] - '0';
+  }
+  return to_string(ret);
+}
+
+void solve(void) {
+  string x;
+  cin >> x;
+
+  int ans = 0;
+  while (x.size() > 1) {
+    x = change(x);
+    ans++;
+  }
+
+  if (x == "3" || x == "6" || x == "9") cout << ans << "\nYES";
+  else cout << ans << "\nNO";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
