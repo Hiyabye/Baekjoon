1493b457a70ef1915e6a316fc1bbe94c12b71f1e
Hiyabye <hiyabye0465@gmail.com>
2023-07-18 15:51:27 +0900

    Add 03036.cpp

---
+++ 03xxx/03036.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a, int b) {
+  if (!b) return a;
+  return gcd(b, a % b);
+}
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) {
+    cin >> a[i];
+  }
+
+  for (int i=1; i<n; i++) {
+    cout << a[0] / gcd(a[0], a[i]) << "/" << a[i] / gcd(a[0], a[i]) << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
