6ce9fa1fa3e0c988cb82207a71dc5497fc89fdef
Hiyabye <hiyabye0465@gmail.com>
2023-07-18 16:25:21 +0900

    Add 02018.cpp

---
+++ 02xxx/02018.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  int ans = 0;
+  for (int i=1; i<=n; i++) {
+    if (n <= i*(i-1)/2) break;
+    if (n*2%i == 0 && (n*2/i-i+1)%2 == 0) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
