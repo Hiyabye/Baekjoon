9bf4d919d778eec1f8ab2d6715380add1350ef51
Hiyabye <hiyabye0465@gmail.com>
2023-07-18 19:27:57 +0900

    Add 02014.cpp

---
+++ 02xxx/02014.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int k, n;
+  cin >> k >> n;
+  vector<int> a(k);
+  for (int i=0; i<k; i++) {
+    cin >> a[i];
+  }
+
+  priority_queue<long long, vector<long long>, greater<long long> > pq;
+  for (int i=0; i<k; i++) {
+    pq.push(a[i]);
+  }
+
+  long long ans = 0;
+  for (int i=0; i<n-1; i++) {
+    long long cur = pq.top();
+    pq.pop();
+    for (int j=0; j<k; j++) {
+      pq.push(cur * a[j]);
+      if (cur % a[j] == 0) break;
+    }
+  }
+  cout << pq.top();
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
