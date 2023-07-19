db8459422239e8539ad2f05301fa22336f542fab
Hiyabye <hiyabye0465@gmail.com>
2023-07-19 15:08:54 +0900

    Add 16395.cpp

---
+++ 16xxx/16395.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int c(int n, int k) {
+  int ret = 1;
+  for (int i=1; i<=k; i++) {
+    ret *= (n-i+1);
+    ret /= i;
+  }
+  return ret;
+}
+
+void solve(void) {
+  int n, k;
+  cin >> n >> k;
+  cout << c(n-1, k-1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
