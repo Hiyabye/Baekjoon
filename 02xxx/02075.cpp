97e24af01b48d815c1332e9466fc9a75cfd4fca3
Hiyabye <hiyabye0465@gmail.com>
2023-07-19 16:05:51 +0900

    Add 02075.cpp

---
+++ 02xxx/02075.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n*n);
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+      cin >> a[i*n+j];
+    }
+  }
+
+  sort(a.begin(), a.end(), greater<int>());
+  cout << a[n-1];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
