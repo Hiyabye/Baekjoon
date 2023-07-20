df9e97c9d93540d0e5db37ed569488209abc2852
Hiyabye <hiyabye0465@gmail.com>
2023-07-20 23:17:20 +0900

    Add 07785.cpp

---
+++ 07xxx/07785.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  map<string, bool> a;
+  string s, t;
+  for (int i=0; i<n; i++) {
+    cin >> s >> t;
+    if (t == "enter") a[s] = true;
+    else a[s] = false;
+  }
+
+  for (auto it=a.rbegin(); it!=a.rend(); it++) {
+    if (it->second) cout << it->first << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
