08c8dd2370f294a9112918e3b35ea45f85b7cc6b
Hiyabye <hiyabye0465@gmail.com>
2023-07-20 23:35:19 +0900

    Add 10816.cpp

---
+++ 10xxx/10816.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n, m, tmp;
+  map<int, int> a;
+
+  cin >> n;
+  for (int i=0; i<n; i++) {
+    cin >> tmp;
+    a[tmp]++;
+  }
+
+  cin >> m;
+  for (int i=0; i<m; i++) {
+    cin >> tmp;
+    cout << a[tmp] << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
