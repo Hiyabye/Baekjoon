948f7bc58b201275cd11b0a1a504c1fcb9a1d118
Hiyabye <hiyabye0465@gmail.com>
2023-07-20 23:30:11 +0900

    Add 01620.cpp

---
+++ 01xxx/01620.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n, m;
+  cin >> n >> m;
+
+  map<string, int> a;
+  vector<string> b(n+1);
+  for (int i=1; i<=n; i++) {
+    cin >> b[i];
+    a[b[i]] = i;
+  }
+
+  string s;
+  for (int i=0; i<m; i++) {
+    cin >> s;
+    if (isdigit(s[0])) cout << b[stoi(s)];
+    else cout << a[s];
+    cout << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
