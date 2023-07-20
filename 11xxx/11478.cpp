c63df66eb63f254fa5a40848899315f5a13500cf
Hiyabye <hiyabye0465@gmail.com>
2023-07-20 23:44:15 +0900

    Add 11478.cpp

---
+++ 11xxx/11478.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+
+  set<string> a;
+  for (int i=0; i<s.size(); i++) {
+    for (int j=1; j<=s.size()-i; j++) {
+      a.insert(s.substr(i, j));
+    }
+  }
+  cout << a.size();
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
