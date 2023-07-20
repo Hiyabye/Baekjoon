ca17fc73618b86c2fba51dcbf380d2826d1ff6e5
Hiyabye <hiyabye0465@gmail.com>
2023-07-20 21:25:59 +0900

    Add 01094.cpp

---
+++ 01xxx/01094.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int x;
+  cin >> x;
+  
+  int ans = 0;
+  for (int i=0; i<=6; i++) {
+    if (x & (1<<i)) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
