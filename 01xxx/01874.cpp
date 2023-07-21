75e6f473ac97f8547bd17d2fb4b3812fb64374f4
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 15:32:46 +0900

    Add 01874.cpp

---
+++ 01xxx/01874.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) {
+    cin >> a[i];
+  }
+
+  stack<int> s;
+  int cur = 1;
+  vector<char> ans;
+  for (int i=0; i<n; i++) {
+    while (cur <= a[i]) {
+      s.push(cur++);
+      ans.push_back('+');
+    }
+    if (s.top() == a[i]) {
+      s.pop();
+      ans.push_back('-');
+    } else {
+      cout << "NO\n";
+      return;
+    }
+  }
+
+  for (int i=0; i<ans.size(); i++) {
+    cout << ans[i] << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
