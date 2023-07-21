45a6325a8dc98fedeff43f7cae6a5b49c99671b0
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 15:18:59 +0900

    Add 09085.cpp

---
+++ 09xxx/09085.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n, k, sum = 0;
+  cin >> n;
+  for (int i=0; i<n; i++) {
+    cin >> k;
+    sum += k;
+  }
+  
+  cout << sum << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
