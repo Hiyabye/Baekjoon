a1a41f7978f9bdf028468de9a6ff062c7c6c3070
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 15:02:25 +0900

    Add 09325.cpp

---
+++ 09xxx/09325.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n, q, p, sum;
+  cin >> sum;
+  cin >> n;
+  for (int i=0; i<n; i++) {
+    cin >> q >> p;
+    sum += q * p;
+  }
+  
+  cout << sum << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
