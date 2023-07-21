17387527d85a0e42060c7b24a2f69beb259cec99
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 15:24:54 +0900

    Add 11866.cpp

---
+++ 11xxx/11866.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n, k;
+  cin >> n >> k;
+
+  queue<int> q;
+  for (int i=1; i<=n; i++) {
+    q.push(i);
+  }
+
+  cout << "<";
+  while (!q.empty()) {
+    for (int i=0; i<k-1; i++) {
+      q.push(q.front());
+      q.pop();
+    }
+    cout << q.front();
+    q.pop();
+    if (!q.empty()) cout << ", ";
+  }
+  cout << ">";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
