4c14fcab3f226ec6805e1775e41af851509bb47d
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 16:38:32 +0900

    Add 05397.cpp

---
+++ 05xxx/05397.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+  list<char> l;
+  auto cur = l.end();
+  
+  for (auto c : s) {
+    if (c == '<') {
+      if (cur != l.begin()) cur--;
+    } else if (c == '>') {
+      if (cur != l.end()) cur++;
+    } else if (c == '-') {
+      if (cur != l.begin()) {
+        cur--;
+        cur = l.erase(cur);
+      }
+    } else {
+      l.insert(cur, c);
+    }
+  }
+
+  for (auto c : l) {
+    cout << c;
+  }
+  cout << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
