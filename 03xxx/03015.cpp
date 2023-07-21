e7c47d41b8bcb0f5bf3b6c72a6ee3b53e8c919d1
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 18:49:22 +0900

    Add 03015.cpp

---
+++ 03xxx/03015.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) {
+    cin >> a[i];
+  }
+
+  long long ans = 0;
+  stack<pair<int, int> > s;
+  for (int i=0; i<n; i++) {
+    int same = 1;
+    while (!s.empty() && s.top().first <= a[i]) {
+      ans += s.top().second;
+      if (s.top().first == a[i]) same += s.top().second;
+      s.pop();
+    }
+    if (!s.empty()) ans++;
+    s.push(make_pair(a[i], same));
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
