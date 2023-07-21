90db4d14d5b1cab1d3f3b0c35b27cdef5133547e
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 16:28:05 +0900

    Add 01406.cpp

---
+++ 01xxx/01406.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+  list<char> l;
+  for (auto c: s) {
+    l.push_back(c);
+  }
+  auto cur = l.end();
+
+  int m;
+  char command, $;
+  cin >> m;
+  while (m--) {
+    cin >> command;
+    if (command == 'L') {
+      if (cur != l.begin()) cur--;
+    } else if (command == 'D') {
+      if (cur != l.end()) cur++;
+    } else if (command == 'B') {
+      if (cur != l.begin()) {
+        cur--;
+        cur = l.erase(cur);
+      }
+    } else if (command == 'P') {
+      cin >> $;
+      l.insert(cur, $);
+    }
+  }
+
+  for (auto c: l) {
+    cout << c;
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
