1a152fd7e3cb34ccbb208c0ca7ca9ad925f580be
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 18:22:32 +0900

    Add 17298.cpp

---
+++ 17xxx/17298.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) {
+    cin >> a[i];
+  }
+
+  vector<int> ans(n, -1);
+  stack<int> s;
+  s.push(0);
+  for (int i=1; i<n; i++) {
+    while (!s.empty() && a[s.top()] < a[i]) {
+      ans[s.top()] = a[i];
+      s.pop();
+    }
+    s.push(i);
+  }
+
+  for (int i=0; i<n; i++) {
+    cout << ans[i] << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
