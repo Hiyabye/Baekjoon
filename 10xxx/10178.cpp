7224f881646dd04d7e3602a826cd49fd61b88006
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 15:04:34 +0900

    Add 10178.cpp

---
+++ 10xxx/10178.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int c, v;
+  cin >> c >> v;
+  cout << "You get " << c/v << " piece(s) and your dad gets " << c%v << " piece(s).\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
