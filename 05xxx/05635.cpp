2cb314d8210ec032d1d8fa9243c296d324c5cf19
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 14:45:11 +0900

    Add 05635.cpp

---
+++ 05xxx/05635.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef struct Student {
+  string name;
+  int d, m, y;
+} Student;
+
+bool cmp(Student &a, Student &b) {
+  if (a.y != b.y) return a.y > b.y;
+  if (a.m != b.m) return a.m > b.m;
+  return a.d > b.d;
+}
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<Student> s(n);
+  for (int i=0; i<n; i++) {
+    cin >> s[i].name >> s[i].d >> s[i].m >> s[i].y;
+  }
+
+  sort(s.begin(), s.end(), cmp);
+  cout << s.front().name << "\n";
+  cout << s.back().name << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
