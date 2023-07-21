18eb478f0d72cf5f7ee4abe038a7ac0728c3557d
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 14:58:13 +0900

    Add 10984.cpp

---
+++ 10xxx/10984.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n, credit, total = 0;
+  double grade, sum = 0;
+
+  cin >> n;
+  for (int i=0; i<n; i++) {
+    cin >> credit >> grade;
+    total += credit;
+    sum += credit * grade;
+  }
+
+  cout << total << " " << fixed << setprecision(1) << sum/total << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
