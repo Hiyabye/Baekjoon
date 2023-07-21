a5439b0fad2f5ea042ca5c526d7efa88a2123e28
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 18:08:59 +0900

    Add 06198.cpp

---
+++ 06xxx/06198.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) {
+    cin >> a[i];
+  }
+
+  long long ans = 0;
+  stack<int> s;
+  s.push(a[0]);
+  for (int i=1; i<n; i++) {
+    while (!s.empty() && a[i] >= s.top()) s.pop();
+    ans += s.size();
+    s.push(a[i]);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
