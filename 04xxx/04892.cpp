9a4e24a0fdfd401c348cd541e8dbfdc2d6770008
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 14:31:48 +0900

    Add 04892.cpp

---
+++ 04xxx/04892.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool solve(int idx) {
+  int n0;
+  cin >> n0;
+  if (n0 == 0) return false;
+
+  int n1 = n0*3;
+  int n2 = n1 % 2 == 0 ? n1/2 : (n1+1)/2;
+  int n3 = n2*3;
+  int n4 = n3/9;
+  
+  if (n1 % 2 == 0) cout << idx << ". even " << n4 << "\n";
+  else cout << idx << ". odd " << n4 << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int idx = 1;
+  while (solve(idx++));
+  return 0;
+}
