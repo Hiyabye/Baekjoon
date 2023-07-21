8597ad04791106f117e21c53d93e62ecab25ca9a
Hiyabye <hiyabye0465@gmail.com>
2023-07-21 14:40:10 +0900

    Add 11098.cpp

---
+++ 11xxx/11098.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int p;
+  cin >> p;
+  vector<pair<int, string> > v(p);
+  for (int i=0; i<p; i++) {
+    cin >> v[i].first >> v[i].second;
+  }
+
+  sort(v.begin(), v.end());
+  cout << v.back().second << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n;
+  cin >> n;
+  while (n--) {
+    solve();
+  }
+  return 0;
+}
