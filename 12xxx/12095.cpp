c9f2ff5e8a720733056a6e10f50f110ffc43f39f
Hiyabye <hiyabye0465@gmail.com>
2023-07-22 16:16:24 +0900

    Add 12095.cpp

---
+++ 12xxx/12095.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  cout << "0 0 0 0 0 0 0 0 0\n";
+  cout << "0 0 3 6 0 0 0 0 0\n";
+  cout << "0 7 0 0 9 0 2 0 0\n";
+  cout << "0 5 0 0 0 7 0 0 0\n";
+  cout << "0 0 0 0 4 5 7 0 0\n";
+  cout << "0 0 0 1 0 0 0 3 0\n";
+  cout << "0 0 1 0 0 0 0 6 8\n";
+  cout << "0 0 8 5 0 0 0 1 0\n";
+  cout << "0 9 0 0 0 0 4 0 0\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
