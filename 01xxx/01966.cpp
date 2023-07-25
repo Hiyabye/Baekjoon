f356c609807755f0cdb24a5072728776265a1cb1
Hiyabye <hiyabye0465@gmail.com>
2023-07-25 16:40:35 +0900

    Add 01966.cpp

---
+++ 01xxx/01966.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(void) {
+  int n, m, x;
+  cin >> n >> m;
+  queue<pair<int, int> > q;
+  priority_queue<int> pq;
+  for (int i=0; i<n; i++) {
+    cin >> x;
+    q.push(make_pair(i, x));
+    pq.push(x);
+  }
+
+  int ans = 0;
+  while (!q.empty()) {
+    pair<int, int> cur = q.front();
+    q.pop();
+
+    if (cur.second == pq.top()) {
+      pq.pop();
+      ans++;
+      if (cur.first == m) break;
+    } else {
+      q.push(cur);
+    }
+  }
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
