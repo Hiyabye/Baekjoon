9a8fb11509135d1ad05f41df57d2143551f89f91
Hiyabye <hiyabye0465@gmail.com>
2023-07-27 17:11:05 +0900

    Add 11724.cpp

---
+++ 11xxx/11724.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, a, b;
+vector<vector<int> > adj;
+vector<bool> visited;
+
+void dfs(int cur) {
+  visited[cur] = true;
+  for (int i=0; i<adj[cur].size(); i++) {
+    if (visited[adj[cur][i]]) continue;
+    dfs(adj[cur][i]);
+  }
+}
+
+void solve(void) {
+  cin >> n >> m;
+  adj.resize(n+1, vector<int>());
+  visited.resize(n+1, false);
+  for (int i=0; i<m; i++) {
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+
+  int ans = 0;
+  for (int i=1; i<=n; i++) {
+    if (visited[i]) continue;
+    dfs(i);
+    ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
