b2585e1f0f3c0b59b35a263245ed326db22f7811
Hiyabye <hiyabye0465@gmail.com>
2023-07-27 15:26:26 +0900

    Add 01541.cpp

---
+++ 01xxx/01541.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+
+  int ans = 0, sign = 1, num = 0;
+  for (int i=0; i<s.size(); i++) {
+    if (s[i] == '+') {
+      ans += sign * num;
+      num = 0;
+    } else if (s[i] == '-') {
+      ans += sign * num;
+      num = 0;
+      sign = -1;
+    } else {
+      num = num * 10 + (s[i] - '0');
+    }
+  }
+  ans += sign * num;
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
