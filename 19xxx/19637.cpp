3df2c992b26c84ca8f42b23d5f45686c84c27ef0
Hiyabye <hiyabye0465@gmail.com>
2023-07-28 23:32:37 +0900

    Add 19637.cpp

---
+++ 19xxx/19637.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, m, x;
+  cin >> n >> m;
+  vector<pair<int, string> > a;
+  string s;
+  cin >> s >> x;
+  a.push_back(make_pair(x, s));
+  for (int i=1; i<n; i++) {
+    cin >> s >> x;
+    if (a[i-1].first == x) continue;
+    else a.push_back(make_pair(x, s));
+  }
+
+  for (int i=0; i<m; i++) {
+    cin >> x;
+    int l = 0, r = n-1;
+    while (l < r) {
+      int m = l+(r-l)/2;
+      if (a[m].first >= x) r = m;
+      else l = m+1;
+    }
+    cout << a[l].second << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
