76c720559e37b434c20345f13c25d1d8b7fdb78a
Hiyabye <hiyabye0465@gmail.com>
2023-07-28 17:48:06 +0900

    Add 01237.cpp

---
+++ 01xxx/01237.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  cout << "문제의 정답";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
