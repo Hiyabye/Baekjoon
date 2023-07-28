23ccca0800e91762ddf42a86f33158891ba02794
Hiyabye <hiyabye0465@gmail.com>
2023-07-28 21:20:26 +0900

    Add 02467.cpp

---
+++ 02xxx/02467.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) {
+    cin >> a[i];
+  }
+
+  sort(a.begin(), a.end());
+
+  int l = 0, r = n - 1;
+  int ans_l = l, ans_r = r, key = a[l] + a[r];
+  while (l < r) {
+    if (abs(a[l] + a[r]) < abs(key)) {
+      ans_l = l;
+      ans_r = r;
+      key = a[ans_l] + a[ans_r];
+    }
+    if (a[l] + a[r] < 0) l++;
+    else                 r--;
+  }
+  cout << a[ans_l] << " " << a[ans_r];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
