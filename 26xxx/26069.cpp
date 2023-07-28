0d9df5b8985b8b26f6addbcf1f2e5f09a563ce3b
Hiyabye <hiyabye0465@gmail.com>
2023-07-28 18:07:33 +0900

    Add 26069.cpp

---
+++ 26xxx/26069.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  map<string, bool> m;
+  string a, b;
+  for (int i=0; i<n; i++) {
+    cin >> a >> b;
+    if (a == "ChongChong") m[a] = true;
+    if (b == "ChongChong") m[b] = true;
+    if (m.find(a) == m.end()) {
+      if (m.find(b) == m.end()) m[a] = m[b] = false;
+      else m[a] = m[b];
+    } else {
+      if (m.find(b) == m.end()) m[b] = m[a];
+      else m[a] = m[b] = m[a] || m[b];
+    }
+  }
+
+  int ans = 0;
+  for (auto it : m) {
+    if (it.second) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
