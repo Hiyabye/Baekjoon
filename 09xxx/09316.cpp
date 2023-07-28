8b047022e9d8d4b6ed35f69f80b8cca4cab4ed95
Hiyabye <hiyabye0465@gmail.com>
2023-07-29 07:38:13 +0900

    Add 09316.cpp

---
+++ 09xxx/09316.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  for (int i=1; i<=n; i++) {
+    cout << "Hello World, Judge " << i << "!\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
