ba2cdfaa422552d11634f1528a92752ec5c324ef
Hiyabye <hiyabye0465@gmail.com>
2023-07-28 17:58:07 +0900

    Add 25192.cpp

---
+++ 25xxx/25192.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, ans = 0;
+  cin >> n;
+
+  map<string, bool> m;
+  string s;
+  for (int i=0; i<n; i++) {
+    cin >> s;
+    if (s == "ENTER") m.clear();
+    else if (m.find(s) == m.end()) {
+      m[s] = true;
+      ans++;
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
