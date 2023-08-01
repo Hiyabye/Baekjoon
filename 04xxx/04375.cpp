4c96cdcc82fa2fb7af5e65babb3f473c5e17b8f8
Hiyabye <hiyabye0465@gmail.com>
2023-08-02 00:06:40 +0900

    Add 04375.cpp

---
+++ 04xxx/04375.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+
+  while (cin >> n) {
+    int ans = 1, r = 1;
+    while (r % n) {
+      r = (r*10+1) % n;
+      ans++;
+    }
+    cout << ans << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
