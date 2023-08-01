80af57a53c89476b07eb1440a7bdda1129111ae7
Hiyabye <hiyabye0465@gmail.com>
2023-08-01 14:44:58 +0900

    Add 02592.cpp

---
+++ 02xxx/02592.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, sum = 0;
+  map<int, int> m;
+  for (int i=0; i<10; i++) {
+    cin >> n;
+    sum += n;
+    if (m.find(n) == m.end()) m[n] = 1;
+    else m[n]++;
+  }
+
+  cout << sum / 10 << "\n";
+  int max_cnt = 0, max_num = 0;
+  for (auto it=m.begin(); it!=m.end(); it++) {
+    if (it->second > max_cnt) {
+      max_cnt = it->second;
+      max_num = it->first;
+    }
+  }
+  cout << max_num;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
