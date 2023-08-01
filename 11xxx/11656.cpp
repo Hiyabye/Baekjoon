b7d6c02351347ce72e88640db2a85cdd5f2aa0fd
Hiyabye <hiyabye0465@gmail.com>
2023-08-01 23:40:10 +0900

    Add 11656.cpp

---
+++ 11xxx/11656.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+  vector<string> a;
+  for (int i=0; i<s.size(); i++) {
+    a.push_back(s.substr(i));
+  }
+
+  sort(a.begin(), a.end());
+
+  for (int i=0; i<s.size(); i++) {
+    cout << a[i] << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
