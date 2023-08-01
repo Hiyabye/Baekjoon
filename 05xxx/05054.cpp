8ea7b72cf0cc8f2bc235f1a545d54056cf5cdc4d
Hiyabye <hiyabye0465@gmail.com>
2023-08-01 14:54:28 +0900

    Add 05054.cpp

---
+++ 05xxx/05054.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, x;
+  cin >> n;
+
+  int max = -1, min = 100;
+  for (int i=0; i<n; i++) {
+    cin >> x;
+    max = max > x ? max : x;
+    min = min < x ? min : x;
+  }
+  cout << (max - min) * 2 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
