91e865e7d056c7dd76eac9bc52b694eb1a9f725e
Hiyabye <hiyabye0465@gmail.com>
2023-08-01 14:47:47 +0900

    Add 02711.cpp

---
+++ 02xxx/02711.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  string s;
+  cin >> n >> s;
+  for (int i=0; i<s.size(); i++) {
+    if (i != n-1) cout << s[i];
+  }
+  cout << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
