889054b1b39c63e8ef546e04732863c1d242af7e
Hiyabye <hiyabye0465@gmail.com>
2023-08-01 14:50:47 +0900

    Add 03460.cpp

---
+++ 03xxx/03460.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, idx = 0;
+  cin >> n;
+
+  while (n) {
+    if (n % 2) cout << idx << " ";
+    n /= 2;
+    idx++;
+  }
+  cout << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
