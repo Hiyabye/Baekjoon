00ff0ab78d39c54131f602352b9e5e9d80b2f527
Hiyabye <hiyabye0465@gmail.com>
2023-08-01 23:44:21 +0900

    Add 01302.cpp

---
+++ 01xxx/01302.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  map<string, int> a;
+  string s;
+  for (int i=0; i<n; i++) {
+    cin >> s;
+    if (a.find(s) == a.end()) a[s] = 1;
+    else a[s]++;
+  }
+
+  int best_cnt = 0;
+  string best_name;
+  for (auto it=a.begin(); it!=a.end(); it++) {
+    if (it->second > best_cnt) {
+      best_cnt = it->second;
+      best_name = it->first;
+    } else if (it->second == best_cnt && it->first < best_name) {
+      best_name = it->first;
+    }
+  }
+  cout << best_name;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
