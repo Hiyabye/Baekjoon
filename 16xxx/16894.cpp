6b65cc7fc474afb56c1eb91f1d95a2eaec45fc55
Hiyabye <hiyabye0465@gmail.com>
2023-08-01 16:44:25 +0900

    Add 16894.cpp

---
+++ 16xxx/16894.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  long long n;
+  cin >> n;
+
+  int cnt = 0;
+  for (int i=2; i<=sqrt(n); i++) {
+    while (n % i == 0) {
+      n /= i;
+      cnt++;
+    }
+    if (cnt >= 2) break;
+  }
+  if (n > 1) cnt++;
+  
+  cout << (cnt == 2 ? "cubelover" : "koosaga");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
