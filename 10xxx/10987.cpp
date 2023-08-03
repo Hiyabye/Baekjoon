d040cf6f95bc988d8a126df385ec2b8183b98a14
Hiyabye <hiyabye0465@gmail.com>
2023-08-03 13:37:07 +0900

    Add 10987.cpp

---
+++ 10xxx/10987.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+
+  int ans = 0;
+  for (int i=0; i<s.size(); i++) {
+    if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
