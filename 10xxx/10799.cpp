f66bfc0e2b5adfdaa73eb02d2e26e18a0fdd81f3
Hiyabye <hiyabye0465@gmail.com>
2023-08-03 20:14:15 +0900

    Add 10799.cpp

---
+++ 10xxx/10799.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+
+  int ans = 0;
+  stack<char> st;
+  for (int i=0; i<s.size(); i++) {
+    if (s[i] == '(') st.push(s[i]);
+    else {
+      st.pop();
+      if (s[i-1] == '(') ans += st.size();
+      else ans++;
+    }
+  }
+  
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
