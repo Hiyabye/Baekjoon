3cfd77e8ae919425ac54610de82bb414ec5a2afc
Hiyabye <hiyabye0465@gmail.com>
2023-08-04 11:33:10 +0900

    Add 10173.cpp

---
+++ 10xxx/10173.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solve(void) {
+  string s;
+  getline(cin, s);
+  if (s == "EOI") return false;
+
+  for (int i=0; i<s.size()-3; i++) {
+    if (toupper(s[i]) == 'N' && toupper(s[i+1]) == 'E' && toupper(s[i+2]) == 'M' && toupper(s[i+3]) == 'O') {
+      cout << "Found\n";
+      return true;
+    }
+  }
+  cout << "Missing\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  while (solve());
+  return 0;
+}
