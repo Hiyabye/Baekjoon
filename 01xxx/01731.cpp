8bb8f93be303ee58e1e6a72d836d916f2d0931d0
Hiyabye <hiyabye0465@gmail.com>
2023-08-05 12:32:57 +0900

    Add 01731.cpp

---
+++ 01xxx/01731.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<long long> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  if (a[2]-a[1] == a[1]-a[0]) cout << a[n-1]+(a[1]-a[0]);
+  else                        cout << a[n-1]*(a[1]/a[0]);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
