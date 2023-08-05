87c3d3b61e5d2d35a02ff47ee2d33949b9782e02
Hiyabye <hiyabye0465@gmail.com>
2023-08-05 12:44:47 +0900

    Add 02386.cpp

---
+++ 02xxx/02386.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solve(void) {
+  char c;
+  cin >> c;
+  if (c == '#') return false;
+  string s;
+  getline(cin, s);
+
+  int cnt = 0;
+  for (int i=0; i<s.length(); i++) {
+    if (tolower(s[i]) == tolower(c)) cnt++;
+  }
+  cout << c << " " << cnt << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  while (solve());
+  return 0;
+}
