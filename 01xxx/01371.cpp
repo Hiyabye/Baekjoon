4bffa3e9c4f5db5b22f68a5acc7a97562e7dda1a
Hiyabye <hiyabye0465@gmail.com>
2023-08-05 12:25:15 +0900

    Add 01371.cpp

---
+++ 01xxx/01371.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  vector<int> cnt(26, 0);
+  string s;
+  while (cin >> s) {
+    for (int i=0; i<s.length(); i++) {
+      if (s[i] >= 'a' && s[i] <= 'z') cnt[s[i]-'a']++;
+      else if (s[i] >= 'A' && s[i] <= 'Z') cnt[s[i]-'A']++;
+    }
+  }
+
+  int mx = *max_element(cnt.begin(), cnt.end());
+  for (int i=0; i<26; i++) {
+    if (cnt[i] == mx) cout << (char)(i+'a');
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
