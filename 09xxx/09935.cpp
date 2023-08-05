b33f7b2f4b4ce77f70a96d1439d2069d538926e7
Hiyabye <hiyabye0465@gmail.com>
2023-08-05 13:27:53 +0900

    Add 09935.cpp

---
+++ 09xxx/09935.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s, t, ans;
+  cin >> s;
+  cin >> t;
+
+  for (int i=0; i<s.length(); i++) {
+    ans += s[i];
+    if (ans.length() >= t.length() && ans.substr(ans.length()-t.length()) == t) {
+      ans.erase(ans.length()-t.length());
+    }
+  }
+  cout << (ans.length() ? ans : "FRULA");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
