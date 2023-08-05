2eab5d32f2e29b14ebdbc49a8f3aaa065453445e
Hiyabye <hiyabye0465@gmail.com>
2023-08-05 12:28:35 +0900

    Add 01568.cpp

---
+++ 01xxx/01568.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  int ans = 0, i = 1;
+  while (n > 0) {
+    if (n < i) i = 1;
+    n -= i;
+    i++;
+    ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
