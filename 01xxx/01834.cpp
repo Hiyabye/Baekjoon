a7ba523344236572d6d22698afff44c7876e6fc8
Hiyabye <hiyabye0465@gmail.com>
2023-08-05 12:36:15 +0900

    Add 01834.cpp

---
+++ 01xxx/01834.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  long long ans = 0;
+  for (long long i=1; i<=n-1; i++) {
+    ans += n*i+i;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
