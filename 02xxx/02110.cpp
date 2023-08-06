cf1f8a32d6b33bc343337ffc24cd08ea47bc881f
Hiyabye <hiyabye0465@gmail.com>
2023-08-06 23:38:25 +0900

    Add 02110.cpp

---
+++ 02xxx/02110.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int calculate(vector<int> &x, int mid) {
+  int cnt = 1, prev = x[0];
+  for (int i=1; i<x.size(); i++) {
+    if (x[i]-prev >= mid) {
+      cnt++;
+      prev = x[i];
+    }
+  }
+  return cnt;
+}
+
+void solve(void) {
+  int n, c;
+  cin >> n >> c;
+  vector<int> x(n);
+  for (int i=0; i<n; i++) cin >> x[i];
+
+  sort(x.begin(), x.end());
+
+  int lo = 1, hi = x[n-1]-x[0], ans = 0;
+  while (lo <= hi) {
+    int mid = (lo+hi)/2;
+    if (calculate(x, mid) >= c) {
+      ans = mid;
+      lo = mid+1;
+    } else {
+      hi = mid-1;
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
