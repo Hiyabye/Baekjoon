59edfceab96b6bb4d25be26b342c6d07e07c15b6
Hiyabye <hiyabye0465@gmail.com>
2023-08-06 23:44:16 +0900

    Add 02470.cpp

---
+++ 02xxx/02470.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  sort(a.begin(), a.end());
+
+  int lo = 0, hi = n-1, sum = 2e9;
+  pair<int, int> ans;
+  while (lo < hi) {
+    if (abs(a[lo]+a[hi]) < sum) {
+      sum = abs(a[lo]+a[hi]);
+      ans = make_pair(a[lo], a[hi]);
+    }
+    if (a[lo]+a[hi] < 0) lo++;
+    else hi--;
+  }
+  
+  cout << ans.first << " " << ans.second;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
