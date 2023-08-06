6344da014474c4215f0dcd99163cc7901618c367
Hiyabye <hiyabye0465@gmail.com>
2023-08-06 19:04:25 +0900

    Add 01339.cpp

---
+++ 01xxx/01339.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  vector<int> alpha(26, 0);
+  string s;
+  for (int i=0; i<n; i++) {
+    cin >> s;
+    for (auto j=0; j<s.length(); j++) {
+      alpha[s[j]-'A'] += pow(10, s.length()-j-1);
+    }
+  }
+
+  sort(alpha.begin(), alpha.end(), greater<int>());
+
+  int ans = 0;
+  for (int i=0; i<9; i++) {
+    ans += alpha[i] * (9-i);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
