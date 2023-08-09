74f4a65a38b5615709ded61d64fc1165e3f6ade7
Hiyabye <hiyabye0465@gmail.com>
2023-08-09 12:57:57 +0900

    Add 10821.cpp

---
+++ 10xxx/10821.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s;
+  cin >> s;
+
+  int ans = 0;
+  for (auto i=0; i<s.length(); i++) {
+    if (s[i] == ',') ans++;
+  }
+  cout << ans + 1 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
