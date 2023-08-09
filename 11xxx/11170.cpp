828ecb0dae2c3dfd0a5c41e2e0e859553a37c908
Hiyabye <hiyabye0465@gmail.com>
2023-08-09 13:03:21 +0900

    Add 11170.cpp

---
+++ 11xxx/11170.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, m;
+  cin >> n >> m;
+
+  int ans = 0;
+  for (int i=n; i<=m; i++) {
+    string s = to_string(i);
+    for (auto j=0; j<s.length(); j++) {
+      if (s[j] == '0') ans++;
+    }
+  }
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
