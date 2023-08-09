8b42e347dd409f5831c9d3f75e3c5326df28cba0
Hiyabye <hiyabye0465@gmail.com>
2023-08-09 12:37:52 +0900

    Add 05176.cpp

---
+++ 05xxx/05176.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int p, m;
+  cin >> p >> m;
+
+  int ans = 0, x;
+  vector<bool> a(m, false);
+  for (int i=0; i<p; i++) {
+    cin >> x;
+    if (a[x-1]) ans++;
+    else a[x-1] = true;
+  }
+
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int k;
+  cin >> k;
+  while (k--) {
+    solve();
+  }
+  return 0;
+}
