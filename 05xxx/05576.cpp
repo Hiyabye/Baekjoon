23f59d773cf2a2f0c98122d89c93973520ae82e5
Hiyabye <hiyabye0465@gmail.com>
2023-08-09 12:45:44 +0900

    Add 05576.cpp

---
+++ 05xxx/05576.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  vector<int> a(10), b(10);
+  for (int i=0; i<10; i++) cin >> a[i];
+  for (int i=0; i<10; i++) cin >> b[i];
+  sort(a.begin(), a.end(), greater<int>());
+  sort(b.begin(), b.end(), greater<int>());
+
+  cout << a[0] + a[1] + a[2] << " " << b[0] + b[1] + b[2] << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
