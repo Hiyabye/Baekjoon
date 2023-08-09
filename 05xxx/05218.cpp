6039794d6177c9c8ecdbe3baafff217a3e201e0a
Hiyabye <hiyabye0465@gmail.com>
2023-08-09 13:00:59 +0900

    Add 05218.cpp

---
+++ 05xxx/05218.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string a, b;
+  cin >> a >> b;
+
+  cout << "Distances: ";
+  for (auto i=0; i<a.length(); i++) {
+    cout << (b[i] - a[i] + 26) % 26 << " ";
+  }
+  cout << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
