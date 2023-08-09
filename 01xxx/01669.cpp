67cb026c365540c0eb11243e1e337d535e434389
Hiyabye <hiyabye0465@gmail.com>
2023-08-09 18:09:56 +0900

    Add 01669.cpp

---
+++ 01xxx/01669.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int x, y;
+  cin >> x >> y;
+
+  if (x == y) {
+    cout << "0\n";
+    return;
+  }
+
+  int d = sqrt(y-x);
+  if (y-x > d*d+d) cout << 2*d+1;
+  else if (y-x > d*d) cout << 2*d;
+  else cout << 2*d-1;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
