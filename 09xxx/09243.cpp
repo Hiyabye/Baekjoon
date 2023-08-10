3dbac2ebd4f03d2cc7b20ed2e39e340a24843a99
Hiyabye <hiyabye0465@gmail.com>
2023-08-10 18:27:19 +0900

    Add 09243.cpp

---
+++ 09xxx/09243.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(int n, string a, string b) {
+  for (int i=0; i<a.length(); i++) {
+    if (n % 2 == 0 && a[i] != b[i]) return false;
+    if (n % 2 == 1 && a[i] == b[i]) return false;
+  }
+  return true;
+}
+
+void solve(void) {
+  int n;
+  string a, b;
+  cin >> n;
+  cin >> a;
+  cin >> b;
+
+  cout << (check(n, a, b) ? "Deletion succeeded" : "Deletion failed") << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
