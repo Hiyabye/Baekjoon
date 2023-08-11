30a80cd93e323f557c0021cc33da47027221ed05
Hiyabye <hiyabye0465@gmail.com>
2023-08-11 17:34:59 +0900

    Add 11003.cpp

---
+++ 11xxx/11003.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, l;
+  cin >> n >> l;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  deque<pair<int, int> > dq;
+  for (int i=1; i<=n; i++) {
+    if (!dq.empty() && dq.front().second < i-l+1) dq.pop_front();
+    while (!dq.empty() && dq.back().first > a[i-1]) dq.pop_back();
+    dq.push_back(make_pair(a[i-1], i));
+    cout << dq.front().first << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
