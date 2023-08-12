f0e2c4c90c3b3f241e057d92823056a07df86821
Hiyabye <hiyabye0465@gmail.com>
2023-08-12 19:14:24 +0900

    Add 11689.cpp

---
+++ 11xxx/11689.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  long long n;
+  cin >> n;
+
+  long long ans = n;
+  for (long long i=2; i*i<=n; i++) {
+    if (n % i) continue;
+    ans -= ans / i;
+    while (n % i == 0) n /= i;
+  }
+  if (n > 1) ans -= ans / n;
+
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
