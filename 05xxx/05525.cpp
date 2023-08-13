c0ea64b7affd78751daa9abdaaca7cfe6b581e80
Hiyabye <hiyabye0465@gmail.com>
2023-08-13 20:46:11 +0900

    Add 05525.cpp

---
+++ 05xxx/05525.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, m;
+  string s;
+  cin >> n >> m >> s;
+
+  vector<int> dp(m, 0);
+  for (int i=0; i<m-2; i++) {
+    if (s[i] == 'I' && s[i+1] == 'O' && s[i+2] == 'I') dp[i+2] = dp[i] + 1;
+  }
+
+  int ans = 0;
+  for (int i=0; i<m; i++) {
+    if (dp[i] >= n) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
