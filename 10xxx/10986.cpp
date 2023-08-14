d9ec8d0b318ad59491f442c4612adb3ba3e5a06f
Hiyabye <hiyabye0465@gmail.com>
2023-08-14 13:14:44 +0900

    Add 10986.cpp

---
+++ 10xxx/10986.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(n, 0);
+  for (int i=0; i<n; i++) {
+    cin >> a[i];
+    a[i] %= m;
+  }
+
+  for (int i=1; i<n; i++) {
+    a[i] = (a[i-1]+a[i]) % m;
+  }
+
+  vector<long long> cnt(m, 0);
+  for (int i=0; i<n; i++) {
+    cnt[a[i]]++;
+  }
+
+  long long ans = cnt[0];
+  for (int i=0; i<m; i++) {
+    ans += cnt[i] * (cnt[i]-1) / 2;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
