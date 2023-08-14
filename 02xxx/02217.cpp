8ad89e0b854c9f1d0fa6dcc1634f22ca3cbb28f5
Hiyabye <hiyabye0465@gmail.com>
2023-08-14 15:04:24 +0900

    Add 02217.cpp

---
+++ 02xxx/02217.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+  sort(a.begin(), a.end());
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    ans = max(ans, a[i]*(n-i));
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
