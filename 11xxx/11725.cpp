4c1ef9bf68b0de93ea07a3e13af3f6cb77a54894
Hiyabye <hiyabye0465@gmail.com>
2023-08-14 14:56:33 +0900

    Add 11725.cpp

---
+++ 11xxx/11725.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, a, b;
+  cin >> n;
+
+  vector<int> adj[n+1];
+  for (int i=0; i<n-1; i++) {
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+
+  vector<int> ans(n+1, 0);
+  queue<int> q;
+  q.push(1);
+  while (!q.empty()) {
+    int cur = q.front();
+    q.pop();
+
+    for (int i=0; i<adj[cur].size(); i++) {
+      int p = adj[cur][i];
+      if (ans[p] == 0) {
+        ans[p] = cur;
+        q.push(p);
+      }
+    }
+  }
+
+  for (int i=2; i<=n; i++) {
+    cout << ans[i] << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
