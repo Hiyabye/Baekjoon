7ffb1048c3da376f7d92977d2d1cded5da61c49d
Hiyabye <hiyabye0465@gmail.com>
2023-08-14 14:49:44 +0900

    Add 10815.cpp

---
+++ 10xxx/10815.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, m, x;
+  map<int, bool> mp;
+
+  cin >> n;
+  for (int i=0; i<n; i++) {
+    cin >> x;
+    mp[x] = true;
+  }
+
+  cin >> m;
+  for (int i=0; i<m; i++) {
+    cin >> x;
+    cout << mp[x] << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
