611b4ae37fea9230d08dc09243533b4f71186f6f
Hiyabye <hiyabye0465@gmail.com>
2023-08-15 21:04:42 +0900

    Add 22251.cpp

---
+++ 22xxx/22251.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  // precompute
+  int num[10] = {0b1110111, 0b0010010, 0b1011101, 0b1011011, 0b0111010, 0b1101011, 0b1101111, 0b1010010, 0b1111111, 0b1111011};
+
+  // input
+  int n, k, p, x;
+  cin >> n >> k >> p >> x;
+
+  // calculate
+  int ans = 0;
+  for (int i=1; i<=n; i++) {
+    if (i == x) continue;
+    int cnt = 0, from = x, to = i;
+    for (int j=0; j<k; j++) {
+      cnt += __builtin_popcount(num[from%10] ^ num[to%10]);
+      from /= 10, to /= 10;
+    }
+    if (cnt <= p) ans++;
+  }
+
+  // output
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
