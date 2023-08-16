8ce0bc7caf4140ab7c60254d4d97d42a7229b151
Hiyabye <hiyabye0465@gmail.com>
2023-08-16 15:21:34 +0900

    Add 20363.cpp

---
+++ 20xxx/20363.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int x, y;
+  cin >> x >> y;
+  cout << x + y + min(x/10, y/10);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
