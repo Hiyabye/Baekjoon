fe88496d89ef281d7fea86e6a8d83bd8b99a59c0
Hiyabye <hiyabye0465@gmail.com>
2023-08-17 00:37:25 +0900

    Add 28701.cpp

---
+++ 28xxx/28701.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+
+  cout << n*(n+1)/2 << "\n";
+  cout << n*n*(n+1)*(n+1)/4 << "\n";
+  cout << n*n*(n+1)*(n+1)/4 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
