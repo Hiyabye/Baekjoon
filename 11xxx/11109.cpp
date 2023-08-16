fbec6f1fb0fcf31940e1995a44d0124f62533a8f
Hiyabye <hiyabye0465@gmail.com>
2023-08-16 15:01:59 +0900

    Add 11109.cpp

---
+++ 11xxx/11109.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int d, n, s, p;
+  cin >> d >> n >> s >> p;
+
+  if (d+n*p < n*s) cout << "parallelize\n";
+  else if (d+n*p > n*s) cout << "do not parallelize\n";
+  else cout << "does not matter\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
