3c5ca6df97ccd14e07fc486c00350f2a0030f698
Hiyabye <hiyabye0465@gmail.com>
2023-08-18 19:55:08 +0900

    Add 29198.cpp

---
+++ 29xxx/29198.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, m, k;
+  cin >> n >> m >> k;
+  vector<string> s(n);
+  for (int i=0; i<n; i++) {
+    cin >> s[i];
+    sort(s[i].begin(), s[i].end());
+  }
+  sort(s.begin(), s.end());
+
+  string t = "";
+  for (int i=0; i<k; i++) t += s[i];
+  sort(t.begin(), t.end());
+  cout << t;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
