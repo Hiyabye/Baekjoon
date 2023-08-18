3aed68991fa2130175f323cb2540f895069a4d5c
Hiyabye <hiyabye0465@gmail.com>
2023-08-18 18:38:38 +0900

    Add 01356.cpp

---
+++ 01xxx/01356.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(string a, string b) {
+  int c = 1, d = 1;
+  for (int i=0; i<a.length(); i++) c *= a[i]-'0';
+  for (int i=0; i<b.length(); i++) d *= b[i]-'0';
+  return c == d;
+}
+
+void solve(void) {
+  string n;
+  cin >> n;
+
+  for (int i=1; i<n.length(); i++) {
+    if (check(n.substr(0, i), n.substr(i, n.length()-i))) {
+      cout << "YES\n";
+      return;
+    }
+  }
+  cout << "NO\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
