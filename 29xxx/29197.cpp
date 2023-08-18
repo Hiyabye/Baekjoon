3b33f0a784efc68627a9f45a87fce0c01687e5b6
Hiyabye <hiyabye0465@gmail.com>
2023-08-18 19:50:34 +0900

    Add 29197.cpp

---
+++ 29xxx/29197.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+  return b ? gcd(b, a % b) : a;
+}
+
+void solve(void) {
+  int n, x, y;
+  cin >> n;
+
+  set<pair<int, int> > s;
+  for (int i=0; i<n; i++) {
+    cin >> x >> y;
+    if (x == 0) {
+      if (y > 0) s.insert(make_pair(0, 1));
+      else s.insert(make_pair(0, -1));
+    } else if (y == 0) {
+      if (x > 0) s.insert(make_pair(1, 0));
+      else s.insert(make_pair(-1, 0));
+    } else {
+      int g = gcd(abs(x), abs(y));
+      s.insert(make_pair(x/g, y/g));
+    }
+  }
+  cout << s.size();
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
