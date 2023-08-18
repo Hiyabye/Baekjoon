ecfdc0d4822a8a2bc1943c59d89e4c6bb5584e30
Hiyabye <hiyabye0465@gmail.com>
2023-08-18 18:06:50 +0900

    Add 13430.cpp

---
+++ 13xxx/13430.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+using namespace std;
+
+long long power(long long base, long long exp) {
+  if (exp == 0) return 1;
+  if (exp == 1) return base;
+  long long half = power(base, exp / 2);
+  if (exp % 2 == 0) return half * half % MOD;
+  else return (half * half % MOD) * base % MOD;
+}
+
+long long c(int k, int n) {
+  long long a = 1, b = 1;
+  for (int i=0; i<n; i++) {
+    a = a * (k-i) % MOD;
+    b = b * (i+1) % MOD;
+  }
+  return a * power(b, MOD-2) % MOD;
+}
+
+void solve(void) {
+  int k, n;
+  cin >> k >> n;
+  cout << c(k+n, k+1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
