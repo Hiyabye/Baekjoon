e564dd013408eb10ebebeeb81c4bfa14e7ed03a4
Hiyabye <hiyabye0465@gmail.com>
2023-08-19 20:13:44 +0900

    Add 14495.cpp

---
+++ 14xxx/14495.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  vector<long long> f(117);
+  f[1] = f[2] = f[3] = 1;
+  for (int i=4; i<=116; i++) f[i] = f[i-1] + f[i-3];
+
+  int n;
+  cin >> n;
+  cout << f[n];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
