5d89d0386a214bc893abada4c3d744cbe5c7873d
Hiyabye <hiyabye0465@gmail.com>
2023-08-19 19:27:22 +0900

    Add 01343.cpp

---
+++ 01xxx/01343.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(string s) {
+  int cnt = 0;
+  for (int i=0; i<s.length(); i++) {
+    if (s[i] == 'X') cnt++;
+    else if (cnt % 2) return false;
+    else cnt = 0;
+  }
+  return cnt % 2 == 0;
+}
+
+void cover(int &cnt) {
+  for (int i=0; i<cnt/4; i++) cout << "AAAA";
+  cnt %= 4;
+  for (int i=0; i<cnt/2; i++) cout << "BB";
+  cnt %= 2;
+}
+
+void solve(void) {
+  string s;
+  cin >> s;
+
+  if (!check(s)) {
+    cout << "-1";
+    return;
+  }
+
+  int cnt = 0;
+  for (int i=0; i<s.length(); i++) {
+    if (s[i] == 'X') cnt++;
+    else {
+      cover(cnt);
+      cout << ".";
+    }
+  }
+  cover(cnt);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
