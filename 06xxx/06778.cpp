556e76515ea68544fc9946d31fe4e5e60f3b5613
Hiyabye <hiyabye0465@gmail.com>
2023-08-23 22:06:04 +0900

    Add 06778.cpp

---
+++ 06xxx/06778.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int a, b;
+  cin >> a;
+  cin >> b;
+
+  if (a >= 3 && b <= 4) cout << "TroyMartian\n";
+  if (a <= 6 && b >= 2) cout << "VladSaturnian\n";
+  if (a <= 2 && b <= 3) cout << "GraemeMercurian\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
