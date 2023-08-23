6b56737b25b5f539e658973fff7f423acc2b0181
Hiyabye <hiyabye0465@gmail.com>
2023-08-23 22:02:31 +0900

    Add 06749.cpp

---
+++ 06xxx/06749.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int y, m;
+  cin >> y;
+  cin >> m;
+
+  cout << m*2-y;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
