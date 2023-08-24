ea29227499a7b56b2f4934aa203e5e7f49c31eef
Hiyabye <hiyabye0465@gmail.com>
2023-08-24 23:03:29 +0900

    Add 07891.cpp

---
+++ 07xxx/07891.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int a, b;
+  cin >> a >> b;
+  cout << a+b << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
