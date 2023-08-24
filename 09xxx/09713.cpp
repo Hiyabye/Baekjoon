491eb6eaa7411f0778fb8dac8c0e060ab6656d2a
Hiyabye <hiyabye0465@gmail.com>
2023-08-24 23:06:54 +0900

    Add 09713.cpp

---
+++ 09xxx/09713.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n;
+  cin >> n;
+  cout << (n+1)*(n+1)/4 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
