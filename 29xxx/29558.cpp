f0ab8f336e674d802a3b10440c528306a92ad6f5
Hiyabye <hiyabye0465@gmail.com>
2023-08-26 23:10:17 +0900

    Add 29558.cpp

---
+++ 29xxx/29558.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n, d;
+  cin >> n >> d;
+
+  if (n & 1) {
+    for (int i=d-(n-1)/2; i<=d+(n-1)/2; i++) cout << i << " ";
+  } else {
+    for (int i=d-n/2; i<=d-1; i++) cout << i << " ";
+    for (int i=d+1; i<=d+n/2; i++) cout << i << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
