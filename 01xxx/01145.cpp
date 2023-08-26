ce643770f8ef9a3372c73ffa494cd530c854f897
Hiyabye <hiyabye0465@gmail.com>
2023-08-26 23:56:03 +0900

    Add 01145.cpp

---
+++ 01xxx/01145.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+  return b ? gcd(b, a % b) : a;
+}
+
+int lcm(int a, int b) {
+  return a * b / gcd(a, b);
+}
+
+void solve(void) {
+  int a, b, c, d, e;
+  cin >> a >> b >> c >> d >> e;
+
+  int ans = lcm(lcm(a, b), c);
+  ans = min(ans, lcm(lcm(a, b), d));
+  ans = min(ans, lcm(lcm(a, b), e));
+  ans = min(ans, lcm(lcm(a, c), d));
+  ans = min(ans, lcm(lcm(a, c), e));
+  ans = min(ans, lcm(lcm(a, d), e));
+  ans = min(ans, lcm(lcm(b, c), d));
+  ans = min(ans, lcm(lcm(b, c), e));
+  ans = min(ans, lcm(lcm(b, d), e));
+  ans = min(ans, lcm(lcm(c, d), e));
+
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
