4ffb70e65b522bde699e431eb34b004102eb10ca
Hiyabye <hiyabye0465@gmail.com>
2023-08-27 21:53:56 +0900

    Add 10214.cpp

---
+++ 10xxx/10214.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int a = 0, b = 0;
+  for (int i=0; i<9; i++) {
+    int x, y; cin >> x >> y;
+    a += x; b += y;
+  }
+  cout << (a > b ? "Yonsei\n" : (a < b ? "Korea\n" : "Draw\n"));
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
