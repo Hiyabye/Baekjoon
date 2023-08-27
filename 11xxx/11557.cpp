db2ec605b6a28e81bd99014e3b2a6d7f9f3fc0f0
Hiyabye <hiyabye0465@gmail.com>
2023-08-27 21:56:44 +0900

    Add 11557.cpp

---
+++ 11xxx/11557.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<pair<int, string> > v(n);
+  for (int i=0; i<n; i++) cin >> v[i].second >> v[i].first;
+  sort(v.begin(), v.end(), greater<pair<int, string> >());
+  cout << v[0].second << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
