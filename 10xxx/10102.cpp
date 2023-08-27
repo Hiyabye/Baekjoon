2d82a81a0f81e70c01167413262df43fc21dbc7a
Hiyabye <hiyabye0465@gmail.com>
2023-08-27 21:42:38 +0900

    Add 10102.cpp

---
+++ 10xxx/10102.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int v; cin >> v;
+  string s; cin >> s;
+
+  int a = 0, b = 0;
+  for (int i=0; i<v; i++) {
+    if (s[i] == 'A') a++;
+    else b++;
+  }
+  cout << (a > b ? "A" : (a < b ? "B" : "Tie"));
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
