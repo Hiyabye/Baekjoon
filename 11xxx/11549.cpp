8fb6f66d3c404bbb511b9286f96c44c21bd184cd
Hiyabye <hiyabye0465@gmail.com>
2023-08-29 16:55:25 +0900

    Add 11549.cpp

---
+++ 11xxx/11549.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int t; cin >> t;
+  vector<int> a(5);
+  for (int i=0; i<5; i++) cin >> a[i];
+
+  int ans = 0;
+  for (int i=0; i<5; i++) {
+    if (a[i] == t) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
