4775390d7835069bcd1614855b53d7f83338f506
Hiyabye <hiyabye0465@gmail.com>
2023-08-30 16:24:20 +0900

    Add 02605.cpp

---
+++ 02xxx/02605.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a;
+
+  for (int i=1; i<=n; i++) {
+    int x; cin >> x;
+    a.insert(a.begin()+x, i);
+  }
+  reverse(a.begin(), a.end());
+
+  for (int i=0; i<a.size(); i++) {
+    cout << a[i] << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
