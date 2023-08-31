051e505216ac520949f04313b9a2de659a42a0ba
Hiyabye <hiyabye0465@gmail.com>
2023-08-31 15:34:11 +0900

    Add 27542.cpp

---
+++ 27xxx/27542.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  for (int i=0; i<n-1; i++) {
+    for (int j=0; j<n-i-1; j++) {
+      a[j] = abs(a[j] - a[j+1]);
+    }
+  }
+  cout << a[0];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
