4b9a439bd6a24155da5a7ff81a3ebcf8147b6cc9
Hiyabye <hiyabye0465@gmail.com>
2023-08-31 15:29:23 +0900

    Add 27541.cpp

---
+++ 27xxx/27541.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  string s; cin >> s;
+
+  if (s[n-1] == 'G') cout << s.substr(0, n-1);
+  else cout << s << 'G';
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
