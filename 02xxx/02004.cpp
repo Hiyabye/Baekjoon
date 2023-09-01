04dfe31e45ebaf761a23a4618939ad55a91cecdd
Hiyabye <hiyabye0465@gmail.com>
2023-09-01 16:15:06 +0900

    Add 02004.cpp

---
+++ 02xxx/02004.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long two(long long n) {
+  long long ret = 0;
+  for (long long i=2; i<=n; i*=2) ret += n/i;
+  return ret;
+}
+
+long long five(long long n) {
+  long long ret = 0;
+  for (long long i=5; i<=n; i*=5) ret += n/i;
+  return ret;
+}
+
+void solve(void) {
+  long long n, m;
+  cin >> n >> m;
+  
+  cout << min(two(n)-two(m)-two(n-m), five(n)-five(m)-five(n-m));
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
