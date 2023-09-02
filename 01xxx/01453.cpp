3228b9943682c2b8b79440d6619ab993710d56a4
Hiyabye <hiyabye0465@gmail.com>
2023-09-02 19:32:46 +0900

    Add 01453.cpp

---
+++ 01xxx/01453.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  vector<bool> v(101, true);
+  while (n--) {
+    int x; cin >> x;
+    if (v[x]) v[x] = false;
+    else ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
