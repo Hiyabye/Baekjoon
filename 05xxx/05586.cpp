6c8d2b484c06d4d7bddfc1a80b3d12194529f384
Hiyabye <hiyabye0465@gmail.com>
2023-09-02 19:35:06 +0900

    Add 05586.cpp

---
+++ 05xxx/05586.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  int a = 0, b = 0;
+  for (int i=0; i<s.length()-2; i++) {
+    if (s.substr(i, 3) == "JOI") a++;
+    if (s.substr(i, 3) == "IOI") b++;
+  }
+  cout << a << "\n" << b;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
