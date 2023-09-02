09ac6b476b5ac5e496c1a30f8e8c91faf2e1c4fd
Hiyabye <hiyabye0465@gmail.com>
2023-09-02 19:45:58 +0900

    Add 02495.cpp

---
+++ 02xxx/02495.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  int ans = 1, cnt = 1;
+  for (int i=1; i<s.length(); i++) {
+    if (s[i] == s[i-1]) cnt++;
+    else cnt = 1;
+    ans = max(ans, cnt);
+  }
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  for (int i=0; i<3; i++) solve();
+  return 0;
+}
