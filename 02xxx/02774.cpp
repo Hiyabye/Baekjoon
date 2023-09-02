5cc0afc813d88c4177cd39829b37cf4a1c8517d6
Hiyabye <hiyabye0465@gmail.com>
2023-09-02 19:29:17 +0900

    Add 02774.cpp

---
+++ 02xxx/02774.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int x;
+  cin >> x;
+
+  vector<bool> v(10, false);
+  while (x) {
+    v[x%10] = true;
+    x /= 10;
+  }
+
+  int ans = 0;
+  for (int i=0; i<10; i++) {
+    if (v[i]) ans++;
+  }
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
