6d6fac27f5d67007769c4df133ae9e9592c5090f
Hiyabye <hiyabye0465@gmail.com>
2023-09-02 18:09:16 +0900

    Add 13305.cpp

---
+++ 13xxx/13305.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n-1), b(n);
+  for (int i=0; i<n-1; i++) cin >> a[i];
+  for (int i=0; i<n; i++) cin >> b[i];
+
+  long long ans = 0, min = b[0];
+  for (int i=0; i<n-1; i++) {
+    if (min > b[i]) min = b[i];
+    ans += min * a[i];
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
