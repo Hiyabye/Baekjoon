1dfac4e8fb4252c0d901b02f96059ba26014b4fd
Hiyabye <hiyabye0465@gmail.com>
2023-09-03 22:40:45 +0900

    Add 02631.cpp

---
+++ 02xxx/02631.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  vector<int> dp(n, 1);
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<i; j++) {
+      if (a[j] < a[i]) dp[i] = max(dp[i], dp[j]+1);
+    }
+  }
+  cout << n - *max_element(dp.begin(), dp.end()) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
