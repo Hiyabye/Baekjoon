bf6ddc825bc2cd139999fb01567e4ab30e1b35e5
Hiyabye <hiyabye0465@gmail.com>
2023-09-03 22:47:11 +0900

    Add 02810.cpp

---
+++ 02xxx/02810.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  string s; cin >> s;
+
+  int ans = 1;
+  for (int i=0; i<n; i++) {
+    if (s[i] == 'L') i++;
+    ans++;
+  }
+  cout << min(ans, n);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
