d17af7c9823c8a59d611a79944237f790159dce9
Hiyabye <hiyabye0465@gmail.com>
2023-09-05 19:49:35 +0900

    Add 10707.cpp

---
+++ 10xxx/10707.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int a, b, c, d, p;
+  cin >> a;
+  cin >> b;
+  cin >> c;
+  cin >> d;
+  cin >> p;
+
+  cout << min(a*p, b+max(0, p-c)*d);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
