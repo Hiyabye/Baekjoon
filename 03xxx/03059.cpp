671a71e260d50496826a59ac473257c7cda8a571
Hiyabye <hiyabye0465@gmail.com>
2023-09-06 23:45:25 +0900

    Add 03059.cpp

---
+++ 03xxx/03059.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  vector<bool> a(26, false);
+  for (int i=0; i<s.length(); i++) a[s[i]-'A'] = true;
+
+  int ans = 0;
+  for (int i=0; i<26; i++) if (!a[i]) ans += 'A'+i;
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
