58cf1d04f5a5bb078947235e05f4c5927d6402f7
Hiyabye <hiyabye0465@gmail.com>
2023-09-06 23:34:50 +0900

    Add 02789.cpp

---
+++ 02xxx/02789.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  for (int i=0; i<s.length(); i++) {
+    bool flag = true;
+    for (int j=0; j<9; j++) {
+      if (s[i] == "CAMBRIDGE"[j]) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) cout << s[i];
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
