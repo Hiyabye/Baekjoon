9f70cdada45c0f7a285f882a8c87be243d16d19b
Hiyabye <hiyabye0465@gmail.com>
2023-09-06 23:38:53 +0900

    Add 04470.cpp

---
+++ 04xxx/04470.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int idx) {
+  string s; getline(cin, s);
+  cout << idx << ". " << s << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  cin.ignore();
+  for (int i=1; i<=n; i++) solve(i);
+  return 0;
+}
