78a40911097db44978c6ba6cb5dcd3530ad694e5
Hiyabye <hiyabye0465@gmail.com>
2023-09-08 12:06:42 +0900

    Add 09742.cpp

---
+++ 09xxx/09742.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> fact(11, 1);
+
+void solve(string s, int n) {
+  cout << s << " " << n << " = ";
+  sort(s.begin(), s.end());
+
+  if (n > fact[s.length()]) cout << "No permutation\n";
+  else {
+    while (--n) next_permutation(s.begin(), s.end());
+    cout << s << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  for (int i=2; i<=10; i++) fact[i] = fact[i-1] * i;
+
+  string s; int n;
+  while (cin >> s >> n) solve(s, n);
+  return 0;
+}
