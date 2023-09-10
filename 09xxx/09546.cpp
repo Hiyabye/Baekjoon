a6e3e525b80db66505b261edbd508a1aeed47bb5
Hiyabye <hiyabye0465@gmail.com>
2023-09-10 20:51:26 +0900

    Add 09546.cpp

---
+++ 09xxx/09546.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int k; cin >> k;
+  
+  cout << (1<<k)-1 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
