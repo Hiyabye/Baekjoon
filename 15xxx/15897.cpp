ab9ecc9c7d37b78efec1cee4187512e1b6fc74c7
Hiyabye <hiyabye0465@gmail.com>
2023-09-11 21:57:15 +0900

    Add 15897.cpp

---
+++ 15xxx/15897.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  if (n == 1) {
+    cout << "1";
+    return;
+  }
+
+  long long ans = n;
+  int mx = sqrt(n-1);
+  for (int i=1; i<mx; i++) ans += (n-1)/i;
+  for (int i=(n-1)/mx; i>=1; i--) ans += i*((n-1)/i-(n-1)/(i+1));
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
