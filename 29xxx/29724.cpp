17bb05b205aca6eda05969a1889f073ec719b7e4
Hiyabye <hiyabye0465@gmail.com>
2023-09-12 03:17:21 +0900

    Add 29724.cpp

---
+++ 29xxx/29724.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  pair<int, int> ans = make_pair(0, 0);
+  while (n--) {
+    char t; int w, h, l; cin >> t >> w >> h >> l;
+    if (t == 'A') {
+      ans.first += 1000+(w/12)*(h/12)*(l/12)*500;
+      ans.second += (w/12)*(h/12)*(l/12)*4000;
+    } else ans.first += 6000;
+  }
+  cout << ans.first << "\n" << ans.second;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
