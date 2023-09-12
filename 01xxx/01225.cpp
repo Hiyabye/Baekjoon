b15049b73148b925992d703ce9b8d83c6b1ac5bc
Hiyabye <hiyabye0465@gmail.com>
2023-09-12 09:55:17 +0900

    Add 01225.cpp

---
+++ 01xxx/01225.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string a, b; cin >> a >> b;
+
+  long long ans = 0;
+  for (int i=0; i<a.length(); i++) {
+    for (int j=0; j<b.length(); j++) {
+      ans += (a[i]-'0') * (b[j]-'0');
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
