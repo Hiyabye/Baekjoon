4ec21f95e40dcfe2bd65776b74a4f7973ff8ebc2
Hiyabye <hiyabye0465@gmail.com>
2023-09-13 14:48:05 +0900

    Add 02910.cpp

---
+++ 02xxx/02910.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool cmp(pair<int, pair<int, int> > &a, pair<int, pair<int, int> > &b) {
+  if (a.second.first == b.second.first) return a.second.second < b.second.second;
+  return a.second.first > b.second.first;
+}
+
+void solve(void) {
+  int n, c; cin >> n >> c;
+
+  map<int, pair<int, int> > mp;
+  for (int i=0; i<n; i++) {
+    int x; cin >> x;
+    if (mp.find(x) == mp.end()) mp[x] = make_pair(1, i);
+    else mp[x].first++;
+  }
+
+  vector<pair<int, pair<int, int> > > v;
+  for (auto it=mp.begin(); it!=mp.end(); it++) {
+    v.push_back(make_pair(it->first, make_pair(it->second.first, it->second.second)));
+  }
+  sort(v.begin(), v.end(), cmp);
+
+  for (int i=0; i<v.size(); i++) {
+    for (int j=0; j<v[i].second.first; j++) cout << v[i].first << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
