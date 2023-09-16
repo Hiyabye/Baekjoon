c9ceb50b0219d5b9ebc25a9c0201dd2cafad29bb
Hiyabye <hiyabye0465@gmail.com>
2023-09-16 12:57:43 +0900

    Add 01213.cpp

---
+++ 01xxx/01213.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+  vector<int> a(26, 0);
+  for (int i=0; i<s.length(); i++) a[s[i]-'A']++;
+
+  int odd = -1;
+  for (int i=0; i<26; i++) {
+    if (a[i] & 1) {
+      if (odd != -1) {
+        cout << "I'm Sorry Hansoo";
+        return;
+      }
+      odd = i;
+    }
+  }
+
+  string ans = "";
+  for (int i=0; i<26; i++) {
+    for (int j=0; j<a[i]/2; j++) {
+      ans += (char)('A'+i);
+    }
+  }
+  if (odd != -1) ans += (char)('A'+odd);
+  for (int i=25; i>=0; i--) {
+    for (int j=0; j<a[i]/2; j++) {
+      ans += (char)('A'+i);
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
