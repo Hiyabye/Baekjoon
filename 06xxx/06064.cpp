dd0534a0c131082ceb1f63fa2416eb44836c6232
Hiyabye <hiyabye0465@gmail.com>
2023-09-18 00:26:45 +0900

    Add 06064.cpp

---
+++ 06xxx/06064.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int m, n, x, y; cin >> m >> n >> x >> y;
+
+  int ans = -1;
+  for (int i=x; i<=m*n; i+=m) {
+    if ((i-y) % n == 0) {
+      ans = i;
+      break;
+    }
+  }
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
