8fee3f66a9e0645e8b80113eb8fa8872e143f608
Hiyabye <hiyabye0465@gmail.com>
2023-09-18 15:32:29 +0900

    Add 14918.cpp

---
+++ 14xxx/14918.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+  cout << a + b << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
