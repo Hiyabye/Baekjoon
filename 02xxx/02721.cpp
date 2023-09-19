feb095677c2e57dbf86952fbef552f6a67e9e1fa
Hiyabye <hiyabye0465@gmail.com>
2023-09-19 19:07:25 +0900

    Add 02721.cpp

---
+++ 02xxx/02721.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int t(int n) {
+  return n*(n+1)/2;
+}
+
+int w(int n) {
+  int sum = 0;
+  for (int i=1; i<=n; i++) sum += i*t(i+1);
+  return sum;
+}
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << w(n) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
