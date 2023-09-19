598cd792a3cdced9df75b67270feef3ff73dce5d
Hiyabye <hiyabye0465@gmail.com>
2023-09-19 19:05:27 +0900

    Add 30017.cpp

---
+++ 30xxx/30017.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+
+  int ans = 1; a--;
+  while (a > 0 && b > 0) {
+    ans += 2;
+    a--; b--;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
