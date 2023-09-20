0dbb5886af23f195abdd2df2639df79fe773926c
Hiyabye <hiyabye0465@gmail.com>
2023-09-20 22:05:46 +0900

    Add 15802.cpp

---
+++ 15xxx/15802.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "0";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
