50a074f5b7ced91c7ff9d2ddbb92966f6e1a6d7f
Hiyabye <hiyabye0465@gmail.com>
2023-09-20 22:14:01 +0900

    Add 15000.cpp

---
+++ 15xxx/15000.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+  for (int i=0; i<s.length(); i++) cout << (char)(s[i]-'a'+'A');
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
