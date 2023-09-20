7f820027dca93050098c1b68d5decd86d495dcc6
Hiyabye <hiyabye0465@gmail.com>
2023-09-20 16:00:35 +0900

    Add 09783.cpp

---
+++ 09xxx/09783.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void solve(void) {
+  string s; getline(cin, s);
+
+  for (int i=0; i<s.length(); i++) {
+    if (s[i] >= 'a' && s[i] <= 'z') cout << setw(2) << setfill('0') << s[i]-'a'+1;
+    else if (s[i] >= 'A' && s[i] <= 'Z') cout << s[i]-'A'+27;
+    else if (s[i] >= '0' && s[i] <= '9') cout << "#" << s[i];
+    else cout << s[i];
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
