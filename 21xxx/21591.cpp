48b2adcab4ca2e77adafa28b26bcb5b90800691e
Hiyabye <hiyabye0465@gmail.com>
2023-09-20 10:27:18 +0900

    Add 21591.cpp

---
+++ 21xxx/21591.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(void) {
+  int wc, hc, ws, hs; cin >> wc >> hc >> ws >> hs;
+
+  cout << (ws+2 <= wc && hs+2 <= hc ? "1" : "0");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
