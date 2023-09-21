a845632030801c270bc6753b481f489671f09598
Hiyabye <hiyabye0465@gmail.com>
2023-09-21 12:57:31 +0900

    Add 01312.cpp

---
+++ 01xxx/01312.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, n; cin >> a >> b >> n;
+
+  for (int i=0; i<n; i++) {
+    a %= b;
+    a *= 10;
+  }
+  cout << a / b;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
