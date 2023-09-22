5eb0a402756ecf3d8bdb82aece8bbbd583f4651b
Hiyabye <hiyabye0465@gmail.com>
2023-09-22 11:20:25 +0900

    Add 01964.cpp

---
+++ 01xxx/01964.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define MOD 45678
+using namespace std;
+
+void solve(void) {
+  long long n; cin >> n;
+
+  cout << ((3*n*n + 5*n) / 2 + 1) % MOD;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
