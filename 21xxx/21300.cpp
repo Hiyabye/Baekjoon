dd859192dcdfeb6c57765f2812b7ea81d2d9fb6b
Hiyabye <hiyabye0465@gmail.com>
2023-09-23 23:14:22 +0900

    Add 21300.cpp

---
+++ 21xxx/21300.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int sum = 0;
+  for (int i=0; i<6; i++) {
+    int x; cin >> x;
+    sum += x;
+  }
+  
+  cout << sum * 5;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
