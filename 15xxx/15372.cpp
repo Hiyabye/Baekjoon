4177524e7ce07e045ddaa43edbab034ea7b459bd
Hiyabye <hiyabye0465@gmail.com>
2023-09-25 10:21:07 +0900

    Add 15372.cpp

---
+++ 15xxx/15372.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  long long n; cin >> n;
+
+  cout << n * n << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
