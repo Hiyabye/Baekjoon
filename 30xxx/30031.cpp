0988f7446a9092dfcd3dc37e384d5dda28cc3f4a
Hiyabye <hiyabye0465@gmail.com>
2023-09-26 19:01:47 +0900

    Add 30031.cpp

---
+++ 30xxx/30031.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    int x, y; cin >> x >> y;
+    if (x == 136) ans += 1000;
+    else if (x == 142) ans += 5000;
+    else if (x == 148) ans += 10000;
+    else if (x == 154) ans += 50000;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
