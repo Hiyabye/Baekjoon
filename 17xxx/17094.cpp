82c5ec3cbdfdba3f1e995f28266b4c4e58589ba1
Hiyabye <hiyabye0465@gmail.com>
2023-09-27 15:20:23 +0900

    Add 17094.cpp

---
+++ 17xxx/17094.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  string s; cin >> s;
+
+  int a = 0, b = 0;
+  for (int i=0; i<n; i++) {
+    if (s[i] == '2') a++;
+    else b++;
+  }
+  cout << (a > b ? "2" : (a < b ? "e" : "yee"));
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
