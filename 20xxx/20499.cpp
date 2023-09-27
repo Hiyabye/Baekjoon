afbe83da553f3b4753e1bfe53c114e6bf55010d2
Hiyabye <hiyabye0465@gmail.com>
2023-09-27 23:46:48 +0900

    Add 20499.cpp

---
+++ 20xxx/20499.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int k, d, a; char tmp; cin >> k >> tmp >> d >> tmp >> a;
+
+  if (k + a < d || d == 0) cout << "hasu";
+  else cout << "gosu";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
