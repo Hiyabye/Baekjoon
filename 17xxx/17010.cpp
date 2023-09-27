bf9e6e0a7f13bee0651e1e1fd6dffeb09be98b4d
Hiyabye <hiyabye0465@gmail.com>
2023-09-27 15:08:21 +0900

    Add 17010.cpp

---
+++ 17xxx/17010.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int l; cin >> l;
+
+  while (l--) {
+    int n; char c; cin >> n >> c;
+    while (n--) cout << c;
+    cout << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
