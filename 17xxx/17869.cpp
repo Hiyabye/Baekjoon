1861c824882951f4f8dbb896507a45217d9e3e7f
Hiyabye <hiyabye0465@gmail.com>
2023-09-27 23:57:12 +0900

    Add 17869.cpp

---
+++ 17xxx/17869.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  long long n; cin >> n;
+
+  int ans = 0;
+  while (n > 1) {
+    if (n & 1) n++;
+    else n /= 2;
+    ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
