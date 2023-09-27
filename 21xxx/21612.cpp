7312fe1a00ef3567c61d5f36a77fbb3cb317490e
Hiyabye <hiyabye0465@gmail.com>
2023-09-27 23:43:01 +0900

    Add 21612.cpp

---
+++ 21xxx/21612.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int b; cin >> b;
+
+  cout << 5*b - 400 << "\n";
+  if (5*b - 400 > 100) cout << "-1";
+  else if (5*b - 400 < 100) cout << "1";
+  else cout << "0";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
