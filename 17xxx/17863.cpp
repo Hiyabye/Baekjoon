38d8c126831fb4843a114650922f45350cc1e648
Hiyabye <hiyabye0465@gmail.com>
2023-09-27 23:52:52 +0900

    Add 17863.cpp

---
+++ 17xxx/17863.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << (n / 10000 == 555 ? "YES" : "NO");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
