782e2c9a8efe30075941228055a632ec315abbf7
Hiyabye <hiyabye0465@gmail.com>
2023-09-28 20:11:16 +0900

    Add 30007.cpp

---
+++ 30xxx/30007.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, x; cin >> a >> b >> x;
+
+  cout << a*(x-1)+b << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
