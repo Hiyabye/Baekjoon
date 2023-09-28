df9d5dfc4e0389dbfec95295030677fe674cabf4
Hiyabye <hiyabye0465@gmail.com>
2023-09-28 22:43:43 +0900

    Add 14492.cpp

---
+++ 14xxx/14492.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<vector<int> > a(n, vector<int>(n)), b(n, vector<int>(n));
+  for (int i=0; i<n; i++) for (int j=0; j<n; j++) cin >> a[i][j];
+  for (int i=0; i<n; i++) for (int j=0; j<n; j++) cin >> b[i][j];
+
+  vector<vector<int> > c(n, vector<int>(n, 0));
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+      for (int k=0; k<n; k++) {
+        c[i][j] |= a[i][k] & b[k][j];
+      }
+    }
+  }
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+      if (c[i][j]) ans++;
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
