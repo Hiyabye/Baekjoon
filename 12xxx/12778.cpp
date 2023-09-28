8dd648176202d5166a607756b576c7d09796c008
Hiyabye <hiyabye0465@gmail.com>
2023-09-28 22:15:17 +0900

    Add 12778.cpp

---
+++ 12xxx/12778.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int m; char c; cin >> m >> c;
+
+  if (c == 'C') {
+    vector<char> v(m);
+    for (int i=0; i<m; i++) cin >> v[i];
+    for (int i=0; i<m; i++) cout << (int)(v[i]-64) << " ";
+  } else if (c == 'N') {
+    vector<int> v(m);
+    for (int i=0; i<m; i++) cin >> v[i];
+    for (int i=0; i<m; i++) cout << (char)(v[i]+64) << " ";
+  }
+  cout << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
