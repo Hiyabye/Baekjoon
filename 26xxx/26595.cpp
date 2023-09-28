7a1ae2c7419ded075ae8cea051d4b0dcb85c4502
Hiyabye <hiyabye0465@gmail.com>
2023-09-28 13:48:15 +0900

    Add 26595.cpp

---
+++ 26xxx/26595.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  long long a, pa, b, pb; cin >> a >> pa >> b >> pb;
+
+  long long x = 0, y = 0, mx = 0;
+  for (long long i=0, j=n/pb; i<=n/pa; i++, j=(n-i*pa)/pb) {
+    if (j < 0) break;
+    if (a*i + b*j > mx) {
+      mx = a*i + b*j;
+      x = i; y = j;
+    }
+  }
+  cout << x << " " << y;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
