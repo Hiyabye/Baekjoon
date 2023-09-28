26307e06765286ac86de4992b4df3e65ed656386
Hiyabye <hiyabye0465@gmail.com>
2023-09-28 17:31:54 +0900

    Add 02877.cpp

---
+++ 02xxx/02877.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int k; cin >> k;
+  k++;
+
+  vector<int> binary;
+  while (k > 0) {
+    binary.push_back(k & 1);
+    k >>= 1;
+  }
+  reverse(binary.begin(), binary.end());
+
+  for (int i=1; i<binary.size(); i++) {
+    cout << (binary[i] ? 7 : 4);
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
