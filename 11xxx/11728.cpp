c3b8e7808ff8156158877486ba17bee37821e2ff
Hiyabye <hiyabye0465@gmail.com>
2023-09-29 21:26:30 +0900

    Add 11728.cpp

---
+++ 11xxx/11728.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  vector<int> v(n+m);
+  for (int i=0; i<n+m; i++) cin >> v[i];
+  sort(v.begin(), v.end());
+
+  for (int i=0; i<n+m; i++) cout << v[i] << " ";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
