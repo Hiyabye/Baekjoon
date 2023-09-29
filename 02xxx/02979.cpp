ff9832c2d14129c2f60e78b8675083b29a0a2a40
Hiyabye <hiyabye0465@gmail.com>
2023-09-29 11:09:36 +0900

    Add 02979.cpp

---
+++ 02xxx/02979.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int a, b, c; cin >> a >> b >> c;
+
+  vector<int> v(101, 0);
+  for (int i=0; i<3; i++) {
+    int x, y; cin >> x >> y;
+    for (int j=x; j<y; j++) v[j]++;
+  }
+
+  int ans = 0;
+  for (int i=1; i<=100; i++) {
+    if (v[i] == 1) ans += a;
+    else if (v[i] == 2) ans += b*2;
+    else if (v[i] == 3) ans += c*3;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
