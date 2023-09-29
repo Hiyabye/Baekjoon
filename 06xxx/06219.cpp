73a516afd13337c6183de15efd6eb8e9db2d7b62
Hiyabye <hiyabye0465@gmail.com>
2023-09-29 21:01:26 +0900

    Add 06219.cpp

---
+++ 06xxx/06219.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<bool> prime(4000001, true);
+
+void precompute(void) {
+  prime[0] = prime[1] = false;
+  for (int i=4; i<=4000000; i+=2) {
+    prime[i] = false;
+  }
+
+  for (int i=3; i*i<=4000000; i+=2) {
+    if (!prime[i]) continue;
+    for (int j=i*i; j<=4000000; j+=i*2) {
+      prime[j] = false;
+    }
+  }
+}
+
+int count(int n, int d) {
+  int cnt = 0;
+  while (n) {
+    if (n % 10 == d) cnt++;
+    n /= 10;
+  }
+  return cnt;
+}
+
+void solve(void) {
+  int a, b, d; cin >> a >> b >> d;
+
+  int ans = 0;
+  for (int i=a; i<=b; i++) {
+    if (prime[i] && count(i, d)) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  precompute();
+  solve();
+  return 0;
+}
