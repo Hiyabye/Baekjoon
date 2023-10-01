7873c6f30536bd23bd74bc6dd352e36f4467fc20
Hiyabye <hiyabye0465@gmail.com>
2023-10-02 00:28:51 +0900

    Add 10163.cpp

---
+++ 10xxx/10163.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  vector<vector<int> > v(1001, vector<int>(1001, 0));
+  for (int i=1; i<=n; i++) {
+    int a, b, w, h; cin >> a >> b >> w >> h;
+    for (int j=a; j<a+w; j++) {
+      for (int k=b; k<b+h; k++) {
+        v[j][k] = i;
+      }
+    }
+  }
+
+  vector<int> cnt(n+1, 0);
+  for (int i=0; i<1001; i++) {
+    for (int j=0; j<1001; j++) {
+      cnt[v[i][j]]++;
+    }
+  }
+
+  for (int i=1; i<=n; i++) {
+    cout << cnt[i] << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
