d62df4aae143fa7dfe167fe093e4faffc1abff37
Hiyabye <hiyabye0465@gmail.com>
2023-10-01 13:06:20 +0900

    Add 11023.cpp

---
+++ 11xxx/11023.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int ans = 0, n;
+  
+  while (cin >> n) ans += n;
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
