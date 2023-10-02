c59f2b9a02084ce7dfdc73606c7dfec6a33506ae
Hiyabye <hiyabye0465@gmail.com>
2023-10-03 00:41:02 +0900

    Add 02502.cpp

---
+++ 02xxx/02502.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<pair<int, int> > v(31);
+
+void precompute(void) {
+  v[1] = make_pair(1, 0);
+  v[2] = make_pair(0, 1);
+  for (int i=3; i<=30; i++) {
+    v[i].first = v[i-1].first + v[i-2].first;
+    v[i].second = v[i-1].second + v[i-2].second;
+  }
+}
+
+void solve(void) {
+  int d, k; cin >> d >> k;
+
+  int b = 1;
+  while (1) {
+    for (int a=1; a<=b; a++) {
+      if (a*v[d].first + b*v[d].second == k) {
+        cout << a << "\n" << b;
+        return;
+      }
+    }
+    b++;
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  precompute();
+  solve();
+  return 0;
+}
