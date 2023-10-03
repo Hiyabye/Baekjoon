9a1ae778603f4fdf7e34c4705782223135dbfde5
Hiyabye <hiyabye0465@gmail.com>
2023-10-03 13:32:38 +0900

    Add 04880.cpp

---
+++ 04xxx/04880.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+bool solve(void) {
+  int a, b, c; cin >> a >> b >> c;
+  if (a == 0 && b == 0 && c == 0) return false;
+
+  if (b-a == c-b) cout << "AP " << c+(b-a) << "\n";
+  else cout << "GP " << c*(b/a) << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  
+  while (solve());
+  return 0;
+}
