1d888a68c66c7f6034c52677543257e466e4d7b3
Hiyabye <hiyabye0465@gmail.com>
2023-10-05 22:38:39 +0900

    Add 09094.cpp

---
+++ 09xxx/09094.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+
+  int ans = 0;
+  for (int a=1; a<n-1; a++) {
+    for (int b=a+1; b<n; b++) {
+      if ((a*a+b*b+m) % (a*b) == 0) ans++;
+    }
+  }
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
