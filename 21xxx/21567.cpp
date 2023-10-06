47ba5e684b3cc0d9fc6cfbba039b279d1f9245c4
Hiyabye <hiyabye0465@gmail.com>
2023-10-06 11:20:18 +0900

    Add 21567.cpp

---
+++ 21xxx/21567.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  long long a; cin >> a;
+  long long b; cin >> b;
+  long long c; cin >> c;
+
+  long long d = a * b * c;
+  vector<int> cnt(10, 0);
+  while (d) {
+    cnt[d % 10]++;
+    d /= 10;
+  }
+
+  for (int i=0; i<10; i++) {
+    cout << cnt[i] << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
