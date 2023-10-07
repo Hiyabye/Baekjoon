d7c1c1676344eaafdf803681e901a30d9fce217e
Hiyabye <hiyabye0465@gmail.com>
2023-10-07 18:02:10 +0900

    Add 30204.cpp

---
+++ 30xxx/30204.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n, x; cin >> n >> x;
+
+  int sum = 0;
+  for (int i=0; i<n; i++) {
+    int p; cin >> p;
+    sum += p;
+  }
+  cout << (sum % x == 0 ? "1" : "0");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
