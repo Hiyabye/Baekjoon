2568fbe8acbac28fe2fa5fca70601c8773b15196
Hiyabye <hiyabye0465@gmail.com>
2023-10-07 18:14:52 +0900

    Add 30205.cpp

---
+++ 30xxx/30205.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  long long n, m, p; cin >> n >> m >> p;
+  vector<vector<long long> > s(n, vector<long long>(m));
+  for (int i=0; i<n; i++) for (int j=0; j<m; j++) cin >> s[i][j];
+
+  for (int i=0; i<n; i++) {
+    sort(s[i].begin(), s[i].end());
+
+    int item = 0;
+    for (int j=0; j<m; j++) {
+      if (s[i][j] == -1) {
+        item++;
+        continue;
+      }
+      while (item > 0 && p < s[i][j]) {
+        item--;
+        p <<= 1;
+      }
+      if (p < s[i][j]) {
+        cout << "0";
+        return;
+      }
+      p += s[i][j];
+    }
+    while (item > 0) {
+      item--;
+      p <<= 1;
+    }
+  }
+  cout << "1";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
