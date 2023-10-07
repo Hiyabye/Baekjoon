3940ea1dfd946302e806e71cf225a46d5957f092
Hiyabye <hiyabye0465@gmail.com>
2023-10-07 10:44:13 +0900

    Add 02947.cpp

---
+++ 02xxx/02947.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  vector<int> a(5);
+  for (int i=0; i<5; i++) cin >> a[i];
+
+  for (int i=0; i<5; i++) {
+    for (int i=0; i<4; i++) {
+      if (a[i] > a[i+1]) {
+        swap(a[i], a[i+1]);
+        for (int j=0; j<5; j++) cout << a[j] << " ";
+        cout << "\n";;
+      }
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
