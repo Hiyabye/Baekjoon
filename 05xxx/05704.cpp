5df7b278e0f73b10dca3b24f146f523d424a4e88
Hiyabye <hiyabye0465@gmail.com>
2023-10-09 15:15:20 +0900

    Add 05704.cpp

---
+++ 05xxx/05704.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool solve(void) {
+  string s; getline(cin, s);
+  if (s == "*") return false;
+
+  int n = s.length();
+  vector<bool> v(26, false);
+  for (int i=0; i<n; i++) {
+    if (s[i] == ' ') continue;
+    v[s[i]-'a'] = true;
+  }
+
+  bool ans = true;
+  for (int i=0; i<26; i++) {
+    if (!v[i]) ans = false;
+  }
+  cout << (ans ? "Y" : "N") << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  while (solve());
+  return 0;
+}
