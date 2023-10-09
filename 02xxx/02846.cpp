3baef62f366e7e975a31b8143b3661e83314d788
Hiyabye <hiyabye0465@gmail.com>
2023-10-09 13:21:20 +0900

    Add 02846.cpp

---
+++ 02xxx/02846.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> p(n);
+  for (int i=0; i<n; i++) cin >> p[i];
+
+  int ans = 0, cur = 0;
+  for (int i=0; i<n-1; i++) {
+    if (p[i+1] > p[i]) cur += p[i+1] - p[i];
+    else cur = 0;
+    ans = max(ans, cur);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
