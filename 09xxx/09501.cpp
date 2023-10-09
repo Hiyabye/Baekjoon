f27b0995e83aeb4f6bbfaed82cc680dd834fbb96
Hiyabye <hiyabye0465@gmail.com>
2023-10-09 15:18:47 +0900

    Add 09501.cpp

---
+++ 09xxx/09501.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, d; cin >> n >> d;
+  vector<int> v(n), f(n), c(n);
+  for (int i=0; i<n; i++) cin >> v[i] >> f[i] >> c[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    if (v[i] * f[i] >= c[i] * d) ans++;
+  }
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
