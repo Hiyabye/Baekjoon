5ba595488bdb3c49887615c15b62fb38e5f97feb
Hiyabye <hiyabye0465@gmail.com>
2023-10-09 15:25:19 +0900

    Add 09469.cpp

---
+++ 09xxx/09469.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void solve(void) {
+  int n; double d, a, b, f;
+  cin >> n >> d >> a >> b >> f;
+
+  cout << n << " " << fixed << setprecision(6) << d/(a+b)*f << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int p; cin >> p;
+  while (p--) solve();
+  return 0;
+}
