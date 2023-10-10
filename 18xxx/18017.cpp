fcd7cd170a1c11660af82ec066b67b10aa05a38c
Hiyabye <hiyabye0465@gmail.com>
2023-10-10 21:21:42 +0900

    Add 18017.cpp

---
+++ 18xxx/18017.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void solve(void) {
+  double a, b; cin >> a >> b;
+  double c = 299792458.0;
+  
+  cout << fixed << setprecision(10) << (a+b)/(1+a*b/c/c);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
