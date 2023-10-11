3033d19358328f0ea58cfe20da48c6f9c6a9ee71
Hiyabye <hiyabye0465@gmail.com>
2023-10-11 09:39:29 +0900

    Add 16199.cpp

---
+++ 16xxx/16199.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int by, bm, bd; cin >> by >> bm >> bd;
+  int cy, cm, cd; cin >> cy >> cm >> cd;
+
+  cout << (cm < bm || (cm == bm && cd < bd) ? cy-by-1 : cy-by) << "\n";
+  cout << cy-by+1 << "\n";
+  cout << cy-by;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
