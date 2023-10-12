cfadb7bee19c500e336de2782825081b985e3825
Hiyabye <hiyabye0465@gmail.com>
2023-10-12 16:44:23 +0900

    Add 02089.cpp

---
+++ 02xxx/02089.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  if (n == 0) {
+    cout << 0;
+    return;
+  }
+
+  string ans = "";
+  while (n != 0) {
+    if (n % -2) {
+      ans += '1';
+      n--;
+    } else {
+      ans += '0';
+    }
+    n /= -2;
+  }
+  reverse(ans.begin(), ans.end());
+  
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
