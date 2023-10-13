991e41ed3a3ec9bff83b7d189735c84414a483ef
Hiyabye <hiyabye0465@gmail.com>
2023-10-14 00:03:33 +0900

    Add 14910.cpp

---
+++ 14xxx/14910.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  bool ans = true;
+
+  int prev = -1000000, cur = 0;
+  while (cin >> cur) {
+    if (prev > cur) ans = false;
+    prev = cur;
+  }
+  cout << (ans ? "Good" : "Bad");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
