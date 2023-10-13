a108a622c83f72cc6a5d31d6f011f3f5493a4039
Hiyabye <hiyabye0465@gmail.com>
2023-10-13 23:44:58 +0900

    Add 11586.cpp

---
+++ 11xxx/11586.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<string> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+  int k; cin >> k;
+
+  switch (k) {
+    case 1: for (int i=0; i<n; i++) cout << v[i] << "\n"; break;
+    case 2: for (int i=0; i<n; i++) {
+              for (int j=n-1; j>=0; j--) cout << v[i][j];
+              cout << "\n";
+            } break;
+    case 3: for (int i=n-1; i>=0; i--) cout << v[i] << "\n"; break;
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
