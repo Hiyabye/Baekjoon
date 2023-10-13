c9ea0785626a348a6aea60fd836e6818e9381e12
Hiyabye <hiyabye0465@gmail.com>
2023-10-13 23:36:45 +0900

    Add 10769.cpp

---
+++ 10xxx/10769.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; getline(cin, s);
+
+  int happy = 0, sad = 0, n = s.length();
+  for (int i=0; i<n-2; i++) {
+    if (s[i] == ':' && s[i+1] == '-') {
+      if (s[i+2] == ')') happy++;
+      else if (s[i+2] == '(') sad++;
+    }
+  }
+
+  if (happy == 0 && sad == 0) cout << "none";
+  else if (happy == sad) cout << "unsure";
+  else if (happy > sad) cout << "happy";
+  else cout << "sad";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
