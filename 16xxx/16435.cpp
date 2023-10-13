ef678c7212d95624fed5daa21299e9a8e5a56af4
Hiyabye <hiyabye0465@gmail.com>
2023-10-14 00:11:18 +0900

    Add 16435.cpp

---
+++ 16xxx/16435.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int n, l; cin >> n >> l;
+  vector<int> h(n);
+  for (int i=0; i<n; i++) cin >> h[i];
+  sort(h.begin(), h.end());
+
+  for (int i=0; i<n; i++) {
+    if (l >= h[i]) l++;
+    else break;
+  }
+  cout << l;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
