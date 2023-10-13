2f3d27924c37faf2cc881582926777e034c4bb6b
Hiyabye <hiyabye0465@gmail.com>
2023-10-13 23:57:27 +0900

    Add 14582.cpp

---
+++ 14xxx/14582.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  vector<int> a(9), b(9);
+  for (int i=0; i<9; i++) cin >> a[i];
+  for (int i=0; i<9; i++) cin >> b[i];
+
+  int scoreA = 0, scoreB = 0, cur = 0;
+  for (int i=0; i<9; i++) {
+    scoreA += a[i];
+    if (scoreA > scoreB) cur = 1;
+    scoreB += b[i];
+    if (scoreA < scoreB && cur == 1) cur = 2;
+  }
+  cout << (cur == 2 ? "Yes" : "No");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
