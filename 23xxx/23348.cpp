bf950dee4389680d0373d5d93ed2a597b7112cc4
Hiyabye <hiyabye0465@gmail.com>
2023-10-14 00:19:17 +0900

    Add 23348.cpp

---
+++ 23xxx/23348.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int a, b, c; cin >> a >> b >> c;
+  int n; cin >> n;
+
+  vector<int> v(n, 0);
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<3; j++) {
+      int x, y, z; cin >> x >> y >> z;
+      v[i] += a*x + b*y + c*z;
+    }
+  }
+  cout << *max_element(v.begin(), v.end());
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
