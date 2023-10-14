244fdb31b72e815ddf73bb3e3ec2fcaa24a1d890
Hiyabye <hiyabye0465@gmail.com>
2023-10-14 11:00:50 +0900

    Add 06131.cpp

---
+++ 06xxx/06131.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  for (int b=1; b<=500; b++) {
+    for (int a=b; a<=500; a++) {
+      if (a*a == b*b + n) ans++;
+      else if (a*a > b*b + n) break;
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
