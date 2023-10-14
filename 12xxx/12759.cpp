3c83e97322c84fa48fab827ea3a0b0ac5a5bb5b6
Hiyabye <hiyabye0465@gmail.com>
2023-10-14 11:06:38 +0900

    Add 12759.cpp

---
+++ 12xxx/12759.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int check(vector<vector<int> >& board) {
+  for (int i=0; i<3; i++) {
+    if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) return board[i][0];
+    if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) return board[0][i];
+  }
+  if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0];
+  if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return board[0][2];
+  return 0;
+}
+
+void solve(void) {
+  int cur; cin >> cur;
+  vector<vector<int> > board(3, vector<int>(3, 0));
+
+  int ans = 0;
+  for (int i=0; i<9; i++) {
+    int x, y; cin >> x >> y;
+    x--; y--;
+    board[x][y] = cur;
+    if (ans == 0) ans = check(board);
+    cur = 3 - cur;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
