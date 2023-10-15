f7b6ea9b3174b00303eddf399ddacbee1d2c05be
Hiyabye <hiyabye0465@gmail.com>
2023-10-15 21:33:20 +0900

    Add 12756.cpp

---
+++ 12xxx/12756.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a_attack, a_life; cin >> a_attack >> a_life;
+  int b_attack, b_life; cin >> b_attack >> b_life;
+
+  while (a_life > 0 && b_life > 0) {
+    a_life -= b_attack;
+    b_life -= a_attack;
+  }
+
+  if (a_life <= 0 && b_life <= 0) cout << "DRAW";
+  else if (a_life <= 0) cout << "PLAYER B";
+  else cout << "PLAYER A";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
