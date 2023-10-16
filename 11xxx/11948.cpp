a81ca82429c6b4c8174924f6d72ce7c9ff3df21b
Hiyabye <hiyabye0465@gmail.com>
2023-10-16 12:58:30 +0900

    Add 11948.cpp

---
+++ 11xxx/11948.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+void solve(void) {
+  vector<int> v(6);
+  for (int i=0; i<6; i++) cin >> v[i];
+
+  cout << accumulate(v.begin(), v.end(), 0) - min(min(v[0], v[1]), min(v[2], v[3])) - min(v[4], v[5]);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
