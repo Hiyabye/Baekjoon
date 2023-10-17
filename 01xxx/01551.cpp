5fe01efc2d78d2d834001de3c4c7c9100d5d8956
Hiyabye <hiyabye0465@gmail.com>
2023-10-17 12:01:02 +0900

    Add 01551.cpp

---
+++ 01xxx/01551.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, k; cin >> n >> k;
+  vector<int> v(n);
+  cin >> v[0];
+  char c;
+  for (int i=1; i<n; i++) cin >> c >> v[i];
+
+  for (int i=0; i<k; i++) {
+    for (int j=0; j<n-1; j++) {
+      v[j] = v[j+1] - v[j];
+    }
+    n--;
+  }
+  
+  cout << v[0];
+  for (int i=1; i<n; i++) cout << "," << v[i];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
