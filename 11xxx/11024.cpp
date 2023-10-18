b1e9e566186e043169c9b6ddb7bf15c6c0e11c5a
Hiyabye <hiyabye0465@gmail.com>
2023-10-18 15:38:31 +0900

    Add 11024.cpp

---
+++ 11xxx/11024.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+void solve(void) {
+  string s; getline(cin, s);
+  stringstream ss(s);
+
+  int ans = 0, x;
+  while (ss >> x) ans += x;
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  cin.ignore();
+  while (t--) solve();
+  return 0;
+}
