08ae444670708bb283546101f75960f0975e1edd
Hiyabye <hiyabye0465@gmail.com>
2023-10-18 13:06:31 +0900

    Add 05426.cpp

---
+++ 05xxx/05426.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+  int n = sqrt(s.size());
+
+  vector<vector<char> > v(n, vector<char>(n));
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+      v[i][j] = s[i*n+j];
+    }
+  }
+
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+      cout << v[j][n-i-1];
+    }
+  }
+  cout << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
