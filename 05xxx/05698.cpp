c4f7e443975d332c7ff2db3ccad26e97c165510b
Hiyabye <hiyabye0465@gmail.com>
2023-10-19 00:39:07 +0900

    Add 05698.cpp

---
+++ 05xxx/05698.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+bool solve(void) {
+  string s; getline(cin, s);
+  if (s == "*") return false;
+  for (int i=0; i<s.length(); i++) {
+    if (s[i] >= 'A' && s[i] <= 'Z') {
+      s[i] += 'a' - 'A';
+    }
+  }
+
+  stringstream ss(s);
+  string tmp;
+  while (ss >> tmp) {
+    if (tmp[0] != s[0]) {
+      cout << "N\n";
+      return true;
+    }
+  }
+  cout << "Y\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  while (solve());
+  return 0;
+}
