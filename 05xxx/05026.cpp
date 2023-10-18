68d09a71e172dd432737aa502304611b6b5ee632
Hiyabye <hiyabye0465@gmail.com>
2023-10-18 15:45:51 +0900

    Add 05026.cpp

---
+++ 05xxx/05026.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+  if (s == "P=NP") {
+    cout << "skipped\n";
+    return;
+  }
+
+  stringstream ss(s);
+  int a, b; char c;
+  ss >> a >> c >> b;
+  cout << a + b << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
