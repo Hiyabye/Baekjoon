8a3adb0ae79d7fb8b989626361ab88e51fb32368
Hiyabye <hiyabye0465@gmail.com>
2023-10-20 00:57:00 +0900

    Add 12760.cpp

---
+++ 12xxx/12760.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  vector<vector<int> > v(n, vector<int>(m));
+  for (int i=0; i<n; i++) for (int j=0; j<m; j++) cin >> v[i][j];
+  for (int i=0; i<n; i++) sort(v[i].begin(), v[i].end(), greater<int>());
+
+  vector<int> score(n, 0);
+  for (int i=0; i<m; i++) {
+    int mx = 0;
+    for (int j=0; j<n; j++) mx = max(mx, v[j][i]);
+    for (int j=0; j<n; j++) if (v[j][i] == mx) score[j]++;
+  }
+
+  int ans = *max_element(score.begin(), score.end());
+  for (int i=0; i<n; i++) {
+    if (score[i] == ans) cout << i+1 << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
