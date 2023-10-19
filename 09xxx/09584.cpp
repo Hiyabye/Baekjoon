40b6cb564d43a2785a71834598d4f1f89bdf15a0
Hiyabye <hiyabye0465@gmail.com>
2023-10-20 00:51:48 +0900

    Add 09584.cpp

---
+++ 09xxx/09584.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+  int n; cin >> n;
+
+  vector<string> ans;
+  for (int i=0; i<n; i++) {
+    string t; cin >> t;
+    bool flag = true;
+    for (int j=0; j<9; j++) {
+      if (s[j] == '*') continue;
+      if (s[j] != t[j]) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) ans.push_back(t);
+  }
+
+  cout << ans.size() << "\n";
+  for (string t : ans) cout << t << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
