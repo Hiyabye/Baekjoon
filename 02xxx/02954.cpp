c6f89c562da9794b6fd0b010e1170ff8d8639d80
Hiyabye <hiyabye0465@gmail.com>
2023-10-21 16:45:23 +0900

    Add 02954.cpp

---
+++ 02xxx/02954.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+void solve(void) {
+  string s; getline(cin, s);
+  stringstream ss(s);
+
+  string t;
+  while (ss >> t) {
+    for (int i=0; i<t.length(); i++) {
+      if (t[i] == 'a' || t[i] == 'e' || t[i] == 'i' || t[i] == 'o' || t[i] == 'u') i += 2;
+      cout << t[i];
+    }
+    cout << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
