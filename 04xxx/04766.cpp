68ce681d9b7d75cf984ebdad1d60584601a3353c
Hiyabye <hiyabye0465@gmail.com>
2023-10-21 20:30:56 +0900

    Add 04766.cpp

---
+++ 04xxx/04766.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void solve(void) {
+  double pre, cur;
+
+  cin >> pre;
+  cin >> cur;
+  while (cur != 999) {
+    cout << fixed << setprecision(2) << cur - pre << "\n";
+    pre = cur;
+    cin >> cur;
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
