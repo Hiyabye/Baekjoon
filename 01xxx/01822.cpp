fabbf2caf0911497496f18f534cecc6b8913b23c
Hiyabye <hiyabye0465@gmail.com>
2023-10-24 14:06:39 +0900

    Add 01822.cpp

---
+++ 01xxx/01822.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+void solve(void) {
+  int na, nb; cin >> na >> nb;
+  vector<int> a(na), b(nb), c;
+  for (int i=0; i<na; i++) cin >> a[i];
+  for (int i=0; i<nb; i++) cin >> b[i];
+
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+  set_difference(a.begin(), a.end(), b.begin(), b.end(), back_inserter(c));
+
+  cout << c.size() << "\n";
+  for (int i=0; i<c.size(); i++) cout << c[i] << " ";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
