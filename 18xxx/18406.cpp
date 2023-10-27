929a3829fd75d52e8650c91ab874f156dea7151a
Hiyabye <hiyabye0465@gmail.com>
2023-10-28 00:48:44 +0900

    Add 18406.cpp

---
+++ 18xxx/18406.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string n; cin >> n;
+
+  int sum = 0, len = n.length();
+  for (int i=0; i<len/2; i++) sum += n[i] - '0';
+  for (int i=len/2; i<len; i++) sum -= n[i] - '0';
+
+  cout << (sum == 0 ? "LUCKY" : "READY");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
