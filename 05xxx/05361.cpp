086776bf355ce7bc07ae79f37e20d48902d846f3
Hiyabye <hiyabye0465@gmail.com>
2023-10-27 17:08:29 +0900

    Add 05361.cpp

---
+++ 05xxx/05361.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void solve(void) {
+  int a, b, c, d, e; cin >> a >> b >> c >> d >> e;
+
+  double ans = 0.0;
+  ans += a * 350.34;
+  ans += b * 230.90;
+  ans += c * 190.55;
+  ans += d * 125.30;
+  ans += e * 180.90;
+
+  cout << "$" << fixed << setprecision(2) << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
