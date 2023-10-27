101c5f89b421e9f040a1b475fc10a6dd5cbcbc00
Hiyabye <hiyabye0465@gmail.com>
2023-10-28 00:37:50 +0900

    Add 02083.cpp

---
+++ 02xxx/02083.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool solve(void) {
+  string name; int age, weight;
+  cin >> name >> age >> weight;
+  if (name == "#" && age == 0 && weight == 0) return false;
+
+  cout << name << " " << (age > 17 || weight >= 80 ? "Senior" : "Junior") << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  while (solve());
+  return 0;
+}
