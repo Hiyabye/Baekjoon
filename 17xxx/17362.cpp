1c75c28555ccac66cfbeb89a5863cfd26e856aab
Hiyabye <hiyabye0465@gmail.com>
2023-10-28 01:04:30 +0900

    Add 17362.cpp

---
+++ 17xxx/17362.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << min((n-1)%8+1, 9-(n-1)%8);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
