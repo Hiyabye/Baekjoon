11df10b7c3a40a31186135851ba7afb07394676d
Hiyabye <hiyabye0465@gmail.com>
2023-10-28 01:12:46 +0900

    Add 14489.cpp

---
+++ 14xxx/14489.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+  int c; cin >> c;
+
+  cout << (a+b >= 2*c ? a+b-2*c : a+b);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
