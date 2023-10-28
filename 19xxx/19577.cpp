0cc281eff705adf5967c2ab54572079e7c20492e
Hiyabye <hiyabye0465@gmail.com>
2023-10-28 17:40:46 +0900

    Add 19577.cpp

---
+++ 19xxx/19577.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int phi(int n) {
+  int ret = n;
+  for (int i=2; i*i<=n; ++i) {
+    if (n % i) continue;
+    ret -= ret / i;
+    while (!(n % i)) n /= i;
+  }
+  if (n > 1) ret -= ret / n;
+  return ret;
+}
+
+void solve(void) {
+  int n; cin >> n;
+
+  vector<int> div;
+  for (int i=1; i*i<=n; i++) {
+    if (n % i) continue;
+    div.push_back(i);
+    if (i * i != n) div.push_back(n / i);
+  }
+  sort(div.begin(), div.end());
+
+  for (int x : div) {
+    if (x * phi(x) == n) {
+      cout << x;
+      return;
+    }
+  }
+  cout << -1;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
