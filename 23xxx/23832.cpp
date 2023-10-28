51fda2d9738016e7f32f17c8aef4dd6a850bb042
Hiyabye <hiyabye0465@gmail.com>
2023-10-28 17:29:39 +0900

    Add 23832.cpp

---
+++ 23xxx/23832.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int phi(int n) {
+  int ret = n;
+  for (int i=2; i*i<=n; ++i) {
+    if (n % i) continue;
+    ret -= ret / i;
+    while (!(n % i)) n /= i;
+  }
+  if (n > 1) ret -= ret / n;
+  return ret;
+}
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = -1;
+  for (int i=1; i<=n; i++) ans += phi(i);
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
