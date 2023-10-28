7e3958cb04d3de0db1051b70a0b547f351282351
Hiyabye <hiyabye0465@gmail.com>
2023-10-28 17:24:09 +0900

    Add 04355.cpp

---
+++ 04xxx/04355.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int phi(int n) {
+  int ret = n;
+  for (int i=2; i*i<=n; ++i) {
+    if (n % i) continue;
+    ret -= ret / i;
+    while (!(n % i)) n /= i;
+  }
+  if (n > 1) ret -= ret / n;
+  return ret;
+}
+
+bool solve(void) {
+  int n; cin >> n;
+  if (n == 0) return false;
+
+  cout << (n == 1 ? 0 : phi(n)) << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  while (solve());
+  return 0;
+}
