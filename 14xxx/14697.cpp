c9741fae28d8a953e09e9eef866c0c63e2d67089
Hiyabye <hiyabye0465@gmail.com>
2023-10-28 16:22:04 +0900

    Add 14697.cpp

---
+++ 14xxx/14697.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c, n; cin >> a >> b >> c >> n;
+
+  for (int i=0; i<=n/a; i++) {
+    for (int j=0; j<=(n-i*a)/b; j++) {
+      for (int k=0; k<=(n-i*a-j*b)/c; k++) {
+        if (i*a + j*b + k*c == n) {
+          cout << 1;
+          return;
+        }
+      }
+    }
+  }
+  cout << 0;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
