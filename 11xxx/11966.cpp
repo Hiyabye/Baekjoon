945aa85d713800b6c684f66319958fec36f42912
Hiyabye <hiyabye0465@gmail.com>
2023-10-29 22:03:29 +0900

    Add 11966.cpp

---
+++ 11xxx/11966.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  while (!(n & 1)) n >>= 1;
+  cout << (n == 1) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
