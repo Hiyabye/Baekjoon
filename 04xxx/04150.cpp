1e2cfc4608451a52580f4fd7e815229015ee8a74
Hiyabye <hiyabye0465@gmail.com>
2023-10-29 23:13:28 +0900

    Add 04150.cpp

---
+++ 04xxx/04150.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string add(string a, string b) {
+  reverse(a.begin(), a.end());
+  reverse(b.begin(), b.end());
+
+  string c = "";
+  int carry = 0;
+  for (int i=0; i<max(a.length(), b.length()); i++) {
+    int sum = carry;
+    if (i < a.length()) sum += a[i] - '0';
+    if (i < b.length()) sum += b[i] - '0';
+    c += (sum % 10) + '0';
+    carry = sum / 10;
+  }
+  if (carry) c += carry + '0';
+
+  reverse(c.begin(), c.end());
+  return c;
+}
+
+void solve(void) {
+  int n; cin >> n;
+
+  string a = "1", b = "1";
+  for (int i=3; i<=n; i++) {
+    string c = add(a, b);
+    a = b;
+    b = c;
+  }
+
+  cout << b;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
