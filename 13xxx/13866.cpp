b296ca4badc5f131cbcfc356e5e33cd5ffd7c43d
Hiyabye <hiyabye0465@gmail.com>
2023-10-29 12:30:32 +0900

    Add 13866.cpp

---
+++ 13xxx/13866.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int a, b, c, d; cin >> a >> b >> c >> d;
+
+  cout << min(abs(a+d-b-c), min(abs(a+c-b-d), abs(a+b-c-d)));
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
