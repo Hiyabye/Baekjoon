6073bc7ea3ae4f28d5d2a53c1a23d97101d990b3
Hiyabye <hiyabye0465@gmail.com>
2023-10-29 22:09:42 +0900

    Add 17614.cpp

---
+++ 17xxx/17614.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int cnt(int n) {
+  int ret = 0;
+  while (n) {
+    if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9) ret++;
+    n /= 10;
+  }
+  return ret;
+}
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  for (int i=1; i<=n; i++) {
+    ans += cnt(i);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
