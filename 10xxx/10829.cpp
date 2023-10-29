74de1eb258042de09c1a29f47bcb00a76b5f88a7
Hiyabye <hiyabye0465@gmail.com>
2023-10-29 22:07:01 +0900

    Add 10829.cpp

---
+++ 10xxx/10829.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  long long n; cin >> n;
+
+  string ans = "";
+  while (n) {
+    ans += (n & 1) + '0';
+    n >>= 1;
+  }
+
+  reverse(ans.begin(), ans.end());
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
