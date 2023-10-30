3572270b1f73300b803db6c65fa8d22c4ad8fb29
Hiyabye <hiyabye0465@gmail.com>
2023-10-30 14:55:24 +0900

    Add 11596.cpp

---
+++ 11xxx/11596.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  vector<int> a(3), b(3);
+  for (int i=0; i<3; i++) cin >> a[i];
+  for (int i=0; i<3; i++) cin >> b[i];
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+
+  bool ans = true;
+  for (int i=0; i<3; i++) {
+    if (a[i] != b[i]) ans = false;
+  }
+  cout << (ans && a[2]*a[2] == a[0]*a[0] + a[1]*a[1] ? "YES" : "NO");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
