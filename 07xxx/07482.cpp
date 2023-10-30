1c70bf4241ac5b78bc54b23bba89b74c667b9dc3
Hiyabye <hiyabye0465@gmail.com>
2023-10-30 11:38:09 +0900

    Add 07482.cpp

---
+++ 07xxx/07482.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void solve(void) {
+  double a; cin >> a;
+
+  cout << fixed << setprecision(10) << a / 6 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
