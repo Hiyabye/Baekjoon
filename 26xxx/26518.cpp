3f75149f2481fdca2d1d8d9c1c0c6d09836edda3
Hiyabye <hiyabye0465@gmail.com>
2023-10-30 21:25:58 +0900

    Add 26518.cpp

---
+++ 26xxx/26518.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+void solve(void) {
+  long long b, c, a1, a2; cin >> b >> c >> a1 >> a2;
+
+  cout << fixed << setprecision(9) << ((double)(b)+sqrt(b*b+4*c))/2;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
