d502a4abb01b582a6325fb39c461547f540e1817
Hiyabye <hiyabye0465@gmail.com>
2023-10-30 11:40:59 +0900

    Add 14730.cpp

---
+++ 14xxx/14730.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  while (n--) {
+    int c, k; cin >> c >> k;
+    ans += c * k;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
