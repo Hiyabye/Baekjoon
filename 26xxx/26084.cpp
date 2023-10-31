a7ecabaf6be7d32b17832937509fc09e1e39bcaa
Hiyabye <hiyabye0465@gmail.com>
2023-10-31 22:03:13 +0900

    Add 26084.cpp

---
+++ 26xxx/26084.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+long long c(int n, int k) {
+  long long ret = 1;
+  for (int i=0; i<k; i++) {
+    ret *= n-i;
+    ret /= i+1;
+  }
+  return ret;
+}
+
+void solve(void) {
+  vector<int> cnt1(26, 0), cnt2(26, 0);
+  string s; cin >> s;
+  for (int i=0; i<3; i++) cnt1[s[i]-'A']++;
+
+  int n; cin >> n;
+  while (n--) {
+    string t; cin >> t;
+    cnt2[t[0]-'A']++;
+  }
+
+  long long ans = 1;
+  for (int i=0; i<26; i++) {
+    if (cnt1[i] == 0) continue;
+    ans *= c(cnt2[i], cnt1[i]);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
