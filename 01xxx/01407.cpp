2d7e55c8c4957b20ffb9740a38b56ca7fbe80f0e
Hiyabye <hiyabye0465@gmail.com>
2023-10-31 14:29:18 +0900

    Add 01407.cpp

---
+++ 01xxx/01407.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+long long f(long long x) {
+  long long ret = x;
+  for (long long i=2; i<=x; i<<=1) {
+    ret += (x/i) * (i>>1);
+  }
+  return ret;
+}
+
+void solve(void) {
+  long long a, b; cin >> a >> b;
+
+  cout << f(b) - f(a-1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
