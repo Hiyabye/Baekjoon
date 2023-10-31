7406e79ff00aa1e6d57cfdb01f1a7cd2ab59b18b
Hiyabye <hiyabye0465@gmail.com>
2023-10-31 21:39:37 +0900

    Add 26082.cpp

---
+++ 26xxx/26082.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c; cin >> a >> b >> c;
+
+  cout << 3 * b * c / a;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
