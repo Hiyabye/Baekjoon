bc5adca8bd8a7327dc64acf2ff64904dc21735ea
Hiyabye <hiyabye0465@gmail.com>
2023-11-03 00:24:09 +0900

    Add 20352.cpp

---
+++ 20xxx/20352.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+void solve(void) {
+  double a; cin >> a;
+
+  cout << fixed << setprecision(10) << sqrt(a/acos(-1)) * 2 * acos(-1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
