9651cdade70e0b672a228d66a648a380786c8e58
Hiyabye <hiyabye0465@gmail.com>
2023-11-02 22:41:56 +0900

    Add 29751.cpp

---
+++ 29xxx/29751.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int w, h; cin >> w >> h;
+
+  cout << w*h/2 << "." << ((w*h) & 1 ? "5" : "0");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
