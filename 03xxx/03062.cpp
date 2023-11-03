26b0fe45a797d5270988aa986463e38f83b64bd4
Hiyabye <hiyabye0465@gmail.com>
2023-11-03 16:17:58 +0900

    Add 03062.cpp

---
+++ 03xxx/03062.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  string n; cin >> n;
+  
+  string sum = to_string(stoi(n) + stoi(string(n.rbegin(), n.rend())));
+  for (int i=0; i<sum.length()/2; i++) {
+    if (sum[i] != sum[sum.length()-i-1]) {
+      cout << "NO\n";
+      return;
+    }
+  }
+  cout << "YES\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
