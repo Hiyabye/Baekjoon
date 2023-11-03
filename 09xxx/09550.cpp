6fb84436796d542abcdd2d46fef2dc2630044370
Hiyabye <hiyabye0465@gmail.com>
2023-11-03 17:05:36 +0900

    Add 09550.cpp

---
+++ 09xxx/09550.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, k; cin >> n >> k;
+  vector<int> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) ans += v[i] / k;
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
