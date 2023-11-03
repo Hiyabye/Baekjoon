dbae5bf208c7d33b44675a911231ad5366da389f
Hiyabye <hiyabye0465@gmail.com>
2023-11-03 16:01:18 +0900

    Add 08949.cpp

---
+++ 08xxx/08949.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  string a, b; cin >> a >> b;
+  reverse(a.begin(), a.end());
+  reverse(b.begin(), b.end());
+
+  vector<int> ans;
+  for (int i=0; i<max(a.length(), b.length()); i++) {
+    int x = (i < a.length()) ? a[i] - '0' : 0;
+    int y = (i < b.length()) ? b[i] - '0' : 0;
+    ans.push_back(x + y);
+  }
+
+  reverse(ans.begin(), ans.end());
+  for (int i=0; i<ans.size(); i++) cout << ans[i];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
