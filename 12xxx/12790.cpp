e7194acba8516e040eb59396220fbb52ae7c3989
Hiyabye <hiyabye0465@gmail.com>
2023-11-04 02:39:58 +0900

    Add 12790.cpp

---
+++ 12xxx/12790.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c, d, e, f, g, h;
+  cin >> a >> b >> c >> d >> e >> f >> g >> h;
+
+  a += e; b += f; c += g; d += h;
+  if (a < 1) a = 1;
+  if (b < 1) b = 1;
+  if (c < 0) c = 0;
+
+  cout << a + 5*b + 2*c + 2*d << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
