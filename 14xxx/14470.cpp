2bd3f1f82a05bb759ea57fda935c0c2e0b91c7bf
Hiyabye <hiyabye0465@gmail.com>
2023-11-03 15:27:51 +0900

    Add 14470.cpp

---
+++ 14xxx/14470.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a; cin >> a;
+  int b; cin >> b;
+  int c; cin >> c;
+  int d; cin >> d;
+  int e; cin >> e;
+
+  cout << (a > 0 ? (b-a) * e : -a*c + d + b*e);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
