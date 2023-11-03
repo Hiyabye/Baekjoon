099986e09bfb8ad23b09b5262f4e97f62af404ee
Hiyabye <hiyabye0465@gmail.com>
2023-11-03 17:22:03 +0900

    Add 01940.cpp

---
+++ 01xxx/01940.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  int m; cin >> m;
+  vector<int> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+  sort(v.begin(), v.end());
+
+  int ans = 0, l = 0, r = n-1;
+  while (l < r) {
+    int sum = v[l] + v[r];
+    if (sum == m) ans++, l++, r--;
+    else if (sum < m) l++;
+    else r--;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
