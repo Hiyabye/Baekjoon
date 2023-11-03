07fe3e70c7c1cdfd52d098a288f95db7e1a2aae5
Hiyabye <hiyabye0465@gmail.com>
2023-11-03 16:11:18 +0900

    Add 04613.cpp

---
+++ 04xxx/04613.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool solve(void) {
+  string s; getline(cin, s);
+  if (s == "#") return false;
+
+  int ans = 0;
+  for (int i=0; i<s.length(); i++) {
+    ans += (i+1) * (s[i] == ' ' ? 0 : s[i] - 'A' + 1);
+  }
+  cout << ans << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  while (solve());
+  return 0;
+}
