bb2b4743533b2335115b9dbb8f47908c81a807d7
Hiyabye <hiyabye0465@gmail.com>
2023-11-03 17:17:53 +0900

    Add 04597.cpp

---
+++ 04xxx/04597.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool solve(void) {
+  string s; cin >> s;
+  if (s == "#") return false;
+
+  int cnt = 0, n = s.length();
+  for (int i=0; i<n-1; i++) {
+    cnt += s[i] - '0';
+    cout << s[i];
+  }
+  
+  cout << (s[n-1] == 'e' && cnt & 1 || s[n-1] == 'o' && !(cnt & 1) ? '1' : '0') << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  while (solve());
+  return 0;
+}
