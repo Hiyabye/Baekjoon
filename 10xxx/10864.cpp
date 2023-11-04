07193f7c333444e835d7691e2b0a476ac158b54e
Hiyabye <hiyabye0465@gmail.com>
2023-11-04 22:21:02 +0900

    Add 10864.cpp

---
+++ 10xxx/10864.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  vector<vector<bool> > v(n, vector<bool>(n, false));
+
+  while (m--) {
+    int a, b; cin >> a >> b;
+    a--; b--;
+    v[a][b] = v[b][a] = true;
+  }
+
+  for (int i=0; i<n; i++) {
+    int cnt = 0;
+    for (int j=0; j<n; j++) {
+      if (v[i][j]) cnt++;
+    }
+    cout << cnt << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
