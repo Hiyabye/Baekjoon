eaa619c4101836d3e99cabfbb714d6e66f57e0e8
Hiyabye <hiyabye0465@gmail.com>
2023-11-05 10:58:13 +0900

    Add 14490.cpp

---
+++ 14xxx/14490.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int gcd(int a, int b) {
+  return b ? gcd(b, a % b) : a;
+}
+
+void solve(void) {
+  string s; cin >> s;
+
+  int n = stoi(s.substr(0, s.find(':')));
+  int m = stoi(s.substr(s.find(':') + 1));
+  int g = gcd(n, m);
+
+  cout << n / g << ":" << m / g;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
