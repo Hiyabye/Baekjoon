2d36930538e42022ad9c74549edc7d8945b711ab
Hiyabye <hiyabye0465@gmail.com>
2023-11-05 20:47:43 +0900

    Add 09437.cpp

---
+++ 09xxx/09437.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool solve(void) {
+  int n; cin >> n;
+  if (n == 0) return false;
+  int p; cin >> p;
+
+  vector<int> ans;
+  ans.push_back(p & 1 ? p+1 : p-1);
+  ans.push_back(p & 1 ? n-p : n-p+1);
+  ans.push_back(p & 1 ? n-p+1 : n-p+2);
+  sort(ans.begin(), ans.end());
+
+  for (int i=0; i<3; i++) cout << ans[i] << " ";
+  cout << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  while (solve());
+  return 0;
+}
