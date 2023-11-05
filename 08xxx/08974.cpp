1ff52b252b4cd42b4a5ee9d8257c237556f7d9d3
Hiyabye <hiyabye0465@gmail.com>
2023-11-05 21:06:28 +0900

    Add 08974.cpp

---
+++ 08xxx/08974.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+
+  int ans = 0, i = 1, j = 1;
+  while (i <= b) {
+    if (i >= a) ans += j;
+    if (i == j*(j+1)/2) j++;
+    i++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
