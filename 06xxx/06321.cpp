a1cdc37cc3b787b65cf3541dae4ed9560a91a4de
Hiyabye <hiyabye0465@gmail.com>
2023-11-05 23:41:36 +0900

    Add 06321.cpp

---
+++ 06xxx/06321.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  string s; cin >> s;
+  cout << "String #1\n";
+  for (int i=0; i<s.length(); i++) {
+    cout << (char)((s[i]-'A'+1)%26+'A');
+  }
+  cout << "\n";
+  for (int i=2; i<=n; i++) {
+    string s; cin >> s;
+
+    cout << "\nString #" << i << "\n";
+    for (int j=0; j<s.length(); j++) {
+      cout << (char)((s[j]-'A'+1)%26+'A');
+    }
+    cout << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
