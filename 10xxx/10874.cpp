5f0b2a790ee67f1e80e0b6caf87cdbf8fabba5b8
Hiyabye <hiyabye0465@gmail.com>
2023-11-06 13:23:09 +0900

    Add 10874.cpp

---
+++ 10xxx/10874.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(int idx) {
+  bool ans = true;
+  for (int i=0; i<10; i++) {
+    int x; cin >> x;
+    if (x != i % 5 + 1) ans = false;
+  }
+  if (ans) cout << idx << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int n; cin >> n;
+  for (int i=1; i<=n; i++) solve(i);
+  return 0;
+}
