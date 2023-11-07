72202bde7d810fb63e24de6e56f582fec9930881
Hiyabye <hiyabye0465@gmail.com>
2023-11-07 10:12:56 +0900

    Add 02161.cpp

---
+++ 02xxx/02161.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  queue<int> q;
+  for (int i=1; i<=n; i++) q.push(i);
+
+  while (!q.empty()) {
+    int x = q.front(); q.pop();
+    cout << x << " ";
+    if (q.empty()) break;
+    q.push(q.front()); q.pop();
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
