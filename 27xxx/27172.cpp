095f06ebec87a426cd3351cb0a500256d05995e9
Hiyabye <hiyabye0465@gmail.com>
2023-11-08 16:09:00 +0900

    Add 27172.cpp

---
+++ 27xxx/27172.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> x(n);
+  for (int i=0; i<n; i++) cin >> x[i];
+
+  vector<bool> exist(1000001, false);
+  for (int i=0; i<n; i++) exist[x[i]] = true;
+
+  vector<int> ans(1000001, 0);
+  for (int i=0; i<n; i++) {
+    for (int j=2*x[i]; j<=1000000; j+=x[i]) {
+      if (!exist[j]) continue;
+      ans[x[i]]++;
+      ans[j]--;
+    }
+  }
+
+  for (int i=0; i<n; i++) {
+    cout << ans[x[i]] << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
