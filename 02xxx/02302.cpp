54399a1a4a5028898a7b5bf7d85947bbcec621ed
Hiyabye <hiyabye0465@gmail.com>
2023-11-08 11:26:21 +0900

    Add 02302.cpp

---
+++ 02xxx/02302.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  int m; cin >> m;
+  vector<int> v(m);
+  for (int i=0; i<m; i++) cin >> v[i];
+
+  vector<int> dp(n+1, 1);
+  dp[2] = 2;
+  for (int i=3; i<=n; i++) dp[i] = dp[i-1] + dp[i-2];
+
+  int ans = 1, p = 0;
+  for (int i=0; i<m; i++) {
+    ans *= dp[v[i]-p-1];
+    p = v[i];
+  }
+  ans *= dp[n-p];
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
