f4fe873478856d99c64869e4588b909886b50b4a
Hiyabye <hiyabye0465@gmail.com>
2023-11-08 10:59:46 +0900

    Add 08394.cpp

---
+++ 08xxx/08394.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int a = 1, b = 1;
+  for (int i=2; i<=n; i++) {
+    int c = (a+b) % 10;
+    a = b;
+    b = c;
+  }
+  cout << b;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
