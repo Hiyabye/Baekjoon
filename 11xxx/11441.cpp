f75821b2d621352269d0e17b345ff83819967580
Hiyabye <hiyabye0465@gmail.com>
2023-11-08 10:55:42 +0900

    Add 11441.cpp

---
+++ 11xxx/11441.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  for (int i=1; i<n; i++) {
+    a[i] += a[i-1];
+  }
+
+  int m; cin >> m;
+  while (m--) {
+    int i, j; cin >> i >> j;
+    i--; j--;
+    cout << (i == 0 ? a[j] : a[j] - a[i-1]) << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
