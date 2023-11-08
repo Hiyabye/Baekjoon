4f0d84d12a8fcb54387c224315223eb3cbc88ab3
Hiyabye <hiyabye0465@gmail.com>
2023-11-08 11:11:28 +0900

    Add 02670.cpp

---
+++ 02xxx/02670.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<double> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+
+  vector<double> dp(n);
+  dp[0] = v[0];
+  for (int i=1; i<n; i++) {
+    dp[i] = max(dp[i-1] * v[i], v[i]);
+  }
+  cout << fixed << setprecision(3) << *max_element(dp.begin(), dp.end());
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
