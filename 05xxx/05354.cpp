549f5f2ee5197ce39996ea352117900972728d67
Hiyabye <hiyabye0465@gmail.com>
2023-11-08 10:23:54 +0900

    Add 05354.cpp

---
+++ 05xxx/05354.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int j; cin >> j;
+
+  for (int i=0; i<j; i++) {
+    for (int k=0; k<j; k++) {
+      cout << (i == 0 || i == j-1 || k == 0 || k == j-1 ? "#" : "J");
+    }
+    cout << "\n";
+  }
+  cout << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
