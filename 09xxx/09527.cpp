7fa8a9d10d70f2b7b68c728c91af71f05e7bb88f
Hiyabye <hiyabye0465@gmail.com>
2023-11-09 13:43:31 +0900

    Add 09527.cpp

---
+++ 09xxx/09527.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<long long> dp(56);
+
+long long cal(long long x) {
+  long long ret = x & 1;
+  for (int i=54; i>0; i--) {
+    if (x & (1LL<<i)) {
+      ret += dp[i-1] + x - (1LL<<i) + 1;
+      x -= 1LL<<i;
+    }
+  }
+  return ret;
+}
+
+void solve(void) {
+  long long a, b; cin >> a >> b;
+
+  dp[0] = 1;
+  for (int i=1; i<=55; i++) {
+    dp[i] = dp[i-1] * 2 + (1LL<<i);
+  }
+  cout << cal(b) - cal(a-1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
