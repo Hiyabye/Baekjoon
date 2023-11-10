d6e6679c0a5e418bd8aceff47beda939abb9c2b5
Hiyabye <hiyabye0465@gmail.com>
2023-11-10 12:25:03 +0900

    Add 12780.cpp

---
+++ 12xxx/12780.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string h; cin >> h;
+  string n; cin >> n;
+
+  int ans = 0;
+  for (int i=0; i<h.length()-n.length()+1; i++) {
+    if (h.substr(i, n.length()) == n) {
+      ans++;
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
