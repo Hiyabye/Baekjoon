517aa4b2416571bc8cc3fb10fb28e824497587b8
Hiyabye <hiyabye0465@gmail.com>
2023-11-12 21:53:25 +0900

    Add 14935.cpp

---
+++ 14xxx/14935.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string x; cin >> x;
+
+  cout << "FA";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
