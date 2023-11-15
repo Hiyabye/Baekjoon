c436f1b974d8ec52a3ce58e7198d298f2410653f
Hiyabye <hiyabye0465@gmail.com>
2023-11-16 00:33:54 +0900

    Add 16561.cpp

---
+++ 16xxx/16561.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  for (int a=3; a<=n-6; a+=3) {
+    for (int b=3; a+b<=n-3; b+=3) {
+      if (n-a-b >= 3) ans++;
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
