b22d3eefc1a3ed0f6796b2632ed334355e32a123
Hiyabye <hiyabye0465@gmail.com>
2023-11-18 16:27:14 +0900

    Add 02526.cpp

---
+++ 02xxx/02526.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int n, p; cin >> n >> p;
+
+  vector<int> cnt(p, 0);
+  int cur = n;
+  while (cnt[cur] < 2) {
+    cnt[cur]++;
+    cur = (cur * n) % p;
+  }
+  cout << count(cnt.begin(), cnt.end(), 2);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
