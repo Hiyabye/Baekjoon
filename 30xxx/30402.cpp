f93dd5a08c982043f1a1e0dc2830d58f07b087bd
Hiyabye <hiyabye0465@gmail.com>
2023-11-20 10:30:14 +0900

    Add 30402.cpp

---
+++ 30xxx/30402.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  vector<vector<char> > v(15, vector<char>(15));
+  for (int i=0; i<15; i++) for (int j=0; j<15; j++) cin >> v[i][j];
+
+  for (int i=0; i<15; i++) {
+    for (int j=0; j<15; j++) {
+      switch (v[i][j]) {
+        case 'w': cout << "chunbae"; return;
+        case 'b': cout << "nabi"; return;
+        case 'g': cout << "yeongcheol"; return;
+      }
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
