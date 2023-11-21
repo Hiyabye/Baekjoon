6a8414d0cdc953bd5a012c231e5c9a11161878f1
Hiyabye <hiyabye0465@gmail.com>
2023-11-22 01:17:24 +0900

    Add 10189.cpp

---
+++ 10xxx/10189.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "#  # #### #### #  #\n";
+  cout << "#### #  # #  # # #\n";
+  cout << "#### #  # #  # # #\n";
+  cout << "#  # #### #### #  #";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
