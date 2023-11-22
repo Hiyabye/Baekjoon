41ea65aa4044e76f3cd46958004e91bdaf1e2059
Hiyabye <hiyabye0465@gmail.com>
2023-11-22 09:52:32 +0900

    Add 14656.cpp

---
+++ 14xxx/14656.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  for (int i=1; i<=n; i++) {
+    int x; cin >> x;
+    if (x != i) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
