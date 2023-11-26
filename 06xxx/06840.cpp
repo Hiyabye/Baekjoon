64497be4b6eb4032149df2e040bd8278b9728657
Hiyabye <hiyabye0465@gmail.com>
2023-11-26 21:38:06 +0900

    Add 06840.cpp

---
+++ 06xxx/06840.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  int a, b, c;
+  cin >> a;
+  cin >> b;
+  cin >> c;
+
+  if (a > b) swap(a, b);
+  if (b > c) swap(b, c);
+  if (a > b) swap(a, b);
+
+  cout << b;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
