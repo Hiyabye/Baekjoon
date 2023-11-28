8fbd6cdfc0b66a95e35aa966fa326bae5c512e1a
Hiyabye <hiyabye0465@gmail.com>
2023-11-28 10:15:36 +0900

    Add 01120.cpp

---
+++ 01xxx/01120.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  string a, b; cin >> a >> b;
+
+  int ans = b.length();
+  for (int i=0; i<b.length()-a.length()+1; i++) {
+    int cnt = 0;
+    for (int j=0; j<a.length(); j++) {
+      if (a[j] != b[i+j]) cnt++;
+    }
+    ans = min(ans, cnt);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
