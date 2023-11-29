983872be682c0a4a54f74110ac21c808f847ce30
Hiyabye <hiyabye0465@gmail.com>
2023-11-29 09:51:16 +0900

    Add 10865.cpp

---
+++ 10xxx/10865.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  vector<int> f(n+1, 0);
+  while (m--) {
+    int a, b; cin >> a >> b;
+    f[a]++; f[b]++;
+  }
+
+  for (int i=1; i<=n; i++) {
+    cout << f[i] << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
