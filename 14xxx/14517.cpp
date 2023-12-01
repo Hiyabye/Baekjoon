40a375f218078e9a9612003bfb9e985197f47ba9
Hiyabye <hiyabye0465@gmail.com>
2023-12-01 12:23:29 +0900

    Add 14517.cpp

---
+++ 14xxx/14517.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#define MOD 10007
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+  int n = s.length();
+
+  vector<vector<int> > dp(n, vector<int>(n, 0));
+  for (int i=0; i<n; i++) dp[i][i] = 1;
+
+  for (int i=1; i<=n-1; i++) {
+    for (int j=0; j<n-i; j++) {
+      dp[j][j+i] = (MOD + dp[j][j+i-1] + dp[j+1][j+i] - dp[j+1][j+i-1] + (s[j] == s[j+i] ? dp[j+1][j+i-1] + 1 : 0)) % MOD;
+    }
+  }
+  cout << dp[0][n-1];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
