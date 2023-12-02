9206ea6ca2222ccb9cb771136289d61675c42179
Hiyabye <hiyabye0465@gmail.com>
2023-12-02 10:30:07 +0900

    Add 07770.cpp

---
+++ 07xxx/07770.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int cur = 0, ans = 0;
+  while (cur <= n) {
+    cur += (ans+1) * (ans+1) + ans * ans;
+    ans++;
+  }
+  cout << ans - 1;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
