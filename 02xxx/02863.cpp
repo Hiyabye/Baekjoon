b9f0bb87327292e644b731519d9a955ab0f23dff
Hiyabye <hiyabye0465@gmail.com>
2023-12-02 18:50:57 +0900

    Add 02863.cpp

---
+++ 02xxx/02863.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool cmp(double &mx, double val) {
+  if (mx < val) {
+    mx = val;
+    return true;
+  }
+  return false;
+}
+
+void solve(void) {
+  double a, b; cin >> a >> b;
+  double c, d; cin >> c >> d;
+
+  int ans = 0;
+  double mx = a/c + b/d;
+  if (cmp(mx, c/d + a/b)) ans = 1;
+  if (cmp(mx, d/b + c/a)) ans = 2;
+  if (cmp(mx, b/a + d/c)) ans = 3;
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
