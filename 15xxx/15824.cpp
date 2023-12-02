769fc8ba8ede2831fbfd9ddc647e855a991cc0cf
Hiyabye <hiyabye0465@gmail.com>
2023-12-02 15:21:00 +0900

    Add 15824.cpp

---
+++ 15xxx/15824.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MOD 1000000007
+using namespace std;
+
+long long power(long long base, long long exp) {
+  long long ret = 1;
+  while (exp) {
+    if (exp & 1) ret = (ret * base) % MOD;
+    base = (base * base) % MOD;
+    exp >>= 1;
+  }
+  return ret;
+}
+
+void solve(void) {
+  int n; cin >> n;
+  vector<long long> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+  sort(a.begin(), a.end());
+
+  long long ans = 0;
+  for (int i=0; i<n; i++) {
+    ans = (ans + (a[i] * power(2, i)) % MOD - (a[i] * power(2, n-i-1)) % MOD + MOD) % MOD;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
