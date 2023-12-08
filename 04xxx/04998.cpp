455384a6c849b5f29bc62e405127375a13dc66f4
Hiyabye <hiyabye0465@gmail.com>
2023-12-08 11:12:20 +0900

    Add 04998.cpp

---
+++ 04xxx/04998.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double n, b, m;
+
+  while (cin >> n >> b >> m) {
+    int ans = 0;
+    while (n <= m) {
+      n *= (1+b/100);
+      ans++;
+    }
+    cout << ans << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
