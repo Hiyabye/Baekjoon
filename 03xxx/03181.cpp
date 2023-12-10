afcc3d68eed617d19110a8522e32cc73ff530a09
Hiyabye <hiyabye0465@gmail.com>
2023-12-10 20:18:22 +0900

    Add 03181.cpp

---
+++ 03xxx/03181.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cctype>
+using namespace std;
+
+void solve(void) {
+  string str; getline(cin, str);
+  stringstream ss(str);
+
+  string s;
+  ss >> s;
+  cout << (char)toupper(s[0]);
+  while (ss >> s) {
+    if (s == "i" || s == "pa" || s == "te" || s == "ni" || s == "niti" || s == "a" || s == "ali" || s == "nego" || s == "no" || s == "ili") continue;
+    cout << (char)toupper(s[0]);
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
