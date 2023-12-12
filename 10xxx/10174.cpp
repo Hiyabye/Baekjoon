512e0b3d6bfe0e81d3394e8b52c67735c03a386e
Hiyabye <hiyabye0465@gmail.com>
2023-12-12 10:16:34 +0900

    Add 10174.cpp

---
+++ 10xxx/10174.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool check(string &s) {
+  int n = s.length();
+  for (char &c : s) if ('A' <= c && c <= 'Z') c += 'a' - 'A';
+  for (int i=0; i<n/2; i++) if (s[i] != s[n-i-1]) return false;
+  return true;
+}
+
+void solve(void) {
+  string s; getline(cin, s);
+
+  cout << (check(s) ? "Yes" : "No") << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int n; cin >> n;
+  cin.ignore();
+  while (n--) solve();
+  return 0;
+}
