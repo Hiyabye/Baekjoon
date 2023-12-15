0c2aabcebe50a797d29c3dc01aedc605009e8087
Hiyabye <hiyabye0465@gmail.com>
2023-12-15 14:50:28 +0900

    Add 13728.cpp

---
+++ 13xxx/13728.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#define MOD 1000000007
+using namespace std;
+
+int gcd(int a, int b) {
+  return b ? gcd(b, a % b) : a;
+}
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> dp(n+2);
+  dp[1] = 1; dp[2] = 1;
+
+  int ans = 1;
+  for (int i=2; i<=n; i++) {
+    dp[i+1] = (dp[i] + dp[i-1]) % MOD;
+    ans = (ans + dp[gcd(i+1, n+1)]) % MOD;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
