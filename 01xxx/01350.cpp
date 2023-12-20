66f5138d95ad6c2dafa209a7c9b142cd80da06cb
Hiyabye <hiyabye0465@gmail.com>
2023-12-20 10:27:24 +0900

    Add 01350.cpp

---
+++ 01xxx/01350.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<long long> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+  long long c; cin >> c;
+
+  long long ans = 0;
+  for (int i=0; i<n; i++) {
+    ans += (v[i]+c-1) / c;
+  }
+  cout << ans * c;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
