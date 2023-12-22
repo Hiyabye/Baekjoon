a0dc5f21d25b7af1aa9cb07d2d1ffd41370f2434
Hiyabye <hiyabye0465@gmail.com>
2023-12-22 17:43:49 +0900

    Add 10698.cpp

---
+++ 10xxx/10698.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(int idx) {
+  int x, y, z; char o, e;
+  cin >> x >> o >> y >> e >> z;
+
+  cout << "Case " << idx << ": " << (o == '+' ? (x + y == z ? "YES" : "NO") : (x - y == z ? "YES" : "NO")) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  for (int i=1; i<=t; i++) solve(i);
+  return 0;
+}
