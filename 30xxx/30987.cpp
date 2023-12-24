f40d480b5b683fdcc78ddf540a554ed13b9f4a24
Hiyabye <hiyabye0465@gmail.com>
2023-12-24 13:01:59 +0900

    Add 30987.cpp

---
+++ 30xxx/30987.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int x1, x2; cin >> x1 >> x2;
+  int a, b, c, d, e; cin >> a >> b >> c >> d >> e;
+
+  cout << a*(x2*x2*x2-x1*x1*x1)/3 + (b-d)*(x2*x2-x1*x1)/2 + (c-e)*(x2-x1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
