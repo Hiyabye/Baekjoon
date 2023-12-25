74d19b973a86e8490ccd2ad30ea003dcea7b35aa
Hiyabye <hiyabye0465@gmail.com>
2023-12-25 13:02:54 +0900

    Add 13410.cpp

---
+++ 13xxx/13410.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int mul(int a, int b) {
+  int ret = 0, tmp = a * b;
+  while (tmp) {
+    ret = ret * 10 + tmp % 10;
+    tmp /= 10;
+  }
+  return ret;
+}
+
+void solve(void) {
+  int n, k; cin >> n >> k;
+
+  int ans = 0;
+  for (int i=1; i<=k; i++) ans = max(ans, mul(n, i));
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
