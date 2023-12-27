c06f180282c24af747d11b917f743339dbc55315
Hiyabye <hiyabye0465@gmail.com>
2023-12-27 11:53:59 +0900

    Add 30993.cpp

---
+++ 30xxx/30993.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+long long fac(int n) {
+  long long ret = 1;
+  for (int i=2; i<=n; i++) ret *= i;
+  return ret;
+}
+
+void solve(void) {
+  int n, a, b, c; cin >> n >> a >> b >> c;
+
+  cout << fac(n) / (fac(a) * fac(b) * fac(c));
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
