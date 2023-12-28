d20fa88815e7c21d6476e4d776de719ae17a1643
Hiyabye <hiyabye0465@gmail.com>
2023-12-29 07:38:40 +0900

    Add 01773.cpp

---
+++ 01xxx/01773.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, c; cin >> n >> c;
+
+  vector<bool> v(c+1, false);
+  while (n--) {
+    int t; cin >> t;
+    for (int i=t; i<=c; i+=t) {
+      v[i] = true;
+    }
+  }
+
+  int ans = 0;
+  for (int i=1; i<=c; i++) {
+    if (v[i]) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
