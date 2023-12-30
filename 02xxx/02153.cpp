6ddf3c6e2af7557a1176ba5422d30bd4d98d01b4
Hiyabye <hiyabye0465@gmail.com>
2023-12-30 18:36:34 +0900

    Add 02153.cpp

---
+++ 02xxx/02153.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  int sum = 0;
+  for (char c : s) {
+    if (c >= 'a' && c <= 'z') sum += c - 'a' + 1;
+    else sum += c - 'A' + 27;
+  }
+
+  cout << "It is ";
+  for (int i=2; i<sum; i++) {
+    if (sum % i) continue;
+    cout << "not ";
+    break;
+  }
+  cout << "a prime word.";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
