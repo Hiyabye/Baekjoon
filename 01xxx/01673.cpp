055db6cb63b00c75735c4297f846ba27ef32a7b6
Hiyabye <hiyabye0465@gmail.com>
2023-12-31 22:00:57 +0900

    Add 01673.cpp

---
+++ 01xxx/01673.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n, k;
+
+  while (cin >> n >> k) {
+    int ans = 0, cnt = 0;
+    while (n > 0) {
+      ans += n;
+      cnt += n;
+      n = cnt / k;
+      cnt %= k;
+    }
+    cout << ans << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
