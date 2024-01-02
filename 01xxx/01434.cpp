5521e21bcc1146343c423635df80b68e72d30e12
Hiyabye <hiyabye0465@gmail.com>
2024-01-02 13:48:49 +0900

    Add 01434.cpp

---
+++ 01xxx/01434.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  vector<int> a(n), b(m);
+  for (int i=0; i<n; i++) cin >> a[i];
+  for (int i=0; i<m; i++) cin >> b[i];
+
+  cout << accumulate(a.begin(), a.end(), 0) - accumulate(b.begin(), b.end(), 0);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
