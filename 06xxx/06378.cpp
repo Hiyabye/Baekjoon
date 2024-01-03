4cba2fd53bedb27122509cfd14faae8d54dcafd4
Hiyabye <hiyabye0465@gmail.com>
2024-01-03 16:26:37 +0900

    Add 06378.cpp

---
+++ 06xxx/06378.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int digital(int n) {
+  int sum = 0;
+  while (n) {
+    sum += n % 10;
+    n /= 10;
+  }
+  return sum;
+}
+
+bool solve(void) {
+  string n; cin >> n;
+  if (n == "0") return false;
+
+  int sum = 0;
+  for (char c : n) sum += c - '0';
+  while (sum >= 10) sum = digital(sum);
+
+  cout << sum << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  while (solve());
+  return 0;
+}
