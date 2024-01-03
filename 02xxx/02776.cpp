bf50cc490df7e30a716e8fa74886b7aadd18c419
Hiyabye <hiyabye0465@gmail.com>
2024-01-03 23:46:36 +0900

    Add 02776.cpp

---
+++ 02xxx/02776.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  set<int> s;
+  for (int i=0; i<n; i++) {
+    int x; cin >> x;
+    s.insert(x);
+  }
+
+  int m; cin >> m;
+  vector<bool> ans(m);
+  for (int i=0; i<m; i++) {
+    int x; cin >> x;
+    ans[i] = s.find(x) != s.end();
+  }
+
+  for (bool b : ans) cout << b << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
