6c70e200b5202c6b4d289096811d27e609ea5e95
Hiyabye <hiyabye0465@gmail.com>
2024-01-04 01:06:40 +0900

    Add 30924.cpp

---
+++ 30xxx/30924.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <random>
+#include <set>
+using namespace std;
+
+int getNum(char c) {
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<int> dis(1, 10000);
+
+  set<int> s;
+  int ret = dis(gen);
+  s.insert(ret);
+
+  while (s.size() < 10000) {
+    cout << "? " << c << " " << ret << endl;
+    int resp; cin >> resp;
+    if (resp == 1) return ret;
+    while (s.find(ret) != s.end()) ret = dis(gen);
+    s.insert(ret);
+  }
+  return -1;
+}
+
+void solve(void) {
+  int a = getNum('A');
+  int b = getNum('B');
+
+  cout << "! " << a + b << endl;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
