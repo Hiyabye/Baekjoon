3f70d04034fb2851b7b1aaa7105b194b0e2b50b5
Hiyabye <hiyabye0465@gmail.com>
2024-01-04 02:33:34 +0900

    Add 11868.cpp

---
+++ 11xxx/11868.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> p(n);
+  for (int i=0; i<n; i++) cin >> p[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) ans ^= p[i];
+  cout << (ans ? "koosaga" : "cubelover");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
