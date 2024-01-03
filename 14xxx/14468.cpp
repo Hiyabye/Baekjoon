636f8e535b49345f9c0625136250db17cae078fb
Hiyabye <hiyabye0465@gmail.com>
2024-01-03 23:18:48 +0900

    Add 14468.cpp

---
+++ 14xxx/14468.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  vector<pair<int, int>> v(26, make_pair(-1, -1));
+  for (int i=0; i<52; i++) {
+    int c = s[i] - 'A';
+    if (v[c].first == -1) v[c].first = i;
+    else v[c].second = i;
+  }
+
+  int ans = 0;
+  for (int i=0; i<26; i++) {
+    for (int j=0; j<26; j++) {
+      if (i == j) continue;
+      if (v[i].first < v[j].first && v[j].first < v[i].second && v[i].second < v[j].second) ans++;
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
