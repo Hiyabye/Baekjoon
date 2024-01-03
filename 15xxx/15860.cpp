91be3ea38a63d11ca716146f264199d3397a3b19
Hiyabye <hiyabye0465@gmail.com>
2024-01-04 00:46:28 +0900

    Add 15860.cpp

---
+++ 15xxx/15860.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+void solve(void) {
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<int> dis(1, 2);
+
+  int x = 0;
+  while (x < 99) {
+    x += (x % 3 == 0 ? dis(gen) : 3 - x % 3);
+    cout << x << endl;
+    if (x >= 99) break;
+    cin >> x;
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
