c1209b4a422aaa0a6c64937605f14cfd6a0a6cc2
Hiyabye <hiyabye0465@gmail.com>
2024-01-04 00:32:56 +0900

    Add 19100.cpp

---
+++ 19xxx/19100.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> shuffle(vector<int> a) {
+  vector<int> b(a.begin(), a.begin() + a.size() / 2);
+  if (b.size() > 2) b = shuffle(b);
+  else if (b.size() == 2) swap(b[0], b[1]);
+
+  vector<int> c(a.begin() + a.size() / 2, a.end());
+  if (c.size() > 2) c = shuffle(c);
+  else if (c.size() == 2) swap(c[0], c[1]);
+
+  vector<int> d;
+  for (int i=0; i<c.size(); i++) d.push_back(c[i]);
+  for (int i=0; i<b.size(); i++) d.push_back(b[i]);
+  return d;
+}
+
+void solve(void) {
+  int n, t; cin >> n >> t;
+  vector<int> a(1 << n);
+  for (int i=0; i<(1<<n); i++) cin >> a[i];
+
+  if (t & 1) a = shuffle(a);
+  for (int i=0; i<(1<<n); i++) cout << a[i] << " ";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
