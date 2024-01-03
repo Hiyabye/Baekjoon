8235d7de164e18d3c132ee0a741fd38dea64f9bb
Hiyabye <hiyabye0465@gmail.com>
2024-01-04 02:48:49 +0900

    Add 07685.cpp

---
+++ 07xxx/07685.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool solve(void) {
+  int n; cin >> n;
+  if (n == 0) return false;
+
+  vector<int> k(n);
+  for (int i=0; i<n; i++) cin >> k[i];
+
+  int x = 0;
+  for (int i=0; i<n; i++) x ^= k[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    if ((x ^ k[i]) < k[i]) ans++;
+  }
+  cout << ans << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  while (solve());
+  return 0;
+}
