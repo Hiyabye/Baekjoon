60d4660fef0bf1bfbbd1b3c63eb4ffc6b0588e47
Hiyabye <hiyabye0465@gmail.com>
2024-01-04 00:58:29 +0900

    Add 30917.cpp

---
+++ 30xxx/30917.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a = 1;
+  while (true){
+    cout << "? A " << a << endl;
+    int resp; cin >> resp;
+    if (resp == 1) break;
+    a++;
+  }
+
+  int b = 1;
+  while (true){
+    cout << "? B " << b << endl;
+    int resp; cin >> resp;
+    if (resp == 1) break;
+    b++;
+  }
+
+  cout << "! " << a + b << endl;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  solve();
+  return 0;
+}
