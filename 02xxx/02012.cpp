ee0cea4367c93d3ab06b1a14e37d5ac044f7584b
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 01:05:06 +0900

    Add 02012.cpp

---
+++ 02xxx/02012.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<long long> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+  sort(v.begin(), v.end());
+
+  for (int i=0; i<n; i++) v[i] = abs(v[i] - (i+1));
+  cout << accumulate(v.begin(), v.end(), 0LL);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
