6f725a1aac4840d5b24c41500f15c8e1b33317df
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 19:51:04 +0900

    Add 28691.cpp

---
+++ 28xxx/28691.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  char c; cin >> c;
+
+  if (c == 'M') cout << "MatKor";
+  else if (c == 'W') cout << "WiCys";
+  else if (c == 'C') cout << "CyKor";
+  else if (c == 'A') cout << "AlKor";
+  else cout << "$clear";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
