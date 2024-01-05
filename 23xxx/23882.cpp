f2a8eb43b9d6c65dd15fa06fb5b04713f5ebad39
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 23:51:21 +0900

    Add 23882.cpp

---
+++ 23xxx/23882.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, k; cin >> n >> k;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  for (int i=n-1; i>=0 && k>0; i--) {
+    int mx = *max_element(a.begin(), a.begin()+i+1);
+    if (a[i] == mx) continue;
+    int j = find(a.begin(), a.begin()+i+1, mx) - a.begin();
+    swap(a[i], a[j]); k--;
+  }
+  
+  if (k > 0) cout << "-1";
+  else for (int i=0; i<n; i++) cout << a[i] << " ";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
