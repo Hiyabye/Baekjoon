a2d4f0385d8fdc4ad350eebec35c5c68868651f8
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 20:03:00 +0900

    Add 29863.cpp

---
+++ 29xxx/29863.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a; cin >> a;
+  int b; cin >> b;
+
+  if (a >= 20) a -= 24;
+  cout << b - a;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
