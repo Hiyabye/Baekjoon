f1560b2209579f839cb3c424278893c44f007b74
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 19:43:58 +0900

    Add 27324.cpp

---
+++ 27xxx/27324.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << (n / 10 == n % 10);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
