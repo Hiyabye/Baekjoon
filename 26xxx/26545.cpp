5a41d361d6e12ee37d8a9ea71fe04041b8b2c4a2
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 15:43:41 +0900

    Add 26545.cpp

---
+++ 26xxx/26545.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int sum = 0;
+  int x; while (n--) cin >> x, sum += x;
+  cout << sum;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
