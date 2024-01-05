b067e690697b4a80fe0ba4a1c3c2547424400eaf
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 19:54:06 +0900

    Add 28927.cpp

---
+++ 28xxx/28927.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int t1, e1, f1; cin >> t1 >> e1 >> f1;
+  int t2, e2, f2; cin >> t2 >> e2 >> f2;
+
+  int max = 3 * t1 + 20 * e1 + 120 * f1;
+  int mel = 3 * t2 + 20 * e2 + 120 * f2;
+  cout << (max > mel ? "Max" : max < mel ? "Mel" : "Draw");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
