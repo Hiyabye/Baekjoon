1a9ed31b557f8404bea6296ecb8bca4c725d1e0a
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 15:36:43 +0900

    Add 23235.cpp

---
+++ 23xxx/23235.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool solve(int idx) {
+  int n; cin >> n;
+  if (n == 0) return false;
+
+  int x; while(n--) cin >> x;
+  cout << "Case " << idx << ": Sorting... done!\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int idx = 1;
+  while (solve(idx++));
+  return 0;
+}
