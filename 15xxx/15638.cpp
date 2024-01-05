6e603857fe24e87c1688a41c5372dd78d39864fb
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 14:32:18 +0900

    Add 15638.cpp

---
+++ 15xxx/15638.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool prime(int n) {
+  if (n == 1) return false;
+  if (n == 2) return true;
+  if (!(n & 1)) return false;
+  for (int i=3; i*i<=n; i+=2) {
+    if (!(n % i)) return false;
+  }
+  return true;
+}
+
+void solve(void) {
+  int n; cin >> n;
+  int x; for (int i=0; i<n; i++) cin >> x;
+
+  cout << (prime(n) ? "Yes" : "No");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
