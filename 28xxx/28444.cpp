969059dd447c03186e1ea02b26fc62ea4a97784a
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 19:48:49 +0900

    Add 28444.cpp

---
+++ 28xxx/28444.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int h, i, a, r, c; cin >> h >> i >> a >> r >> c;
+
+  cout << h * i - a * r * c;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
