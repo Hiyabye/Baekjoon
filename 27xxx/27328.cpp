01c29dd9b84ff3a6f0ac21e80d77151ecc6e89c6
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 19:46:25 +0900

    Add 27328.cpp

---
+++ 27xxx/27328.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+
+  cout << (a < b ? -1 : a == b ? 0 : 1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
