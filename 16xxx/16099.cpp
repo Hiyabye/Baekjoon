704e775de64784149152a76d4b822b5499f0abe8
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 15:34:04 +0900

    Add 16099.cpp

---
+++ 16xxx/16099.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  long long lt, wt, le, we; cin >> lt >> wt >> le >> we;
+
+  cout << (lt * wt > le * we ? "TelecomParisTech" : (lt * wt < le * we ? "Eurecom" : "Tie")) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
