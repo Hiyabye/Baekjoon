e32889bc099895d4020e7370d00e167efb89ec74
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 19:38:13 +0900

    Add 26575.cpp

---
+++ 26xxx/26575.cpp
@@ -0,0 +1,18 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double d, f, p; cin >> d >> f >> p;
+
+  cout << fixed << setprecision(2) << "$" << d * f * p << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
