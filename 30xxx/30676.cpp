2f2a4bd02a6911b89c2b8f449472ccc36ad12983
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 20:42:50 +0900

    Add 30676.cpp

---
+++ 30xxx/30676.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int l; cin >> l;
+
+  if (l >= 620) cout << "Red";
+  else if (l >= 590) cout << "Orange";
+  else if (l >= 570) cout << "Yellow";
+  else if (l >= 495) cout << "Green";
+  else if (l >= 450) cout << "Blue";
+  else if (l >= 425) cout << "Indigo";
+  else cout << "Violet";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
