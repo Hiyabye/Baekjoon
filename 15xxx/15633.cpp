b827bb5bbc28c4c969e288a05d4cefe6493f08bc
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 14:44:16 +0900

    Add 15633.cpp

---
+++ 15xxx/15633.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  for (int i=1; i<=n; i++) {
+    if (!(n % i)) ans += i;
+  }
+  cout << 5*ans-24;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
