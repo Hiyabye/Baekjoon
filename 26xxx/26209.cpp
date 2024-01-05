1ff7cfb0b9ab5a0577118a0d6538f25cf2a4fd6e
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 15:38:58 +0900

    Add 26209.cpp

---
+++ 26xxx/26209.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+char solve(void) {
+  vector<int> v(8);
+  for (int i=0; i<8; i++) cin >> v[i];
+
+  for (int i=0; i<8; i++) {
+    if (v[i] == 9) return 'F';
+  }
+  return 'S';
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  cout << solve();
+  return 0;
+}
