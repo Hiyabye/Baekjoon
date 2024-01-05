fc7cf3500560490af17fd0a710f5c790e2d3ab54
Hiyabye <hiyabye0465@gmail.com>
2024-01-05 19:45:17 +0900

    Add 27327.cpp

---
+++ 27xxx/27327.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int x; cin >> x;
+
+  cout << x * 24;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
