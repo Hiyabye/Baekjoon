2fa12031d781711ff123cfffc6832377192036a1
Hiyabye <hiyabye0465@gmail.com>
2024-01-06 01:35:12 +0900

    Add 24516.cpp

---
+++ 24xxx/24516.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  
+  for (int i=0; i<n; i++) cout << 2*i+1 << " ";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
