bccc8ff0c5e8947089d81cafd7ab147773bb0145
Hiyabye <hiyabye0465@gmail.com>
2024-01-06 15:45:27 +0900

    Add 17356.cpp

---
+++ 17xxx/17356.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+
+  cout << 1 / (1 + pow(10, (b - a) / 400.0));
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
