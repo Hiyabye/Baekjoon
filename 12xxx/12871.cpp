3f549e9ceb2d46fc1813fb8bec96f6e236fb7c06
Hiyabye <hiyabye0465@gmail.com>
2024-01-06 14:29:31 +0900

    Add 12871.cpp

---
+++ 12xxx/12871.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int gcd(int a, int b) {
+  return b ? gcd(b, a % b) : a;
+}
+
+int lcm(int a, int b) {
+  return a * b / gcd(a, b);
+}
+
+void solve(void) {
+  string s; cin >> s;
+  string t; cin >> t;
+
+  int n = lcm(s.length(), t.length());
+  string ss = "", tt = "";
+  for (int i=0; i<n/s.length(); i++) ss += s;
+  for (int i=0; i<n/t.length(); i++) tt += t;
+
+  cout << (ss == tt ? 1 : 0);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
