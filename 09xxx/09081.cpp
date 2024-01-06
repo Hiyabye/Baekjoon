5a6799dbd5287e1c420c9d32264fda23a5a334b3
Hiyabye <hiyabye0465@gmail.com>
2024-01-06 20:56:17 +0900

    Add 09081.cpp

---
+++ 09xxx/09081.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+  string t = s;
+
+  cout << (next_permutation(s.begin(), s.end()) ? s : t) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
