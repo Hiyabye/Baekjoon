5f30bc4b00738f1d2e1bb9fe3dcf4364bb2fe0fb
Hiyabye <hiyabye0465@gmail.com>
2024-01-06 20:34:34 +0900

    Add 20291.cpp

---
+++ 20xxx/20291.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  map<string, int> m;
+
+  for (int i=0; i<n; i++) {
+    string s; cin >> s;
+    m[s.substr(s.find('.')+1)]++;
+  }
+
+  for (auto it=m.begin(); it!=m.end(); it++) {
+    cout << it->first << " " << it->second << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
