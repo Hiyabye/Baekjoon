6e77c661d5fd2d5562929d338d4539b46ad3560f
Hiyabye <hiyabye0465@gmail.com>
2024-01-07 12:07:01 +0900

    Add 01173.cpp

---
+++ 01xxx/01173.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int solve(void) {
+  int N, m, M, T, R; cin >> N >> m >> M >> T >> R;
+  if (m + T > M) return -1;
+
+  int ans = 0, cur = m;
+  while (N > 0) {
+    if (cur + T <= M) {
+      cur += T;
+      N--;
+    } else {
+      cur -= R;
+      if (cur < m) cur = m;
+    }
+    ans++;
+  }
+  return ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  cout << solve();
+  return 0;
+}
