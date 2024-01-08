57196d1150eebaaad09adfe3433f74a643ccfbbd
Jihoon Lee <hiyabye0465@gmail.com>
2024-01-08 11:05:04 +0900

    Add 25628.cpp

---
+++ 25xxx/25628.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+
+  cout << min(a/2, b);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
