f06091c9d4682f8bf7df26bbbd3f3ba18f49e422
Hiyabye <hiyabye0465@gmail.com>
2024-01-12 21:27:43 +0900

    Add 13118.cpp

---
+++ 13xxx/13118.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int solve(void) {
+  int p[5]; cin >> p[1] >> p[2] >> p[3] >> p[4];
+  int x, y, r; cin >> x >> y >> r;
+
+  for (int i=1; i<=4; i++) {
+    if (p[i] == x) return i;
+  }
+  return 0;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  cout << solve();
+  return 0;
+}
