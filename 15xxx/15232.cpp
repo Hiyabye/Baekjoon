821b6526dabd8502e582f671c24c6f4d543252fe
Hiyabye <hiyabye0465@gmail.com>
2024-01-12 23:42:24 +0900

    Add 15232.cpp

---
+++ 15xxx/15232.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int r; cin >> r;
+  int c; cin >> c;
+
+  for (int i=0; i<r; i++) {
+    for (int j=0; j<c; j++) cout << "*";
+    cout << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
