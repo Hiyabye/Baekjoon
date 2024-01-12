fc24a14843babe73b6a18ff1900427c30984d676
Hiyabye <hiyabye0465@gmail.com>
2024-01-12 21:21:53 +0900

    Add 15059.cpp

---
+++ 15xxx/15059.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int ca, ba, pa; cin >> ca >> ba >> pa;
+  int cr, br, pr; cin >> cr >> br >> pr;
+
+  cout << max(0, cr-ca) + max(0, br-ba) + max(0, pr-pa);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
