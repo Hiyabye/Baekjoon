e5c30e76afc9c1e9001493e11752e25a1400f4a2
Hiyabye <hiyabye0465@gmail.com>
2024-01-12 21:41:06 +0900

    Add 20976.cpp

---
+++ 20xxx/20976.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c; cin >> a >> b >> c;
+
+  cout << a + b + c - min({a, b, c}) - max({a, b, c});
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
