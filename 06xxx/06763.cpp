18d82340e6d828abc841b1a1453e4963f7e5fb64
Hiyabye <hiyabye0465@gmail.com>
2024-01-12 21:12:51 +0900

    Add 06763.cpp

---
+++ 06xxx/06763.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int l; cin >> l;
+  int c; cin >> c;
+
+  if (c <= l) cout << "Congratulations, you are within the speed limit!";
+  else if (l+1 <= c && c <= l+20) cout << "You are speeding and your fine is $100.";
+  else if (l+21 <= c && c <= l+30) cout << "You are speeding and your fine is $270.";
+  else cout << "You are speeding and your fine is $500.";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
