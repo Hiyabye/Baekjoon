9ca29b8be9a0a832da0747f2e15df3ad8e181f24
Hiyabye <hiyabye0465@gmail.com>
2024-01-12 23:28:30 +0900

    Add 28352.cpp

---
+++ 28xxx/28352.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 6;
+  for (int i=11; i<=n; i++) ans *= i;
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
