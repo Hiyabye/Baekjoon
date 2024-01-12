6cf436db5438472770f92e2fb6b9a42f66fac521
Jihoon Lee <hiyabye0465@gmail.com>
2024-01-12 09:54:15 +0900

    Add 21964.cpp

---
+++ 21xxx/21964.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  string s; cin >> s;
+
+  cout << s.substr(n-5);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
