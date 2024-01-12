d98f8526895d09345882e1957edb60b9000fc09a
Hiyabye <hiyabye0465@gmail.com>
2024-01-12 21:39:34 +0900

    Add 16600.cpp

---
+++ 16xxx/16600.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  long long a; cin >> a;
+
+  cout << fixed << setprecision(8) << sqrt(a) * 4;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
