4fd625d78fe03ea510a8261fa1f0907536f1a211
Hiyabye <hiyabye0465@gmail.com>
2024-01-12 23:53:22 +0900

    Add 07595.cpp

---
+++ 07xxx/07595.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+bool solve(void) {
+  int n; cin >> n;
+  if (n == 0) return false;
+
+  for (int i=1; i<=n; i++) {
+    for (int j=0; j<i; j++) cout << "*";
+    cout << "\n";
+  }
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  while (solve());
+  return 0;
+}
