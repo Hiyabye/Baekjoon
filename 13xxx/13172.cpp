f7b836cb5239a281a2f296565a0f7b9d7d2e8fe7
Hiyabye <hiyabye0465@gmail.com>
2024-01-13 17:49:21 +0900

    Add 13172.cpp

---
+++ 13xxx/13172.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#define MOD 1000000007
+using namespace std;
+
+long long power(long long base, long long exp) {
+  long long ret = 1;
+  while (exp) {
+    if (exp & 1) ret = ret * base % MOD;
+    base = base * base % MOD;
+    exp >>= 1;
+  }
+  return ret;
+}
+
+void solve(void) {
+  int m; cin >> m;
+
+  long long ans = 0;
+  for (int i=1; i<=m; i++) {
+    long long n, s; cin >> n >> s;
+    ans = (ans + s * power(n, MOD-2) % MOD) % MOD;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
