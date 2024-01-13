110b1e4b73f2d6613020e6a33f01cd10ccef942a
Hiyabye <hiyabye0465@gmail.com>
2024-01-13 13:00:31 +0900

    Add 16017.cpp

---
+++ 16xxx/16017.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+bool telemarketer(int a[]) {
+  if (a[0] != 8 && a[0] != 9) return false;
+  if (a[3] != 8 && a[3] != 9) return false;
+  return a[1] == a[2];
+}
+
+void solve(void) {
+  int a[4]; cin >> a[0] >> a[1] >> a[2] >> a[3];
+
+  cout << (telemarketer(a) ? "ignore" : "answer");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
