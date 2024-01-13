4b91d5e28f8c40ed2cc8aefb70a03041c21d05f2
Hiyabye <hiyabye0465@gmail.com>
2024-01-14 00:27:08 +0900

    Add 07572.cpp

---
+++ 07xxx/07572.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << (char)((n+8)%12+'A') << (n+6)%10;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
