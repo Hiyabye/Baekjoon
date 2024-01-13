d53dfa1e1aec4c767827966ea246931ebda575f5
Hiyabye <hiyabye0465@gmail.com>
2024-01-13 13:54:11 +0900

    Add 11576.cpp

---
+++ 11xxx/11576.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+  int m; cin >> m;
+  vector<int> v(m);
+  for (int i=0; i<m; i++) cin >> v[i];
+
+  long long num = 0;
+  for (int i=0; i<m; i++) {
+    num = a * num + v[i];
+  }
+
+  vector<int> ans;
+  while (num) {
+    ans.push_back(num % b);
+    num /= b;
+  }
+
+  while (!ans.empty()) {
+    cout << ans.back() << " ";
+    ans.pop_back();
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
