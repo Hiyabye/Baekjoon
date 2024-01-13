b6584b509639b89f1fb3f3e6e9f9ff4e8289ee3f
Hiyabye <hiyabye0465@gmail.com>
2024-01-14 00:58:57 +0900

    Add 04690.cpp

---
+++ 04xxx/04690.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  for (int a=6; a<=100; a++) {
+    for (int b=2; b<a; b++) {
+      for (int c=b+1; c<a; c++) {
+        for (int d=c+1; d<a; d++) {
+          if (a*a*a > b*b*b + c*c*c + d*d*d) continue;
+          if (a*a*a < b*b*b + c*c*c + d*d*d) break;
+          cout << "Cube = " << a << ", Triple = (" << b << "," << c << "," << d << ")\n";
+        }
+      }
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
