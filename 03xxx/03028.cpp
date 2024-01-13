4e89e9c6935dc144c60c586de67f62b9eceb35fb
Hiyabye <hiyabye0465@gmail.com>
2024-01-14 01:41:18 +0900

    Add 03028.cpp

---
+++ 03xxx/03028.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  int l = 1, m = 0, r = 0;
+  for (char c : s) {
+    if (c == 'A') swap(l, m);
+    if (c == 'B') swap(m, r);
+    if (c == 'C') swap(l, r);
+  }
+
+  if (l) cout << 1;
+  if (m) cout << 2;
+  if (r) cout << 3;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
