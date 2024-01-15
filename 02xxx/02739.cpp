1603cdf8bcd37ad7e8bcecd4f1b9b365dbbb1eab
Hiyabye <hiyabye0465@gmail.com>
2024-01-15 18:19:47 +0900

    Add 02739.cpp

---
+++ 02xxx/02739.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  for (int i=1; i<=9; i++) {
+    cout << n << " * " << i << " = " << n * i << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
