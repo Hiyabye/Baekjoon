5d2b98a5f85a0234ede1b913b9b8a18b4e44e02b
Hiyabye <hiyabye0465@gmail.com>
2024-01-15 21:00:29 +0900

    Add 15624.cpp

---
+++ 15xxx/15624.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#define MOD 1000000007
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<long long> f(n+2); // if n == 0, f[1] is used
+  f[0] = 0; f[1] = 1;
+  for (int i=2; i<=n; i++) f[i] = (f[i-1] + f[i-2]) % MOD;
+
+  cout << f[n];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
