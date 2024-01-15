b106297a079bc8a946905f017ff2c2fb7090d0ac
Hiyabye <hiyabye0465@gmail.com>
2024-01-15 15:39:57 +0900

    Add 02753.cpp

---
+++ 02xxx/02753.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int year; cin >> year;
+
+  if (year % 4 != 0) cout << 0;
+  else if (year % 100 != 0) cout << 1;
+  else if (year % 400 != 0) cout << 0;
+  else cout << 1;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
