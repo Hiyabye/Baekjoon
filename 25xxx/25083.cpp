e7e715526610226b00075897d7a6735b02511298
Hiyabye <hiyabye0465@gmail.com>
2024-01-15 13:37:57 +0900

    Add 25083.cpp

---
+++ 25xxx/25083.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "         ,r'\"7" << endl;
+  cout << "r`-_   ,'  ,/" << endl;
+  cout << " \\. \". L_r'" << endl;
+  cout << "   `~\\/" << endl;
+  cout << "      |" << endl;
+  cout << "      |" << endl;
+  return 0;
+}
