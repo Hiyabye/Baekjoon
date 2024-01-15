44b89d177500c403d4c97407d3df9d3e365a6703
Hiyabye <hiyabye0465@gmail.com>
2024-01-15 18:18:20 +0900

    Add 10950.cpp

---
+++ 10xxx/10950.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+
+  cout << a + b << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
