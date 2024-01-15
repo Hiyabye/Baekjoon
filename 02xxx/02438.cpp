25cf61c957a2552e88e2860ab8b60bc6c92a21f3
Hiyabye <hiyabye0465@gmail.com>
2024-01-15 18:20:40 +0900

    Add 02438.cpp

---
+++ 02xxx/02438.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  for (int i=1; i<=n; i++) {
+    for (int j=0; j<i; j++) cout << "*";
+    cout << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
