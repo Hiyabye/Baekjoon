94e401f7d0ce892fd22b8d61a840367380f3df37
Hiyabye <hiyabye0465@gmail.com>
2024-01-15 18:17:33 +0900

    Add 02741.cpp

---
+++ 02xxx/02741.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  for (int i=1; i<=n; i++) cout << i << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
