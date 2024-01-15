2640c53e0aa7c5ce8f35790a798901b688d92345
Hiyabye <hiyabye0465@gmail.com>
2024-01-15 13:36:27 +0900

    Add 10172.cpp

---
+++ 10xxx/10172.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "|\\_/|" << endl;
+  cout << "|q p|   /}" << endl;
+  cout << "( 0 )\"\"\"\\" << endl;
+  cout << "|\"^\"`    |" << endl;
+  cout << "||_/=\\\\__|" << endl;
+  return 0;
+}
