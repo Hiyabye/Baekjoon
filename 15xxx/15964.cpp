abbb464fb0914ce27ece2de24dc66813efff4285
Hiyabye <hiyabye0465@gmail.com>
2024-01-17 01:18:07 +0900

    Add 15964.cpp

---
+++ 15xxx/15964.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  long long a, b; cin >> a >> b;
+
+  cout << (a+b)*(a-b) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
