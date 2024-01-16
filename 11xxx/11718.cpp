f4c2acaec4396d61b53e602ef12c66b9d6092417
Hiyabye <hiyabye0465@gmail.com>
2024-01-17 01:15:49 +0900

    Add 11718.cpp

---
+++ 11xxx/11718.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s;
+  while (getline(cin, s)) cout << s << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
