6dcdda2217f6d7202c42603d096faf45a8a8fc96
Hiyabye <hiyabye0465@gmail.com>
2024-01-17 01:11:11 +0900

    Add 02744.cpp

---
+++ 02xxx/02744.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+inline char change(char c) {
+  return (c ^ 32);
+}
+
+void solve(void) {
+  string s; cin >> s;
+
+  for (char c : s) cout << change(c);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
