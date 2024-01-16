a2cff5bdccda7952e4bf5049dc1c0512b81e22d5
Hiyabye <hiyabye0465@gmail.com>
2024-01-17 01:09:28 +0900

    Add 02743.cpp

---
+++ 02xxx/02743.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  cout << s.length();
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
