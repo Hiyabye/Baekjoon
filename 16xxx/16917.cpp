9ce9608da96568184739b6484e715eacd9843ca4
Hiyabye <hiyabye0465@gmail.com>
2024-01-16 12:44:10 +0900

    Add 16917.cpp

---
+++ 16xxx/16917.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c, x, y; cin >> a >> b >> c >> x >> y;
+
+  cout << min({a*x+b*y, 2*c*max(x, y), 2*c*min(x, y)+a*(x-min(x, y))+b*(y-min(x, y))});
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
