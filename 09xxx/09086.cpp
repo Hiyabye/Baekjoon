65531e50852bc7605dc33bf9c1000e4348e22a07
Hiyabye <hiyabye0465@gmail.com>
2024-01-17 01:16:47 +0900

    Add 09086.cpp

---
+++ 09xxx/09086.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  cout << s[0] << s.back() << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
