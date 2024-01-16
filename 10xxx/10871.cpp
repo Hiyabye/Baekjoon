cac28138948b1f5c6972872abab355ef58b70b96
Hiyabye <hiyabye0465@gmail.com>
2024-01-17 01:03:47 +0900

    Add 10871.cpp

---
+++ 10xxx/10871.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n, x; cin >> n >> x;
+
+  while (n--) {
+    int a; cin >> a;
+    if (a < x) cout << a << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
