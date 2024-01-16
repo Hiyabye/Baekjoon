37330707073e0088aa8f5535e7efb4edc2b29dd8
Hiyabye <hiyabye0465@gmail.com>
2024-01-17 01:08:43 +0900

    Add 11654.cpp

---
+++ 11xxx/11654.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  char c; cin >> c;
+
+  cout << (int)c;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
