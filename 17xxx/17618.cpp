71e7158a2a4d0296b5ec50d887b7131247b3de7f
Hiyabye <hiyabye0465@gmail.com>
2024-01-17 16:17:50 +0900

    Add 17618.cpp

---
+++ 17xxx/17618.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int wow(int n) {
+  int sum = 0;
+  for (int i=n; i>0; i/=10) sum += i % 10;
+  return n % sum == 0;
+}
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  for (int i=1; i<=n; i++) ans += wow(i);
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
