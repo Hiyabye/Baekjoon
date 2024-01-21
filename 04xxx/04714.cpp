9dfa8305bbd7c96f7c1bcd644f9c45e92b331975
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 01:15:22 +0900

    Add 04714.cpp

---
+++ 04xxx/04714.cpp
@@ -0,0 +1,19 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double n; cin >> n;
+  while (n >= 0) {
+    cout << fixed << setprecision(2) << "Objects weighing " << n << " on Earth will weigh " << n*0.167 << " on the moon.\n";
+    cin >> n;
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
