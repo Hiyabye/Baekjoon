92ae932d7ecc6b6e01ac75d3414f97cf3f67f640
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 20:35:32 +0900

    Add 29986.cpp

---
+++ 29xxx/29986.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, h; cin >> n >> h;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) ans += (h >= a[i]);
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
