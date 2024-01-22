8438db4dc30c9a3c3117970f7aada1e8c9c08540
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 01:50:57 +0900

    Add 20215.cpp

---
+++ 20xxx/20215.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int w, h; cin >> w >> h;
+
+  cout << fixed << setprecision(7) << w+h-sqrt(w*w+h*h);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
