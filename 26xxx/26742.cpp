d547467028f40163a3e2d0415c938d1359bcd721
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 19:06:38 +0900

    Add 26742.cpp

---
+++ 26xxx/26742.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  cout << count(s.begin(), s.end(), 'B') / 2 + count(s.begin(), s.end(), 'C') / 2;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
