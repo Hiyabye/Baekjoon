a5fb09c6845a54d925948f60b4f783c62c01b67e
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 21:49:21 +0900

    Add 11121.cpp

---
+++ 11xxx/11121.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string a, b; cin >> a >> b;
+
+  cout << (a == b ? "OK" : "ERROR") << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
