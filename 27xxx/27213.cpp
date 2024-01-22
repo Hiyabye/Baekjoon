a107bfebe8d3d88e09523b1ef2eb0bbcbc4195a4
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 21:23:27 +0900

    Add 27213.cpp

---
+++ 27xxx/27213.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  long long m, n; cin >> m >> n;
+
+  cout << (m > 1 && n > 1 ? 2*(m+n-2) : m*n);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
