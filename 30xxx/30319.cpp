0d3835c1d4443d73cd9eb80e86946ad7510de0ff
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 21:10:14 +0900

    Add 30319.cpp

---
+++ 30xxx/30319.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  cout << (s <= "2023-09-16" ? "GOOD" : "TOO LATE");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
