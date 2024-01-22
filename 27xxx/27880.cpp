6c40b61c180102fb84351bce5b73e9577e58f89d
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 20:07:49 +0900

    Add 27880.cpp

---
+++ 27xxx/27880.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; int x;
+
+  int ans = 0;
+  while (cin >> s >> x) ans += (s == "Stair" ? 17 : 21) * x;
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
