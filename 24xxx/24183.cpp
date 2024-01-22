53759ba3fad03a980b5b0a292957b88f087f1146
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 21:37:04 +0900

    Add 24183.cpp

---
+++ 24xxx/24183.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double a, b, c; cin >> a >> b >> c;
+
+  cout << fixed << setprecision(6) << (a*229*324*2 + b*297*420*2 + c*210*297) * 0.000001;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
