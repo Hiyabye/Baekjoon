bf1f92cbfbf7827f98b4c19ff156134cd5a051ec
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 01:53:34 +0900

    Add 05358.cpp

---
+++ 05xxx/05358.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(string s) {
+  for (char c : s) {
+    if (c == 'e' || c == 'E') cout << (char)(c-'E'+'I');
+    else if (c == 'i' || c == 'I') cout << (char)(c-'I'+'E');
+    else cout << c;
+  }
+  cout << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  string s;
+  while (getline(cin, s)) solve(s);
+  return 0;
+}
