126b91491869c8b167ef35341ba1bc41e43bb160
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 02:02:06 +0900

    Add 27855.cpp

---
+++ 27xxx/27855.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int h1, b1; cin >> h1 >> b1;
+  int h2, b2; cin >> h2 >> b2;
+
+  if (3*(h1-h2) > b2-b1) cout << "1 " << 3*(h1-h2)+b1-b2;
+  else if (3*(h1-h2) < b2-b1) cout << "2 " << 3*(h2-h1)+b2-b1;
+  else cout << "NO SCORE";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
