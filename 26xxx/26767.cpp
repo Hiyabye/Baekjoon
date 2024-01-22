2f78d6147bc09b9e85e7e3a9a97c0eb52b796212
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 21:20:15 +0900

    Add 26767.cpp

---
+++ 26xxx/26767.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  for (int i=1; i<=n; i++) {
+    if (!(i % 7) && !(i % 11)) cout << "Wiwat!";
+    else if (!(i % 7)) cout << "Hurra!";
+    else if (!(i % 11)) cout << "Super!";
+    else cout << i;
+    cout << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
