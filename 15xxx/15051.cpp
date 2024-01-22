99927de067a71c2369ef4361459b3652a008a740
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 20:26:53 +0900

    Add 15051.cpp

---
+++ 15xxx/15051.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a1, a2, a3; cin >> a1 >> a2 >> a3;
+
+  cout << min({2*a2+4*a3, 2*a1+2*a3, 4*a1+2*a2});
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
