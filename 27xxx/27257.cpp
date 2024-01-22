ede8de03b7966f79e1f41372215c1dc2029ed7c2
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 01:29:07 +0900

    Add 27257.cpp

---
+++ 27xxx/27257.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int k; cin >> k;
+
+  while (!(k % 10)) k /= 10;
+  string s = to_string(k);
+  cout << count(s.begin(), s.end(), '0');
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
