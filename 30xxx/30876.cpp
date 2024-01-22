e37130c199523b313e5f8aaed3f4ce45e82505e6
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 19:00:59 +0900

    Add 30876.cpp

---
+++ 30xxx/30876.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  pair<int, int> ans = {1e9, 1e9};
+  for (int i=0; i<n; i++) {
+    int x, y; cin >> x >> y;
+    if (y < ans.second) ans = {x, y};
+  }
+  cout << ans.first << " " << ans.second;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
