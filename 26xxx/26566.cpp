aa6ea79f93eaf485b2be6448e6640470b5ce0402
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 19:41:25 +0900

    Add 26566.cpp

---
+++ 26xxx/26566.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a1, p1; cin >> a1 >> p1;
+  int r1, p2; cin >> r1 >> p2;
+
+  cout << (a1 * p2 > acos(-1) * r1 * r1 * p1 ? "Slice of pizza" : "Whole pizza") << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
