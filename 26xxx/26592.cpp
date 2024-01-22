c1d1291d0e7d871610549f3f32abfc9c15869ba1
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 20:11:10 +0900

    Add 26592.cpp

---
+++ 26xxx/26592.cpp
@@ -0,0 +1,18 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double area, base; cin >> area >> base;
+
+  cout << fixed << setprecision(2) << "The height of the triangle is " << area * 2 / base << " units\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
