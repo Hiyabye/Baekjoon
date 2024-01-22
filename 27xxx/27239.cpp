baba238fb202350d330be4aa31a569b72ecc6d1e
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 21:04:15 +0900

    Add 27239.cpp

---
+++ 27xxx/27239.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << (char)((n-1)%8+'a') << (n-1)/8+1;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
