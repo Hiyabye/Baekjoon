84ade3827f49412dd5d6f59c1891d9b5b6aa4fc9
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 19:37:10 +0900

    Add 24075.cpp

---
+++ 24xxx/24075.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+
+  cout << max(a+b, a-b) << "\n";
+  cout << min(a+b, a-b);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
