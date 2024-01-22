25c0fca1a8a4a0e4797a7365e6676d24018b50aa
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 21:44:31 +0900

    Add 08558.cpp

---
+++ 08xxx/08558.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  if (n >= 5) cout << '0';
+  else if (n == 4) cout << '4';
+  else if (n == 3) cout << '6';
+  else if (n == 2) cout << '2';
+  else cout << '1';
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
