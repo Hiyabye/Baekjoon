4e6011b82280fec94b9437406aed3bb3d75c357e
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 20:23:31 +0900

    Add 20867.cpp

---
+++ 20xxx/20867.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double m, s, g; cin >> m >> s >> g;
+  double a, b; cin >> a >> b;
+  double l, r; cin >> l >> r;
+
+  cout << (s*b*(m*a+g*l) > g*a*(m*b+s*r) ? "latmask" : "friskus");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
