df1bea88b0115641bfce4fa609d71c293dbadf31
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 01:55:44 +0900

    Add 16693.cpp

---
+++ 16xxx/16693.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void solve(void) {
+  int a1, p1; cin >> a1 >> p1;
+  int r1, p2; cin >> r1 >> p2;
+
+  cout << (a1*p2 > acos(-1)*r1*r1*p1 ? "Slice of pizza" : "Whole pizza");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
