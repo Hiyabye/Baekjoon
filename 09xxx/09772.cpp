a52cb0bfab6fb19359180930f920ed0356844286
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 01:31:35 +0900

    Add 09772.cpp

---
+++ 09xxx/09772.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool solve(void) {
+  double x, y; cin >> x >> y;
+  if (x == 0 && y == 0) {
+    cout << "AXIS\n";
+    return false;
+  }
+
+  if (x == 0 || y == 0)    cout << "AXIS";
+  else if (x > 0 && y > 0) cout << "Q1";
+  else if (x < 0 && y > 0) cout << "Q2";
+  else if (x < 0 && y < 0) cout << "Q3";
+  else                     cout << "Q4";
+  cout << "\n";
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  while (solve());
+  return 0;
+}
