09b4fdec247e3144d373c73b6d205f0f4921cd2b
Hiyabye <hiyabye0465@gmail.com>
2024-01-22 20:51:31 +0900

    Add 26768.cpp

---
+++ 26xxx/26768.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  for (char c : s) {
+    switch (c) {
+      case 'a': cout << "4"; break;
+      case 'e': cout << "3"; break;
+      case 'i': cout << "1"; break;
+      case 'o': cout << "0"; break;
+      case 's': cout << "5"; break;
+      default: cout << c; break;
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
