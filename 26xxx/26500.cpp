d815098f4c8bc0f3ef6efc0d43087c70b354f8a8
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 21:54:22 +0900

    Add 26500.cpp

---
+++ 26xxx/26500.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double a, b; cin >> a >> b;
+
+  cout << fixed << setprecision(1) << abs(a-b) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
