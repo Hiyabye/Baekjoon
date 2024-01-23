bf3b7094fa549ead82daed069a5734ad55ab98fc
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 23:30:10 +0900

    Add 06888.cpp

---
+++ 06xxx/06888.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int x, y; cin >> x >> y;
+
+  for (int i=x; i<=y; i+=60) {
+    cout << "All positions change in year " << i << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
