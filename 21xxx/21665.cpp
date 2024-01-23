e179fd9c4d1480302768aa2c5d37680725261639
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 00:48:41 +0900

    Add 21665.cpp

---
+++ 21xxx/21665.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  vector<string> a(n), b(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+  for (int i=0; i<n; i++) cin >> b[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) for (int j=0; j<m; j++) {
+    if ((a[i][j] == 'B' && b[i][j] == 'B') || (a[i][j] == 'W' && b[i][j] == 'W')) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
