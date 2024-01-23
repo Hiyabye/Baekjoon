4718182e6beee59f8f142e58746577776dedaeee
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 20:52:49 +0900

    Add 28248.cpp

---
+++ 28xxx/28248.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int p; cin >> p;
+  int c; cin >> c;
+
+  cout << 50*p - 10*c + (p > c ? 500 : 0);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
