eaebda95a3c605d906e0492aabe31dd94bd04c6c
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 21:39:04 +0900

    Add 21633.cpp

---
+++ 21xxx/21633.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int k; cin >> k;
+
+  k += 2500;
+  if (k < 10000) k = 10000;
+  if (k > 200000) k = 200000;
+  cout << k / 100 << ".";
+  if (k % 100 < 10) cout << "0";
+  cout << k % 100;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
