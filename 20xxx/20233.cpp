ebb75dfcfa607b4249026ab534e73c8c18cb6c64
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 21:47:09 +0900

    Add 20233.cpp

---
+++ 20xxx/20233.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a; cin >> a;
+  int x; cin >> x;
+  int b; cin >> b;
+  int y; cin >> y;
+  int t; cin >> t;
+
+  cout << a + (t <= 30 ? 0 : 21 * (t-30) * x) << " ";
+  cout << b + (t <= 45 ? 0 : 21 * (t-45) * y);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
