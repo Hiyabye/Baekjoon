712a47279a089c6e82b3e50a1b6e770fe4b886c7
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 23:22:22 +0900

    Add 27590.cpp

---
+++ 27xxx/27590.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int ds, ys; cin >> ds >> ys;
+  int dm, ym; cin >> dm >> ym;
+
+  ds = ys - ds;
+  dm = ym - dm;
+  while (ds != dm) {
+    if (ds < dm) ds += ys;
+    else dm += ym;
+  }
+  cout << ds;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
