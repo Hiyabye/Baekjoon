f3f5956238f0eab5f9265ca0246411f6ff45c24f
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 21:17:04 +0900

    Add 29683.cpp

---
+++ 29xxx/29683.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, a; cin >> n >> a;
+  vector<int> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) ans += v[i] / a;
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
