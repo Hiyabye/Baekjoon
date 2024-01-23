5db5e2a762610250de4d3c015a27ac31b84ecd20
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 20:56:20 +0900

    Add 30791.cpp

---
+++ 30xxx/30791.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  vector<int> v(5);
+  for (int i=0; i<5; i++) cin >> v[i];
+  sort(v.begin(), v.end(), greater<int>());
+
+  int ans = 0;
+  for (int i=1; i<5; i++) ans += (v[i] + 1000 >= v[0]);
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
