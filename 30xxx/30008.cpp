3f76cfa490224d53fa4e87dd3f7f87700c2f3980
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 21:31:22 +0900

    Add 30008.cpp

---
+++ 30xxx/30008.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n, k; cin >> n >> k;
+
+  for (int i=0; i<k; i++) {
+    int g; cin >> g;
+    int p = (g * 100) / n;
+    if (p <= 4) cout << "1 ";
+    else if (p <= 11) cout << "2 ";
+    else if (p <= 23) cout << "3 ";
+    else if (p <= 40) cout << "4 ";
+    else if (p <= 60) cout << "5 ";
+    else if (p <= 77) cout << "6 ";
+    else if (p <= 89) cout << "7 ";
+    else if (p <= 96) cout << "8 ";
+    else cout << "9 ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
