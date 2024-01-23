cbbf264e7ec973eb990c733163c1afbad2ad9821
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 23:15:25 +0900

    Add 24072.cpp

---
+++ 24xxx/24072.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c; cin >> a >> b >> c;
+
+  cout << (a <= c && c < b);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
