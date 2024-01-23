ddfdb06b11b6d35026a163f700297dc526ba81b3
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 15:45:00 +0900

    Add 28940.cpp

---
+++ 28xxx/28940.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int ceil(int a, int b) {
+  return (a+b-1) / b;
+}
+
+int solve(void) {
+  int w, h; cin >> w >> h;
+  int n, a, b; cin >> n >> a >> b;
+
+  return (a > w || b > h) ? -1 : ceil(n, (w/a)*(h/b));
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  cout << solve();
+  return 0;
+}
