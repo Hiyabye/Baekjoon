5afd03336f23abbb301756de7e7e6c3c4b42c241
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 21:58:42 +0900

    Add 25600.cpp

---
+++ 25xxx/25600.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    int a, d, g; cin >> a >> d >> g;
+    ans = max(ans, a * (d + g) * (a == d + g ? 2 : 1));
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
