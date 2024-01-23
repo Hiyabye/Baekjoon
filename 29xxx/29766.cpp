1579e5d0205650a4a2bb78cb6b2a4c03a6bed645
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 00:37:55 +0900

    Add 29766.cpp

---
+++ 29xxx/29766.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  int ans = 0;
+  for (int i=0; i<s.length()-3; i++) {
+    if (s.substr(i, 4) == "DKSH") ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
