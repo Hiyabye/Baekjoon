faad17f42994de2b51eaaaafb80ff76799dde04e
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 00:09:01 +0900

    Add 17874.cpp

---
+++ 17xxx/17874.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n, h, v; cin >> n >> h >> v;
+
+  cout << max({h*v, (n-h)*v, h*(n-v), (n-h)*(n-v)}) * 4;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
