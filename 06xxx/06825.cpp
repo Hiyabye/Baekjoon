d1a4a49d0d11e5ecb69f5cd662f96c87db14dd08
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 20:51:16 +0900

    Add 06825.cpp

---
+++ 06xxx/06825.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double w, h; cin >> w >> h;
+
+  cout << (w/(h*h) > 25 ? "Overweight" : (w/(h*h) < 18.5 ? "Underweight" : "Normal weight"));
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
