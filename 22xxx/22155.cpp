00def013c8ff4fff6378a9604e51ec4543e58ae5
Hiyabye <hiyabye0465@gmail.com>
2024-01-23 22:17:34 +0900

    Add 22155.cpp

---
+++ 22xxx/22155.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+bool solve(void) {
+  int i, f; cin >> i >> f;
+  
+  return (i <= 1 && f <= 2) || (i <= 2 && f <= 1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) cout << (solve() ? "Yes" : "No") << "\n";
+  return 0;
+}
