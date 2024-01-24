9f604e4821a876528f7b1f51394b321479376029
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 14:55:36 +0900

    Add 26772.cpp

---
+++ 26xxx/26772.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  const vector<string> v = {
+    " @@@   @@@ ",
+    "@   @ @   @",
+    "@    @    @",
+    "@         @",
+    " @       @ ",
+    "  @     @  ",
+    "   @   @   ",
+    "    @ @    ",
+    "     @     "
+  };
+
+  int n; cin >> n;
+  for (int i=0; i<9; i++) {
+    cout << v[i];
+    for (int j=1; j<n; j++) {
+      cout << " " << v[i];
+    }
+    cout << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
