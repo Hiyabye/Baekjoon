7edccaea5ef6d5a440e972943593e78351361140
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 15:23:41 +0900

    Add 28960.cpp

---
+++ 28xxx/28960.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+bool solve(void) {
+  int h, l, a, b; cin >> h >> l >> a >> b;
+
+  return (a <= l && b <= 2*h) || (b <= l && a <= 2*h);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  cout << (solve() ? "YES" : "NO");
+  return 0;
+}
