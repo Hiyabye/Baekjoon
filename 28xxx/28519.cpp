0752d0aa93bcb06bc888541bcafefd1d4255fa69
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 15:40:06 +0900

    Add 28519.cpp

---
+++ 28xxx/28519.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int x, y; cin >> x >> y;
+
+  cout << min(x + y, 2 * min(x, y) + 1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
