96867349ddf783ea4b614b9963968e27c6d584f0
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 17:05:44 +0900

    Add 18198.cpp

---
+++ 18xxx/18198.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  int ans = 0;
+  for (int i=0; i<s.length(); i+=2) {
+    ans += (s[i] == 'A' ? 1 : -1) * (s[i+1] - '0');
+  }
+  cout << (ans > 0 ? "A" : "B");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
