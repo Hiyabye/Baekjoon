67e06347593118b130afdac01cbead7feb86a1b4
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:47:33 +0900

    Add 25893.cpp

---
+++ 25xxx/25893.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c; cin >> a >> b >> c;
+
+  int cnt = (a >= 10) + (b >= 10) + (c >= 10);
+  cout << a << " " << b <<  " " << c << "\n";
+  cout << (cnt == 0 ? "zilch" : (cnt == 1 ? "double" : (cnt == 2 ? "double-double" : "triple-double"))) << "\n\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
