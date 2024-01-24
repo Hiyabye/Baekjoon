2593b403c9dfb159c6d409b07403280f4ec37b31
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 17:02:19 +0900

    Add 25858.cpp

---
+++ 25xxx/25858.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, d; cin >> n >> d;
+  vector<int> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+
+  int p = accumulate(v.begin(), v.end(), 0);
+  for (int i=0; i<n; i++) cout << d / p * v[i] << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
