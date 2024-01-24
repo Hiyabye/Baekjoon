38caad5dd9072fe863bdbfeb7f773b066ed84123
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:33:48 +0900

    Add 28249.cpp

---
+++ 28xxx/28249.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int t = 0;
+  for (int i=0; i<n; i++) {
+    string s; cin >> s;
+    if (s == "Poblano") t += 1500;
+    else if (s == "Mirasol") t += 6000;
+    else if (s == "Serrano") t += 15500;
+    else if (s == "Cayenne") t += 40000;
+    else if (s == "Thai") t += 75000;
+    else if (s == "Habanero") t += 125000;
+  }
+  cout << t;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
