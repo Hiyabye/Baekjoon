a1261c6a5c526f5ef8547415fba0865e60b40160
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:42:02 +0900

    Add 06887.cpp

---
+++ 06xxx/06887.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << "The largest square has side length " << int(sqrt(n)) << ".";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
