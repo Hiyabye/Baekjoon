656a513bf11522fd3313a46dd0ab6442870f7dc2
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:00:58 +0900

    Add 11257.cpp

---
+++ 11xxx/11257.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int id, s, m, t; cin >> id >> s >> m >> t;
+
+  cout << id << " " << s + m + t << " " << (s + m + t >= 55 && 2 * s >= 21 && 2 * m >= 15 && t >= 12 ? "PASS" : "FAIL") << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
