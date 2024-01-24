d09f15320b746a6b458c2b686b02fc1505cb4c61
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 15:13:43 +0900

    Add 08760.cpp

---
+++ 08xxx/08760.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int w, k; cin >> w >> k;
+
+  cout << w * k / 2 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int z; cin >> z;
+  while (z--) solve();
+  return 0;
+}
