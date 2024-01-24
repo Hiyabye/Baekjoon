2b91ff4c3058f3046d9c244fe43e96a291770770
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:06:24 +0900

    Add 30793.cpp

---
+++ 30xxx/30793.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int px, rx; cin >> px >> rx;
+
+  if (px * 10 < rx * 2) cout << "weak";
+  else if (px * 10 < rx * 4) cout << "normal";
+  else if (px * 10 < rx * 6) cout << "strong";
+  else cout << "very strong";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
