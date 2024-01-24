36875436a55795161adbc7a6adf656d9354e3245
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:48:55 +0900

    Add 28490.cpp

---
+++ 28xxx/28490.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    int h, w; cin >> h >> w;
+    ans = max(ans, h * w);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
