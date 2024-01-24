534c683a40401d006273e1790471e1a6289a86db
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 15:26:30 +0900

    Add 17903.cpp

---
+++ 17xxx/17903.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int m, n; cin >> m >> n;
+  vector<vector<int>> a(m, vector<int>(n));
+  for (int i=0; i<m; i++) for (int j=0; j<n; j++) cin >> a[i][j];
+
+  cout << (m >= 8 ? "satisfactory" : "unsatisfactory");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
