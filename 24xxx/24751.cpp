f2e51f268c4117adcbafa64be9e538555e400842
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:20:42 +0900

    Add 24751.cpp

---
+++ 24xxx/24751.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double a; cin >> a;
+
+  cout << fixed << setprecision(4) << 100 / a << "\n" << 100 / (100 - a);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
