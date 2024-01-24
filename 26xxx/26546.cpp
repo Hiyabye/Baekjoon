b74d1e689e84ea12306f4ed02c75fc58da359624
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:04:08 +0900

    Add 26546.cpp

---
+++ 26xxx/26546.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; int i, j; cin >> s >> i >> j;
+
+  cout << s.substr(0, i) << s.substr(j) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
