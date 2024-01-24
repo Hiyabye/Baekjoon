6148c538f72776b82a29e24ab984e1ace1df6def
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 15:18:40 +0900

    Add 30501.cpp

---
+++ 30xxx/30501.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<string> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+
+  for (int i=0; i<n; i++) {
+    if (v[i].find('S') != string::npos) {
+      cout << v[i];
+      return;
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
