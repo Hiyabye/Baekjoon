2c01344dc01f9d5c25f47ef57260bedd5699effc
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:15:28 +0900

    Add 29340.cpp

---
+++ 29xxx/29340.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string a; cin >> a;
+  string b; cin >> b;
+
+  for (int i=0; i<a.length(); i++) {
+    cout << (a[i] > b[i] ? a[i] : b[i]);
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
