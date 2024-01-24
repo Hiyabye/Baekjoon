3df757981062a65d26247c133a63dfb7761d7c27
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 15:17:08 +0900

    Add 21638.cpp

---
+++ 21xxx/21638.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int t1, v1; cin >> t1 >> v1;
+  int t2, v2; cin >> t2 >> v2;
+
+  if (t2 < 0 && v2 >= 10) cout << "A storm warning for tomorrow! Be careful and stay home if possible!";
+  else if (t2 < t1)       cout << "MCHS warns! Low temperature is expected tomorrow.";
+  else if (v2 > v1)       cout << "MCHS warns! Strong wind is expected tomorrow.";
+  else                    cout << "No message";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
