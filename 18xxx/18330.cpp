d05527ae9b2b4cfdee22a0ccbf097e2ad08ed990
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 14:51:54 +0900

    Add 18330.cpp

---
+++ 18xxx/18330.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  int k; cin >> k;
+
+  cout << 1500 * min(n, k+60) + 3000 * max(0, n-k-60);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
