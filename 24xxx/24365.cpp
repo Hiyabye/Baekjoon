7e9dff49faa51632b0ce8fd05adccf931e7ff918
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 15:31:51 +0900

    Add 24365.cpp

---
+++ 24xxx/24365.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c; cin >> a >> b >> c;
+
+  cout << c - a;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
