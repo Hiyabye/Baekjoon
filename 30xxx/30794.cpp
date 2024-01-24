9ae0aa3a3ca44404a5d8fa29c54bb19e19d8dac3
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 15:49:38 +0900

    Add 30794.cpp

---
+++ 30xxx/30794.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int lv; string s; cin >> lv >> s;
+
+  cout << (s == "miss" ? 0 : s == "bad" ? 200 : s == "cool" ? 400 : s == "great" ? 600 : 1000) * lv;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
