b42ff971f8c61ee0e54ff9692ec552549b177b3a
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 17:09:22 +0900

    Add 24087.cpp

---
+++ 24xxx/24087.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int s; cin >> s;
+  int a; cin >> a;
+  int b; cin >> b;
+
+  cout << 250 + (s > a ? (s-a+b-1)/b*100 : 0);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
