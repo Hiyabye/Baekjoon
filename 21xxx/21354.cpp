e9122362f79809dfaa348a5f2c8ab4690e0dfe21
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:12:40 +0900

    Add 21354.cpp

---
+++ 21xxx/21354.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, p; cin >> a >> p;
+
+  cout << (a * 7 < p * 13 ? "Petra" : a * 7 > p * 13 ? "Axel" : "lika");
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
