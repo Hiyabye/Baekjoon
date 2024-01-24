9d1ce36428583292f8717a87f8df03cdfdecef97
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:39:39 +0900

    Add 24196.cpp

---
+++ 24xxx/24196.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  string ans = "";
+  int idx = 0;
+  while (idx < s.length()-1) {
+    ans += s[idx];
+    idx += s[idx] - 'A' + 1;
+  }
+  ans += s.back();
+
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
