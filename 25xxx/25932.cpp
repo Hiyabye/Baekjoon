45b15a979a252202a607290abf853185057cb23a
Hiyabye <hiyabye0465@gmail.com>
2024-01-24 16:11:05 +0900

    Add 25932.cpp

---
+++ 25xxx/25932.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  vector<int> v(10);
+  for (int i=0; i<10; i++) cin >> v[i];
+
+  for (int i=0; i<10; i++) cout << v[i] << " ";
+  cout << "\n";
+
+  bool m = false, z = false;
+  for (int i=0; i<10; i++) {
+    if (v[i] == 18) m = true;
+    if (v[i] == 17) z = true;
+  }
+  cout << (m && z ? "both" : m ? "mack" : z ? "zack" : "none") << "\n\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
