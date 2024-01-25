d8ab49aa4557ae2389c721a263f34685c15a8cbd
Hiyabye <hiyabye0465@gmail.com>
2024-01-26 00:03:19 +0900

    Add 12847.cpp

---
+++ 12xxx/12847.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  vector<long long> t(n);
+  for (int i=0; i<n; i++) cin >> t[i];
+  for (int i=1; i<n; i++) t[i] += t[i-1];
+
+  long long ans = t[m-1];
+  for (int i=m; i<n; i++) ans = max(ans, t[i]-t[i-m]);
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+
+  solve();
+  return 0;
+}
