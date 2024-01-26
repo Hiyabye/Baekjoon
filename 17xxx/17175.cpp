e2c9ae667b97050967d2caad0ca5421eb6c9734a
Hiyabye <hiyabye0465@gmail.com>
2024-01-26 14:56:52 +0900

    Add 17175.cpp

---
+++ 17xxx/17175.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#define MOD 1000000007
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  vector<int> dp(n+1, 1);
+  for (int i=2; i<=n; i++) {
+    dp[i] = (dp[i-1] + dp[i-2] + 1) % MOD;
+  }
+
+  cout << dp[n];
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
