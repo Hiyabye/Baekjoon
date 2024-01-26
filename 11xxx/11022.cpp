21413992732d5ded53eee277d0b7c701212e1e74
Hiyabye <hiyabye0465@gmail.com>
2024-01-26 17:07:02 +0900

    Add 11022.cpp

---
+++ 11xxx/11022.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(int idx) {
+  int a, b; cin >> a >> b;
+
+  cout << "Case #" << idx << ": " << a << " + " << b << " = " << a + b << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  for (int i=1; i<=t; i++) solve(i);
+  return 0;
+}
