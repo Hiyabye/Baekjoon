a2b910258b659126faf5913e5c8fec8959c6a799
Hiyabye <hiyabye0465@gmail.com>
2024-01-26 17:05:29 +0900

    Add 10718.cpp

---
+++ 10xxx/10718.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "강한친구 대한육군\n";
+  cout << "강한친구 대한육군";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
