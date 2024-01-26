99d6f6ebea2a60f918fe12b8b7f193de955ccfe5
Hiyabye <hiyabye0465@gmail.com>
2024-01-26 17:22:22 +0900

    Add 18108.cpp

---
+++ 18xxx/18108.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int y; cin >> y;
+
+  cout << y - 543;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
