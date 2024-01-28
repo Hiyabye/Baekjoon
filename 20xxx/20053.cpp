753762f33275691a2aaa87d771cda69692f17fd7
Hiyabye <hiyabye0465@gmail.com>
2024-01-28 22:04:57 +0900

    Add 20053.cpp

---
+++ 20xxx/20053.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+
+  cout << *min_element(v.begin(), v.end()) << " " << *max_element(v.begin(), v.end()) << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
