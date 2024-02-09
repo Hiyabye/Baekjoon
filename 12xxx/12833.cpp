2ebc0b27c6559df9cbbabf22deb67bf36c2c164f
Hiyabye <hiyabye0465@gmail.com>
2024-02-09 16:04:08 +0900

    Add 12833.cpp

---
+++ 12xxx/12833.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b, c; cin >> a >> b >> c;
+
+  cout << (c & 1 ? a ^ b : a);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
