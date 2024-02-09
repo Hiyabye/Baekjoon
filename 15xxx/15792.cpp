174ec3ac1666996f839bb43515663a510dbb073b
Hiyabye <hiyabye0465@gmail.com>
2024-02-09 15:59:10 +0900

    Add 15792.cpp

---
+++ 15xxx/15792.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+
+  cout << a / b << ".";
+  for (int i=0; i<1000; i++) {
+    a = (a % b) * 10;
+    cout << a / b;
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
