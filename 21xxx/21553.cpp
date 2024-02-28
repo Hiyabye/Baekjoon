8a1f2a17089c9bbaf75b50652aa09cd2d33de0ac
Jihoon Lee <hiyabye0465@gmail.com>
2024-02-28 21:12:47 +0900

    Add 21553.cpp

---
+++ 21xxx/21553.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string a; cin >> a;
+  string p; cin >> p;
+
+  cout << p;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
