f332e9b011a04d2920cc8a50fc826b93b4728f4c
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-04 22:29:10 +0900

    Add 31472.cpp

---
+++ 31xxx/31472.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int w; cin >> w;
+
+  cout << sqrt(32 * w);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
