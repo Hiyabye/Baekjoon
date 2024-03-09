43f74190479605a8a3aab54782a85d93d773a98b
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-09 15:37:48 +0900

    Add 02437.cpp

---
+++ 02xxx/02437.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+  sort(v.begin(), v.end());
+
+  int ans = 1;
+  for (int i=0; i<n; i++) {
+    if (ans < v[i]) break;
+    ans += v[i];
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
