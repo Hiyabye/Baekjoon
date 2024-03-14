725f852f162a01646abe14f6061ff7f20ac49d76
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-14 12:25:25 +0900

    Add 31612.cpp

---
+++ 31xxx/31612.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  string s; cin >> s;
+
+  int ans = 0;
+  for (char c : s) {
+    ans += (c == 'j') * 2 + (c == 'o') + (c == 'i') * 2;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
