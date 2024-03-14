66028634dd179d6af5cc682426d5bc466b3bcec2
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-14 22:01:56 +0900

    Add 23530.cpp

---
+++ 23xxx/23530.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int a, b; cin >> a >> b;
+
+  cout << "1\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
