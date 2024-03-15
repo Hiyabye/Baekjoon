52c6f4f6171c31b868b217fd81d5d99153cac322
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-15 20:25:03 +0900

    Add 24623.cpp

---
+++ 24xxx/24623.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  double n; cin >> n;
+  double a; cin >> a;
+
+  cout << 90;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
