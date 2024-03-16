3142083ed6882787ce6be2216da429076f15e0a9
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-16 21:54:51 +0900

    Add 24724.cpp

---
+++ 24xxx/24724.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void solve(int idx) {
+  int n; cin >> n;
+  int a, b; cin >> a >> b;
+  while (n--) {
+    int u, v; cin >> u >> v;
+  }
+
+  cout << "Material Management " << idx << "\n";
+  cout << "Classification ---- End!\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  for (int i=1; i<=t; i++) solve(i);
+  return 0;
+}
