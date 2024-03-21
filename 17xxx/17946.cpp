845bd4dfe5630cb1ae179a861fcc0facab457922
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-21 23:52:12 +0900

    Add 17946.cpp

---
+++ 17xxx/17946.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int k; cin >> k;
+
+  cout << "1\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
