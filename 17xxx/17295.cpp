9a7100a6ccd75c0a9e078a8a0765fb66adc68eb6
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-23 12:56:19 +0900

    Add 17295.cpp

---
+++ 17xxx/17295.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "Avengers: Endgame";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
