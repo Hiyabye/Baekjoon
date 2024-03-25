bb5938b182848d309c552901e9876a9dcbb5a3a5
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-25 23:43:11 +0900

    Add 08658.cpp

---
+++ 08xxx/08658.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  for (int i=1; i<n; i++) {
+    if (n % i) {
+      cout << i << " ";
+      break;
+    }
+  }
+
+  for (int i=n-1; i>=1; i--) {
+    if (n % i) {
+      cout << i;
+      return;
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
