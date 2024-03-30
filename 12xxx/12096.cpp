e1f2c163388a851177677d068388535633310461
Jihoon Lee <hiyabye0465@gmail.com>
2024-03-31 02:47:08 +0900

    Add 12096.cpp

---
+++ 12xxx/12096.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "02-521-0487";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
