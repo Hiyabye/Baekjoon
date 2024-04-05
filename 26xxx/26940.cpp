40ec417cf2b44eb0a8c3405e5ed10fd82aa7cbaf
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-05 18:36:20 +0900

    Add 26940.cpp

---
+++ 26xxx/26940.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+
+  int ans = 0;
+  for (int i=1; i<n; i++) {
+    ans += (v[i] > v[i-1]);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
