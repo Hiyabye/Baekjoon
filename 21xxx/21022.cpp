793fb9165332b66d28aced9271cb15b906ee8046
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-05 18:31:54 +0900

    Add 21022.cpp

---
+++ 21xxx/21022.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n), b(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+  for (int i=0; i<n; i++) cin >> b[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    ans += (a[i] > b[i]) * 3 + (a[i] == b[i]);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
