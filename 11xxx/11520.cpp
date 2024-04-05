ed97eba93fa744103bde39603d00bbbf352cdfbf
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-05 18:33:48 +0900

    Add 11520.cpp

---
+++ 11xxx/11520.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int p, d; cin >> p >> d;
+
+  cout << d << " 5\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n; cin >> n;
+  while (n--) solve();
+  return 0;
+}
