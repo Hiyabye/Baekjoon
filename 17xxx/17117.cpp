5e4f067c2a9349e2f712589e75cc0be3f2fd2a80
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-06 21:15:56 +0900

    Add 17117.cpp

---
+++ 17xxx/17117.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s;
+  while (getline(cin, s));
+
+  cout << 0;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
