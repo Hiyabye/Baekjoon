8616123cb610412d0edf8466fb83d9b7b414e05b
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-11 12:23:04 +0900

    Add 24296.cpp

---
+++ 24xxx/24296.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  while (n & 1) n = (n + 1) / 2;
+  cout << n;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
