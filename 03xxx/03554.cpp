9029382b15ba84f9deb6acbb562013b9c5c9937c
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-11 14:45:30 +0900

    Add 03554.cpp

---
+++ 03xxx/03554.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  int m; cin >> m;
+  while (m--) {
+    int k, l, r; cin >> k >> l >> r; l--; r--;
+    if (k == 1) for (int j=l; j<r+1; j++) a[j] = a[j] * a[j] % 2010;
+    else cout << accumulate(a.begin()+l, a.begin()+r+1, 0) % 2010 << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
