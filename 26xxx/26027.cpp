40d31a04504fe63334d29c8dbc368ce75335dc82
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-12 14:50:42 +0900

    Add 26027.cpp

---
+++ 26xxx/26027.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int r; cin >> r;
+
+  cout << r << " 1";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
