6adf80a04eb244317f102852733e516c26a53909
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-13 22:50:30 +0900

    Add 25644.cpp

---
+++ 25xxx/25644.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  int mn = a[0], ans = 0;
+  for (int i=1; i<n; i++) {
+    mn = min(mn, a[i]);
+    ans = max(ans, a[i] - mn);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
