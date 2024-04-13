cdf547f9902014ace69a22cd22d9f84f82bd2bb4
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-14 00:10:15 +0900

    Add 24855.cpp

---
+++ 24xxx/24855.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+  sort(a.begin(), a.end());
+
+  cout << accumulate(a.begin()+(n+1)/2, a.end(), 0);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
