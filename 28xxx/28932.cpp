c100f7450fb61ffe682efc8234f30f06f1eb09fb
Jihoon Lee <hiyabye0465@gmail.com>
2024-04-21 14:18:16 +0900

    Add 28932.cpp

---
+++ 28xxx/28932.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n, a; cin >> n;
+  while (n--) cin >> a;
+
+  cout << "1 1";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
