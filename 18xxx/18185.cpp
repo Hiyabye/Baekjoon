e0de8959e4d5d496c846e76d1a9bb49500c1d2e8
Jihoon Lee <hiyabye0465@gmail.com>
2024-05-03 15:41:52 +0900

    Update 18185.cpp

--- 18xxx/18185.cpp
+++ 18xxx/18185.cpp
@@ -1,40 +1,45 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
+
+int one(vector<int> &a, int idx, int x) {
+  a[idx] -= x;
+  return 3 * x;
+}
+
+int two(vector<int> &a, int idx, int x) {
+  a[idx] -= x; a[idx+1] -= x;
+  return 5 * x;
+}
+
+int three(vector<int> &a, int idx, int x) {
+  a[idx] -= x; a[idx+1] -= x; a[idx+2] -= x;
+  return 7 * x;
+}
 
 void solve(void) {
   int n; cin >> n;
   vector<int> a(n+2, 0);
   for (int i=0; i<n; i++) cin >> a[i];
 
-  int ans = 0, cnt;
+  int ans = 0;
   for (int i=0; i<n; i++) {
     if (a[i+1] > a[i+2]) {
-      cnt = min(a[i], a[i+1]-a[i+2]);
-      ans += 5*cnt;
-      a[i] -= cnt; a[i+1] -= cnt;
-
-      cnt = min(min(a[i], a[i+1]), a[i+2]);
-      ans += 7*cnt;
-      a[i] -= cnt; a[i+1] -= cnt; a[i+2] -= cnt;
+      ans += two(a, i, min(a[i], a[i+1] - a[i+2]));
+      ans += three(a, i, min({a[i], a[i+1], a[i+2]}));
     } else {
-      cnt = min(min(a[i], a[i+1]), a[i+2]);
-      ans += 7*cnt;
-      a[i] -= cnt; a[i+1] -= cnt; a[i+2] -= cnt;
-      
-      cnt = min(a[i], a[i+1]);
-      ans += 5*cnt;
-      a[i] -= cnt; a[i+1] -= cnt;
+      ans += three(a, i, min({a[i], a[i+1], a[i+2]}));
+      ans += two(a, i, min(a[i], a[i+1]));
     }
-    ans += 3*a[i];
+    ans += one(a, i, a[i]);
   }
   cout << ans;
 }
 
 int main(void) {
   ios::sync_with_stdio(false);
-  cin.tie(nullptr); cout.tie(nullptr);
+  cin.tie(nullptr);
 
   solve();
   return 0;
