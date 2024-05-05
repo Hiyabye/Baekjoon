5b9f1fce24ec3c3c97171b9d80c53cc86d6a8195
Jihoon Lee <hiyabye0465@gmail.com>
2024-05-05 21:04:11 +0900

    Add 24009.cpp

---
+++ 24xxx/24009.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+long long power(long long base, long long exp, long long mod) {
+  long long ret = 1;
+  while (exp) {
+    if (exp & 1) ret = ret * base % mod;
+    base = base * base % mod;
+    exp >>= 1;
+  }
+  return ret;
+}
+
+void solve(int idx) {
+  int a, n, p; cin >> a >> n >> p;
+
+  long long ans = a % p;
+  for (int i=2; i<=n; i++) {
+    ans = power(ans, i, p);
+  }
+  cout << "Case #" << idx << ": " << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  for (int i=1; i<=t; i++) solve(i);
+  return 0;
+}
