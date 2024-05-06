e74d5e153abbcd6422e03789e028bb28d5545119
Jihoon Lee <hiyabye0465@gmail.com>
2024-05-06 16:21:30 +0900

    Add 18786.cpp

---
+++ 18xxx/18786.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> x(n), y(n);
+  for (int i=0; i<n; i++) cin >> x[i] >> y[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+      for (int k=0; k<n; k++) {
+        if (x[i] != x[j] || y[j] != y[k]) continue;
+        ans = max(ans, abs(y[i] - y[j]) * abs(x[j] - x[k]));
+      }
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
