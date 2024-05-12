248204354e78912d91933fb85cf6e3d853a1b19c
Jihoon Lee <hiyabye0465@gmail.com>
2024-05-12 17:29:20 +0900

    Add 25400.cpp

---
+++ 25xxx/25400.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  int ans = 0, cur = 1;
+  for (int i=0; i<n; i++) {
+    if (a[i] == cur) cur++;
+    else ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
