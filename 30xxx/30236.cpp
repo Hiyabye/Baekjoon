a3a9a9f06c7b60b611837990d3f6a9d43c14aa73
Jihoon Lee <hiyabye0465@gmail.com>
2024-05-12 18:04:37 +0900

    Add 30236.cpp

---
+++ 30xxx/30236.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  int ans = 1;
+  for (int i=0; i<n; i++, ans++) {
+    if (ans == a[i]) ans++;
+  }
+  cout << ans-1 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
