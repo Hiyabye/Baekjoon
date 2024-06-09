8346a334659082aa825f27a1f7bfbaf9808eec91
Jihoon Lee <hiyabye0465@gmail.com>
2024-06-09 16:31:04 +0900

    Add 21771.cpp

---
+++ 21xxx/21771.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int r, c; cin >> r >> c;
+  int rg, cg, rp, cp; cin >> rg >> cg >> rp >> cp;
+  vector<vector<char>> board(r, vector<char>(c));
+  for (int i=0; i<r; i++) for (int j=0; j<c; j++) cin >> board[i][j];
+
+  int cnt = 0;
+  for (int i=0; i<r; i++) for (int j=0; j<c; j++) {
+    if (board[i][j] == 'P') cnt++;
+  }
+  cout << (cnt != rp * cp);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
