ea61cb79e74285613371934bdb2e3d0b9b0d6a26
Jihoon Lee <hiyabye0465@gmail.com>
2024-06-19 10:17:34 +0900

    Add 31995.cpp

---
+++ 31xxx/31995.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  int m; cin >> m;
+
+  cout << 2*(n-1)*(m-1);
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
