83c1a928039897f5c0a8ea11ea193a4315df6909
Jihoon Lee <hiyabye0465@gmail.com>
2024-06-23 10:23:16 +0900

    Update 07562.cpp

--- 07xxx/07562.cpp
+++ 07xxx/07562.cpp
@@ -1,47 +1,38 @@
 #include <iostream>
+#include <queue>
 #include <utility>
 #include <vector>
-#include <queue>
 using namespace std;
 
-int dx[] = {-2, -2, -1, -1, 1, 1, 2, 2};
-int dy[] = {-1, 1, -2, 2, -2, 2, -1, 1};
+int dx[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+int dy[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
 
 void solve(void) {
-  int l;
-  pair<int, int> start, end;
-  cin >> l;
-  cin >> start.first >> start.second;
-  cin >> end.first >> end.second;
+  int l; cin >> l;
+  pair<int, int> a, b;
+  cin >> a.first >> a.second;
+  cin >> b.first >> b.second;
 
-  vector<vector<int> > ans(l, vector<int>(l, -1));
-  ans[start.first][start.second] = 0;
-  queue<pair<int, int> > q;
-  q.push(start);
+  vector<vector<int>> ans(l, vector<int>(l, -1)); ans[a.first][a.second] = 0;
+  queue<pair<int, int>> q; q.push(a);
   while (!q.empty()) {
-    int x = q.front().first, y = q.front().second;
-    q.pop();
-
-    if (x == end.first && y == end.second) break;
+    auto [x, y] = q.front(); q.pop();
+    if (x == b.first && y == b.second) break;
     for (int i=0; i<8; i++) {
       int nx = x + dx[i], ny = y + dy[i];
       if (nx < 0 || nx >= l || ny < 0 || ny >= l || ans[nx][ny] != -1) continue;
       ans[nx][ny] = ans[x][y] + 1;
-      q.push(make_pair(nx, ny));
+      q.push({nx, ny});
     }
   }
-
-  cout << ans[end.first][end.second] << "\n";
+  cout << ans[b.first][b.second] << "\n";
 }
 
 int main(void) {
   ios::sync_with_stdio(false);
-  cin.tie(nullptr); cout.tie(nullptr);
+  cin.tie(nullptr);
 
-  int t;
-  cin >> t;
-  while (t--) {
-    solve();
-  }
+  int t; cin >> t;
+  while (t--) solve();
   return 0;
 }
