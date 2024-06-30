81798680da9ef231bc7336056b7ed6b93053d3da
Jihoon Lee <hiyabye0465@gmail.com>
2024-06-30 17:31:05 +0900

    Add 19155.cpp

---
+++ 19xxx/19155.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  for (int i=0; i<n; i++) {
+    cout << n-a[i]+1 << " ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
