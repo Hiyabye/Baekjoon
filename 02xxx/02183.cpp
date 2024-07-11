f2d3641ab85b00c68740e865b162689a3e7ad86d
Jihoon Lee <hiyabye0465@gmail.com>
2024-07-11 12:24:31 +0900

    Add 02183.cpp

---
+++ 02xxx/02183.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  int n; string s; cin >> n >> s;
+
+  cout << s.back();
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
