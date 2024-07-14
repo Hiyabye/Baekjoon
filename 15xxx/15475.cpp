eeb222e1de0c2a2a462cbf67d540e8f13af2837b
Jihoon Lee <hiyabye0465@gmail.com>
2024-07-14 16:39:34 +0900

    Add 15475.cpp

---
+++ 15xxx/15475.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  int ans = 0, cur = 0;
+  for (int i=0; i<n; i++) {
+    cur = a[i] == 1 ? cur+1 : 0;
+    ans = max(ans, cur+1);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
