1a5fafc5cf59a14b164ef540cab9f6ae8abd859e
Jihoon Lee <hiyabye0465@gmail.com>
2024-07-14 13:20:34 +0900

    Add 24077.cpp

---
+++ 24xxx/24077.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  vector<int> a(n), b(m);
+  for (int i=0; i<n; i++) cin >> a[i];
+  for (int i=0; i<m; i++) cin >> b[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<m; j++) {
+      if (a[i] <= b[j]) ans++;
+    }
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
