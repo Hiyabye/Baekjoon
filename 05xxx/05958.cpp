d226d5cb49d1da201ed4cc73467b253f3da3c596
Jihoon Lee <hiyabye0465@gmail.com>
2024-07-15 10:02:09 +0900

    Add 05958.cpp

---
+++ 05xxx/05958.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+
+void dfs(int n, vector<vector<char>> &v, int y, int x) {
+  for (int i=0; i<4; i++) {
+    int ny = y + dy[i], nx = x + dx[i];
+    if (ny < 0 || ny >= n || nx < 0 || nx >= n || v[ny][nx] == '.') continue;
+    v[ny][nx] = '.';
+    dfs(n, v, ny, nx);
+  }
+}
+
+void solve(void) {
+  int n; cin >> n;
+  vector<vector<char>> v(n, vector<char>(n));
+  for (int i=0; i<n; i++) for (int j=0; j<n; j++) cin >> v[i][j];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) for (int j=0; j<n; j++) {
+    if (v[i][j] == '.') continue;
+    v[i][j] = '.';
+    dfs(n, v, i, j);
+    ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
