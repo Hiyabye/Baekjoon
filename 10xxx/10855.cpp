884657fe79d356de75be8c36cef6effabaf57924
Jihoon Lee <hiyabye0465@gmail.com>
2024-07-18 17:14:22 +0900

    Add 10855.cpp

---
+++ 10xxx/10855.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  for (int i=1; i<n; i++) {
+    if (a[i] < a[i-1]) return false;
+  }
+  return true;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << (solve() ? "yes" : "no");
+  return 0;
+}
