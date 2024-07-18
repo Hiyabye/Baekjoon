ebdfae5439d1235cc5b997e73dbd842025469704
Jihoon Lee <hiyabye0465@gmail.com>
2024-07-18 10:02:04 +0900

    Add 28346.cpp

---
+++ 28xxx/28346.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  long long ans = (a[n-1] ^ a[0]);
+  for (int i=1; i<n; i++) ans += (a[i] ^ a[i-1]);
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
