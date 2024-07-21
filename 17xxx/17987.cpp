b2115865c83fa7a50586475ccbe6d268152b84bc
Jihoon Lee <hiyabye0465@gmail.com>
2024-07-22 00:31:28 +0900

    Add 17987.cpp

---
+++ 17xxx/17987.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  cout << s << "O";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
