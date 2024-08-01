e28321a577e46f55df34d600538fbaa0ced0f1eb
Jihoon Lee <hiyabye0465@gmail.com>
2024-08-01 13:06:37 +0900

    Add 12518.cpp

---
+++ 12xxx/12518.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+inline bool vowel(char c) {
+  return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+         c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+void solve(int idx) {
+  string s; cin >> s;
+
+  cout << "Case #" << idx << ": " << s << " is ruled by ";
+  cout << (s.back() == 'Y' || s.back() == 'y' ? "nobody" : vowel(s.back()) ? "a queen" : "a king");
+  cout << ".\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  for (int i=1; i<=t; i++) solve(i);
+  return 0;
+}
