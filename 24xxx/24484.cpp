478cbb6e75121a7b6e4de006f78091eb981b54ef
Jihoon Lee <hiyabye0465@gmail.com>
2024-08-08 14:51:54 +0900

    Add 24484.cpp

---
+++ 24xxx/24484.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long idx = 1;
+vector<vector<int>> adj;
+vector<long long> a;
+
+void dfs(int r, long long d) {
+  a[r] = d * idx++;
+  for (int v : adj[r]) {
+    if (~a[v]) continue;
+    dfs(v, d+1);
+  }
+}
+
+void solve(void) {
+  int n, m, r; cin >> n >> m >> r; r--;
+  adj.resize(n);
+  while (m--) {
+    int u, v; cin >> u >> v; u--; v--;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  for (int i=0; i<n; i++) {
+    sort(adj[i].begin(), adj[i].end(), greater<int>());
+  }
+
+  a.assign(n, -1);
+  dfs(r, 0);
+
+  long long ans = 0;
+  for (int i=0; i<n; i++) {
+    if (~a[i]) ans += a[i];
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
