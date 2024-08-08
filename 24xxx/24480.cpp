e10070893151993405256d4d3d361c7c93e23b99
Jihoon Lee <hiyabye0465@gmail.com>
2024-08-08 13:50:35 +0900

    Add 24480.cpp

---
+++ 24xxx/24480.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int idx = 1;
+vector<vector<int>> adj;
+vector<int> ans;
+
+void dfs(int r) {
+  ans[r] = idx++;
+  for (int v : adj[r]) {
+    if (ans[v]) continue;
+    dfs(v);
+  }
+}
+
+void solve(void) {
+  int n, m, r; cin >> n >> m >> r; r--;
+  adj.resize(n);
+  while (m--) {
+    int u, v; cin >> u >> v; u--; v--;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  for (int i=0; i<n; i++) {
+    sort(adj[i].begin(), adj[i].end(), greater<int>());
+  }
+
+  ans.assign(n, 0);
+  dfs(r);
+
+  for (int i=0; i<n; i++) {
+    cout << ans[i] << "\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
