274f6ab9cc1b61a23384e6d3d848f1aadaed64ce
Jihoon Lee <hiyabye0465@gmail.com>
2024-08-13 00:16:50 +0900

    Add 09372.cpp

---
+++ 09xxx/09372.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  while (m--) {
+    int a, b; cin >> a >> b;
+  }
+  cout << n - 1 << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
