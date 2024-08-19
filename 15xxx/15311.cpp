41b45deabd573827cbed72aaab26e5f02d9b465b
Jihoon Lee <hiyabye0465@gmail.com>
2024-08-20 01:00:19 +0900

    Add 15311.cpp

---
+++ 15xxx/15311.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << 2000 << "\n";
+  for (int i=0; i<1000; i++) cout << 1 << " ";
+  for (int i=0; i<1000; i++) cout << 1000 << " ";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
