e84cf917f0c8b2b9631cdf1812f646452a7e7672
Jihoon Lee <hiyabye0465@gmail.com>
2024-08-26 13:33:43 +0900

    Add 28136.cpp

---
+++ 28xxx/28136.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  int ans = a[n-1] >= a[0];
+  for (int i=1; i<n; i++) {
+    if (a[i-1] >= a[i]) ans++;
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
