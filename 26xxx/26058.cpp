c6f6dc99cb3369ea115ecf7999701fd0bf9512f7
Jihoon Lee <hiyabye0465@gmail.com>
2024-08-26 17:08:16 +0900

    Add 26058.cpp

---
+++ 26xxx/26058.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  cout << "(-1/2+3)*40";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
