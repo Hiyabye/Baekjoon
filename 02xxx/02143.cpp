468f0f534fe9088c3b78bf7bb69dfb7effa557c0
Jihoon Lee <hiyabye0465@gmail.com>
2024-08-27 12:16:50 +0900

    Update 02143.cpp

--- 02xxx/02143.cpp
+++ 02xxx/02143.cpp
@@ -6,32 +6,22 @@ using namespace std;
 void solve(void) {
   int t; cin >> t;
   int n; cin >> n;
-  vector<int> a(n);
-  for (int i=0; i<n; i++) cin >> a[i];
+  vector<int> a(n+1, 0);
+  for (int i=1; i<=n; i++) cin >> a[i];
   int m; cin >> m;
-  vector<int> b(m);
-  for (int i=0; i<m; i++) cin >> b[i];
+  vector<int> b(m+1, 0);
+  for (int i=1; i<=m; i++) cin >> b[i];
 
   vector<int> c, d;
-  for (int i=0; i<n; i++) {
-    int sum = 0;
-    for (int j=i; j<n; j++) {
-      sum += a[j];
-      c.push_back(sum);
-    }
-  }
-  for (int i=0; i<m; i++) {
-    int sum = 0;
-    for (int j=i; j<m; j++) {
-      sum += b[j];
-      d.push_back(sum);
-    }
-  }
+  for (int i=1; i<=n; i++) a[i] += a[i-1];
+  for (int i=0; i<=n-1; i++) for (int j=i+1; j<=n; j++) c.push_back(a[j] - a[i]);
+  for (int i=1; i<=m; i++) b[i] += b[i-1];
+  for (int i=0; i<=m-1; i++) for (int j=i+1; j<=m; j++) d.push_back(b[j] - b[i]);
   sort(d.begin(), d.end());
 
   long long ans = 0;
   for (int e : c) {
-    ans += upper_bound(d.begin(), d.end(), t-e) - lower_bound(d.begin(), d.end(), t-e);
+    ans += upper_bound(d.begin(), d.end(), t - e) - lower_bound(d.begin(), d.end(), t - e);
   }
   cout << ans;
 }
