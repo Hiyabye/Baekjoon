a626b07e7a396fccfdc2b6898941dda96719310c
Jihoon Lee <hiyabye0465@gmail.com>
2024-08-31 13:13:38 +0900

    Add 08016.cpp

---
+++ 08xxx/08016.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+  sort(a.begin(), a.end());
+
+  int ans = 0;
+  for (int i=0; i<n; i++) ans += a[i];
+  for (int i=0; i<n/2; i++) ans += a[n-i-1] - a[i];
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
