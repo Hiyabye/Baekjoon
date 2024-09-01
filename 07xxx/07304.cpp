3467d9fbfaae7792914a53cf2176b8d40df74fb1
Jihoon Lee <hiyabye0465@gmail.com>
2024-09-01 10:49:15 +0900

    Add 07304.cpp

---
+++ 07xxx/07304.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> v(n);
+  for (int i=0; i<n; i++) cin >> v[i];
+  sort(v.begin(), v.end(), greater<int>());
+
+  int ans = 0;
+  for (int i=0; i<n; i++) {
+    ans = max(ans, v[i] * (i+1));
+  }
+  cout << ans << "\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t; cin >> t;
+  while (t--) solve();
+  return 0;
+}
