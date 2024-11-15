82070a99f8a6b62cb802bbe90a6bb2a4dde95232
Jihoon Lee <hiyabye0465@gmail.com>
2024-11-15 13:42:09 +0900

    Add 32652.cpp

---
+++ 32xxx/32652.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+	int k; cin >> k;
+	
+	cout << "AKA";
+	while (k--) cout << "RAKA";
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	solve();
+	return 0;
+}
