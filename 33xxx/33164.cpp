10e3ce36e572bbd80ab3e7694bc5456107d09b69
Jihoon Lee <hiyabye0465@gmail.com>
2025-01-14 13:39:00 +0000

    Add 33164.cpp

---
+++ 33xxx/33164.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n, m; cin >> n >> m;
+  vector<int> a(n), b(m);
+  for (int i=0; i<n; i++) cin >> a[i];
+  for (int i=0; i<m; i++) cin >> b[i];
+
+  int ans = 0;
+  for (int i=0; i<n; i++) for (int j=0; j<m; j++) {
+    ans += (a[i] + b[j]) * max(a[i], b[j]);
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
