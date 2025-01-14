91b014fac0f9dd13b6e0aeda2aa1336e1705d1ed
Jihoon Lee <hiyabye0465@gmail.com>
2025-01-14 13:39:10 +0000

    Add 33165.cpp

---
+++ 33xxx/33165.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int t; cin >> t;
+  int v; cin >> v;
+
+  cout << t * v;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
