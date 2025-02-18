92f2e98cde47008cc04f1bf42efa631abb99a198
Jihoon Lee <hiyabye0465@gmail.com>
2025-02-18 16:26:02 +0900

    Add 33225.cpp

---
+++ 33xxx/33225.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+
+  for (int i=1; i<s.length(); i++) {
+    if (s[i] == s[i-1]) cout << s[i];
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
