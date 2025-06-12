c14ab37437fc74a3aca939442403c8e674dd1f32
Jihoon Lee <hiyabye0465@gmail.com>
2025-06-12 11:57:51 +0900

    Add 33910.cpp

---
+++ 33xxx/33910.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+  vector<int> a(n);
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  long long ans = a[n-1];
+  for (int i=n-2; i>=0; i--) {
+    a[i] = min(a[i], a[i+1]);
+    ans += a[i];
+  }
+  cout << ans;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
