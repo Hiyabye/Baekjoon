1ee25a95fd2cf23b07b6a09fc01782d012fc669e
Jihoon Lee <hiyabye0465@gmail.com>
2025-07-27 10:16:40 +0900

    Add 12796.cpp

---
+++ 12xxx/12796.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int k; cin >> k;
+
+  cout << "3\n1 1 1 " << k+1;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
