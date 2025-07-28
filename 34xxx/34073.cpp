83a8ddd137a4b4cd9f7f62f8709e649e69386686
Jihoon Lee <hiyabye0465@gmail.com>
2025-07-28 14:38:55 +0000

    Add 34073.cpp

---
+++ 34xxx/34073.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  while (n--) {
+    string s; cin >> s;
+    cout << s << "DORO ";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
