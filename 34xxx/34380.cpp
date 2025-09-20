a712d857d17c596baf06242c4e1acf264a8f7191
Jihoon Lee <hiyabye0465@gmail.com>
2025-09-20 14:18:43 +0900

    Add 34380.cpp

---
+++ 34xxx/34380.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int m, v; cin >> m >> v;
+  int n; cin >> n;
+  int k; cin >> k;
+
+  cout << (n + 20) * 2;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
