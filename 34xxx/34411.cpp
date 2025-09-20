fe29d820803969888045764cde51bb3b485e3d05
Jihoon Lee <hiyabye0465@gmail.com>
2025-09-20 14:18:56 +0900

    Add 34411.cpp

---
+++ 34xxx/34411.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int m; cin >> m;
+  int n; cin >> n;
+  int y; cin >> y;
+
+  cout << m * n;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
