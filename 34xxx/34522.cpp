c798ba88f072f77398d82c361ba650fd295a95de
Jihoon Lee <hiyabye0465@gmail.com>
2025-11-08 10:40:16 +0900

    Add 34522.cpp

---
+++ 34xxx/34522.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << "Yeti";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
