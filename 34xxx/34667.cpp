42057afc55f3aa1f811cde248d9b04a26edef2bc
Jihoon Lee <hiyabye0465@gmail.com>
2025-11-08 15:39:08 +0900

    Add 34667.cpp

---
+++ 34xxx/34667.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(void) {
+  string s; cin >> s;
+  string t; cin >> t;
+
+  cout << s[0] << s;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
