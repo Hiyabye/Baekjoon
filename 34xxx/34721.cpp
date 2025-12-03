28add899cdbaa6f87d90af5ad3486b0c50c79e71
Jihoon Lee <hiyabye0465@gmail.com>
2025-12-03 17:59:55 +0900

    Add 34721.cpp

---
+++ 34xxx/34721.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  while (n--) {
+    cout << "I love DGU\n";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
