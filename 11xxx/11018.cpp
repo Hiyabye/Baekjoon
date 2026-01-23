edc8582f592a6c02ab65129f18d465a58c4f4eac
Jihoon Lee <hiyabye0465@gmail.com>
2026-01-23 11:36:21 +0900

    Add 11018.cpp

---
+++ 11xxx/11018.cpp
@@ -0,0 +1,29 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(int idx) {
+  int n; double M; cin >> n >> M;
+  vector<double> m(n), t(n), F(n);
+  for (int i=0; i<n; i++) cin >> m[i] >> t[i] >> F[i];
+  for (int i=0; i<n; i++) M += m[i];
+
+  double h = 0.0, v = 0.0;
+  for (int i=0; i<n; i++) {
+    double a = F[i] / M - 9.81;
+    h += v * t[i] + a * t[i] * t[i] / 2;
+    v += a * t[i];
+    M -= m[i];
+  }
+  cout << "Data Set " << idx << ":\n" << fixed << setprecision(2) << h << "\n\n";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int k; cin >> k;
+  for (int i=1; i<=k; i++) solve(i);
+  return 0;
+}
