b3035011ec17c6e7da54221a03aedd15d65f4ae0
Jihoon Lee <hiyabye0465@gmail.com>
2026-02-02 17:11:53 +0900

    Add 35213.cpp

---
+++ 35xxx/35213.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  int n; cin >> n;
+
+  cout << n << "\n";
+  while (n--) cout << "1 ";
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
