99525140b5709d0f644c1e3359ecb7523498e2df
Jihoon Lee <hiyabye0465@gmail.com>
2026-02-13 21:21:33 +0900

    Add 16647.cpp

---
+++ 16xxx/16647.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void solve(void) {
+  for (int i=1; i<=100; i++) {
+    cout << "생일축하";
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
